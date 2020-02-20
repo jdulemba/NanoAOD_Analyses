abda33fad13a668439646251efcf4bdfc437b0ff
jdulemba <josdrw9@gmail.com>
2020-02-20 15:26:21 +0000

    fix directories and environment

--- environment.sh
+++ environment.sh
@@ -1,6 +1,7 @@
 # Project-related environment variables
 NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
-unset PYTHONPATH
-export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
+#unset PYTHONPATH
+#export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
+export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled
