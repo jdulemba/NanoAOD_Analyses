1205611cf80a78e880a7cf3685a5b255ccad79cc
jdulemba <josdrw9@gmail.com>
2020-02-22 07:02:17 -0600

    Adds coffea to PYTHONPATH if running from normal LPC node

--- environment.sh
+++ environment.sh
@@ -2,6 +2,10 @@
 NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
 #unset PYTHONPATH
-#export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
+if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
+    echo "Adding coffea to PYTHONPATH to run on the LPC normal nodes"
+    export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
+fi
+
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
 export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled
