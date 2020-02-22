46f63eb8d2894528a6028caf1e43637c18d5ae4a
jdulemba <josdrw9@gmail.com>
2020-02-22 06:53:16 -0600

    Mtt and costheta dists calculated. Output hists are stored as .coffea objects but can be stored in root files for debugging

--- Analysis/scripts/get_meta_info.py
+++ Analysis/scripts/get_meta_info.py
@@ -7,12 +7,15 @@ import coffea.processor.dataframe
 #import itertools
 #import python.MCWeights as MCWeights
 import python.Partons as Partons
+from Utilities.make_variables import ctstar as ctstar
+from coffea.util import load, save
 
 parser = ArgumentParser()
 parser.add_argument('sample', default='ttJets', help='Samples to run over')
 parser.add_argument('--nfiles', default=-1, type=int, help='Specify the first number of files in the txt to run over. -1 means all')
 parser.add_argument('--year', choices=['2016', '2017', '2018'], default=2016, help='Specify which year to run over')
 parser.add_argument('--debug', action='store_true', help='Uses iterative_executor for debugging purposes, otherwise futures_excutor will be used (faster)')
+parser.add_argument('--routput', action='store_true', help='Output (1D) histograms to root file. Only valid during debugging.')
 
 args = parser.parse_args()
 
@@ -20,7 +23,13 @@ proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
 analyzer = 'get_meta_info'
 
-sample = '/'.join([proj_dir, 'inputs', jobid, '%s.txt' % args.sample])
+if args.sample.startswith('data_Single'):
+    raise IOError("Meta Info should only be run over simulation")
+
+isNominalTTbar = True if (args.sample == 'ttJets_PS' or args.sample == 'ttJets') else False
+
+sample = '/'.join([proj_dir, 'inputs', '%s_Testing' % args.year, '%s.txt' % args.sample])
+#sample = '/'.join([proj_dir, 'inputs', jobid, '%s.txt' % args.sample])
 if not os.path.isfile(sample):
     raise IOError("Sample file %s.txt not found" % args.sample)
 
@@ -36,15 +45,21 @@ fileset = {
 }
 
 # Look at ProcessorABC documentation to see the expected methods and what they are supposed to do
-class Test_Analyzer(processor.ProcessorABC):
+class Meta_Analyzer(processor.ProcessorABC):
     def __init__(self):
 
         #    ## make binning for hists
         self.pu_axis = hist.Bin("pu", "nTrueInt", 200, 0, 200)
+        self.mtt_axis = hist.Bin("mtt", "m_{tt}", 340, 300., 2000.)
+        self.ctstar_axis = hist.Bin("ctstar", r"cos($\theta^{*}$)", 200, -1., 1.)
 
         #set_trace()        
             ## make dictionary of hists
         histo_dict = {}
+        if isNominalTTbar:
+            histo_dict['mtt'] = hist.Hist("mtt", self.mtt_axis)
+            histo_dict['top_ctstar'] = hist.Hist("ctstar", self.ctstar_axis)
+            histo_dict['mtt_ctstar'] = hist.Hist("mtt_vs_ctstar", self.mtt_axis, self.ctstar_axis)
         histo_dict['PUDistribution'] = hist.Hist("PUDistribution", self.pu_axis)
         histo_dict['cutflow'] = processor.defaultdict_accumulator(int)
 
@@ -61,10 +76,23 @@ class Test_Analyzer(processor.ProcessorABC):
         if not isinstance(df, coffea.processor.dataframe.LazyDataFrame):
             raise IOError("This function only works for LazyDataFrame objects")
 
-        #set_trace()
 
-        ##df['GenPartons'] = Partons.process_genParts(df)
-        #df['LHEPartons'] = Partons.process_lheParts(df)
+        if isNominalTTbar:
+            genParts = Partons.process_genParts(df)
+
+                ## pick gen particles whose mother index == 0
+            gps = genParts[(genParts.momIdx == 0)]
+            tops = gps[(gps.pdgId == 6)]
+            antitops = gps[(gps.pdgId == -6)]
+
+            #set_trace()
+            mtt = (tops+antitops).p4.mass.flatten()
+            top_ctstar, tbar_ctstar = ctstar(tops.p4, antitops.p4)
+
+            output['mtt'].fill(mtt=mtt)
+            output['top_ctstar'].fill(ctstar=top_ctstar)
+            output['mtt_ctstar'].fill(mtt=mtt, ctstar=top_ctstar)
+
         #lumiBlocks = df.luminosityBlock
         #runs = df.run
         #events = df.event
@@ -79,15 +107,6 @@ class Test_Analyzer(processor.ProcessorABC):
         return output
 
 
-    #def fill_lep_hists(self, accumulator, tdir, obj):
-    #    #set_trace()
-    #    #accumulator['%s_mass' % tdir].fill(mass=obj.mass.flatten())
-    #    accumulator['%s_pt' % tdir].fill(pt=obj.pt.flatten())
-    #    accumulator['%s_eta' % tdir].fill(eta=obj.eta.flatten())
-    #    accumulator['%s_phi' % tdir].fill(phi=obj.phi.flatten())
-
-    #    return accumulator        
-
     def postprocess(self, accumulator):
         return accumulator
 
@@ -95,30 +114,50 @@ class Test_Analyzer(processor.ProcessorABC):
 proc_executor = processor.iterative_executor if args.debug else processor.futures_executor
 output = processor.run_uproot_job(fileset,
     treename='Events',
-    processor_instance=Test_Analyzer(),
+    processor_instance=Meta_Analyzer(),
     executor=proc_executor,
     executor_args={'workers': 4, 'flatten' : True},
     #chunksize=500000,
 )
 
-#print(output)
-#
-    ## write hists to root file
+    ## save output to coffea pkl file
 if args.nfiles == -1:
     outdir = '/'.join([proj_dir, 'results', jobid])
-    rfname = '%s/%s.root' % (outdir, args.sample)
+    cfname = '%s/%s.coffea' % (outdir, args.sample)
 else:
     outdir = proj_dir
-    rfname = '%s/%s.test.%s.root' % (outdir, args.sample, analyzer)
+    cfname = '%s/%s.test.%s.coffea' % (outdir, args.sample, analyzer)
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
-import uproot
-fout = uproot.recreate(rfname) if os.path.isfile(rfname) else uproot.create(rfname)
-histos = [key for key in output.keys() if key != 'cutflow']
-#set_trace()
-for histo in histos:
-    fout[histo] = hist.export1d(output[histo])
-fout.close()
+save(output, cfname)
 
-print('%s has been written' % rfname)
+#print(output)
+
+if (args.debug and args.routput):
+        ## write hists to root file
+    if args.nfiles == -1:
+        outdir = '/'.join([proj_dir, 'results', jobid])
+        rfname = '%s/%s.root' % (outdir, args.sample)
+    else:
+        outdir = proj_dir
+        rfname = '%s/%s.test.%s.root' % (outdir, args.sample, analyzer)
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    
+    import uproot
+    fout = uproot.recreate(rfname) if os.path.isfile(rfname) else uproot.create(rfname)
+    histos = [key for key in output.keys() if key != 'cutflow']
+    #set_trace()
+    for histo in histos:
+        print(histo)
+        if output[histo].dense_dim() == 1:
+            fout[histo] = hist.export1d(output[histo])
+        #elif output[histo].dense_dim() == 2:
+        #    #continue
+        #    fout[histo] = hist.export2d(output[histo])
+        else:
+            print('Only 1D histograms supproted for writing, %s skipped' % histo)
+    fout.close()
+    
+    print('%s has been written' % rfname)
