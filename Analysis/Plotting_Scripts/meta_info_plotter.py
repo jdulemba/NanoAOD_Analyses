140404cf9d485bca273d0605ad17ed30d673ab91
jdulemba <josdrw9@gmail.com>
2020-02-24 12:36:16 -0600

    Updated meta info plotter

--- Analysis/Plotting_Scripts/meta_info_plotter.py
+++ Analysis/Plotting_Scripts/meta_info_plotter.py
@@ -18,27 +18,46 @@ proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
 analyzer = 'get_meta_info'
 
-#set_trace()
+if not args.debug:
+    raise IOError("only debug supported for plotting at the moment")
+
 fname = '%s/%s.test.%s.coffea' % (proj_dir, args.sample, analyzer) if args.debug else '%s/%s.coffea' % ('/'.join([proj_dir, 'results', jobid]), args.sample)
-outdir = '/'.join([proj_dir, 'results'])
+outdir = '/'.join([proj_dir, 'plots', jobid, analyzer, 'Test']) if args.debug else '/'.join([proj_dir, 'plots', jobid, analyzer])
+
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
 hists = load(fname)
+#set_trace()
 
+variables = {
+    'mtt' : '$m_{t\\bart}$',
+    'ctstar' : 'cos($\\theta_{t}^{*}$)'
+}
+
+#set_trace()
 for hname in hists.keys():
+    if hname == 'MetaInfo': continue
     histo = hists[hname]
 
     fig = plt.figure()
-    if histo.dim() == 1:
+    #set_trace()
+    if histo.dense_dim() == 1:
+        #continue
         ax = plot.plot1d(histo)
-        plt.xlabel('$%s$' % histo.axes()[0].label)
+        if 'mtt' in hname:
+            plt.xlabel(variables['mtt'])
+        elif 'ctstar' in hname:
+            plt.xlabel(variables['ctstar'])
+        else:
+            plt.xlabel('$%s$' % histo.axes()[0].label)
         plt.ylabel('Events')
-    elif histo.dim() == 2:
+    elif histo.dense_dim() == 2:
+        continue
         set_trace()
-        ax = plot.plot2d(histo, 'mtt')
-        plt.xlabel('$%s$' % histo.axes()[0].label)
-        plt.ylabel(histo.axes()[1].label)
+        ax = plot.plot2d(histo, xaxis='mtt')
+        plt.xlabel(variables['mtt'])
+        plt.ylabel(variables['ctstar'])
 
 
     figname = '%s/%s_%s.png' % (outdir, args.sample, hname)
