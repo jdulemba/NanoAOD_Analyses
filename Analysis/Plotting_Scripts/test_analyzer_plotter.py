b32f08161e57ee31603c94d2a488b795cf8fe1b5
jdulemba <josdrw9@gmail.com>
2020-02-25 06:46:41 -0600

    Plots now show statistical uncs, I/O arg 'sample' not needed anymore

--- Analysis/Plotting_Scripts/test_analyzer_plotter.py
+++ Analysis/Plotting_Scripts/test_analyzer_plotter.py
@@ -8,13 +8,12 @@ from pdb import set_trace
 import os
 from argparse import ArgumentParser
 import styles
-import fnmatch
+import Utilities.plot_tools as plt_tools
 
 
 parser = ArgumentParser()
-parser.add_argument('sample', default='ttJets', help='Samples to run over')
 parser.add_argument('--year', default='2016', help='What year is the ntuple from.')
-parser.add_argument('--debug', action='store_true', help='Determines where input file is.')
+parser.add_argument('--testing', action='store_true', help='Determines where input file is.')
 
 args = parser.parse_args()
 
@@ -22,11 +21,12 @@ proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
 analyzer = 'test_analyzer'
 
-if not args.debug:
-    raise IOError("only debug supported for plotting at the moment")
+if not args.testing:
+    raise IOError("only testing supported for plotting at the moment")
 
-fname = '%s/%s.test.%s.coffea' % (proj_dir, args.sample, analyzer) if args.debug else '%s/%s.coffea' % ('/'.join([proj_dir, 'results', jobid]), args.sample)
-outdir = '/'.join([proj_dir, 'plots', jobid, analyzer, 'Test']) if args.debug else '/'.join([proj_dir, 'plots', jobid, analyzer])
+fname = '%s/%s.test.coffea' % (proj_dir, analyzer) if args.testing else '%s/%s.coffea' % ('/'.join([proj_dir, 'results', jobid]), 'test')
+#fname = '%s/%s.test.%s.coffea' % (proj_dir, args.sample, analyzer) if args.testing else '%s/%s.coffea' % ('/'.join([proj_dir, 'results', jobid]), args.sample)
+outdir = '/'.join([proj_dir, 'plots', jobid, analyzer, 'Test']) if args.testing else '/'.join([proj_dir, 'plots', jobid, analyzer])
 
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
@@ -61,18 +61,7 @@ objtypes = {
     ## get plotting colors/settings
 hstyles = styles.styles
 stack_fill_opts = {'alpha': 0.8, 'edgecolor':(0,0,0,.5)}
-#stack_error_opts = {'label':'Stat. Unc.', 'hatch':'///', 'facecolor':'none', 'edgecolor':(0,0,0,.5), 'linewidth': 0}
-
-def get_styles(sample, styles):
-    best_pattern = ''
-    for pattern, style_dict in styles.items():
-        if fnmatch.fnmatch(sample, pattern):
-            if len(pattern) > len(best_pattern):
-                best_pattern = pattern
-    if best_pattern:
-        return styles[best_pattern]['facecolor'], styles[best_pattern]['name']
-    else:
-        return None
+stack_error_opts = {'edgecolor':(0,0,0,.5)}
 
 
 for hname in hists.keys():
@@ -85,7 +74,10 @@ for hname in hists.keys():
     fig.subplots_adjust(hspace=.07)
     if histo.dense_dim() == 1:
         #set_trace()
-        plot.plot1d(histo, ax=ax, stack=True, fill_opts=stack_fill_opts)
+        plot.plot1d(histo, ax=ax, stack=True,
+            fill_opts=stack_fill_opts,
+            error_opts=stack_error_opts
+        )
         if kvar in variables.keys():
             xtitle = '%s(%s) [GeV]' % (variables[kvar], objtypes[obj_type]) if kvar == 'pt' else '%s(%s)' % (variables[kvar], objtypes[obj_type])
             plt.xlabel(xtitle)
@@ -98,7 +90,9 @@ for hname in hists.keys():
         ## set legend and corresponding colors
     handles, labels = plt.gca().get_legend_handles_labels()
     for idx, sample in enumerate(labels):
-        facecolor, legname = get_styles(sample, hstyles)
+        #print(sample)
+        #set_trace()
+        facecolor, legname = plt_tools.get_styles(sample, hstyles)
         handles[idx].set_facecolor(facecolor)
         labels[idx] = legname
     # call plt.legend() with the new values
@@ -114,10 +108,11 @@ for hname in hists.keys():
     )
 
 
-    pltdir = outdir if args.debug else '/'.join([outdir, jmult, lep_type])
+    pltdir = outdir if args.testing else '/'.join([outdir, jmult, lep_type])
     if not os.path.isdir(pltdir):
         os.makedirs(pltdir)
-    figname = '%s/%s_%s.png' % (pltdir, args.sample, hname)
+    figname = '%s/%s.png' % (pltdir, hname)
+    #figname = '%s/%s_%s.png' % (pltdir, args.sample, hname)
 
     fig.savefig(figname)
     print('%s written' % figname)
