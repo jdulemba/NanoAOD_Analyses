6338c546f7ddca619c526c99650ca2ee9dba5586
jdulemba <josdrw9@gmail.com>
2020-03-06 08:44:51 -0600

    Check if lumi mask works for data

--- Analysis/bin/meta_info.py
+++ Analysis/bin/meta_info.py
@@ -4,8 +4,6 @@ from pdb import set_trace
 import os
 from argparse import ArgumentParser
 import coffea.processor.dataframe
-#import itertools
-#import python.MCWeights as MCWeights
 import python.Partons as Partons
 from Utilities.make_variables import ctstar as ctstar
 from coffea.util import load, save
@@ -50,8 +48,8 @@ else:
     ## add files to fileset
 fileset = {}
 for sample in samples:
-    if sample.startswith('data_Single'):
-        raise IOError("Meta Info should only be run over simulation")
+    #if sample.startswith('data_Single'):
+    #    raise IOError("Meta Info should only be run over simulation")
 
     spath = '/'.join([indir, '%s.txt' % sample])
     if not os.path.isfile(spath):
@@ -74,17 +72,19 @@ for sample in samples:
     #set_trace()
     fileset[sample] = files_to_use
 
+#set_trace()
+lumi_mask = eval(open('%s/inputs/data/LumiMasks/%s_GoldenJson.txt' % (proj_dir, args.year)).readlines()[0])
 
 # Look at ProcessorABC documentation to see the expected methods and what they are supposed to do
 class Meta_Analyzer(processor.ProcessorABC):
-    def __init__(self):
+    def __init__(self, columns=[]):
+    #def __init__(self):
 
+        #if args.debug: set_trace()
         ## only get columns that are used
-        self._columns = [
-            'event', 'dataset', 'Pileup_nTrueInt',
-            'genWeight', 'nLHEPdfWeight', 'LHEPdfWeight',
-            'nGenPart', 'GenPart_pt', 'GenPart_eta', 'GenPart_phi', 'GenPart_mass', 'GenPart_genPartIdxMother', 'GenPart_pdgId'
-        ]
+        self._columns = columns
+
+        self.lumi_mask = lumi_mask
 
             ## make binning for hists
         self.dataset_axis = hist.Cat("dataset", "Event Process")
@@ -102,6 +102,7 @@ class Meta_Analyzer(processor.ProcessorABC):
             ## construct dictionary of dictionaries to hold meta info for each sample
         for sample in samples:
             histo_dict[sample] = processor.defaultdict_accumulator(int)
+            histo_dict['%s_runs_to_lumis' % sample] = processor.value_accumulator(list)
 
         self._accumulator = processor.dict_accumulator(histo_dict)
         self.sample_name = ''
@@ -120,51 +121,78 @@ class Meta_Analyzer(processor.ProcessorABC):
         if not isinstance(df, coffea.processor.dataframe.LazyDataFrame):
             raise IOError("This function only works for LazyDataFrame objects")
 
+        #if args.debug: set_trace()
         events = df.event
         self.sample_name = df.dataset
 
-        output['PUDistribution'].fill(dataset=self.sample_name, pu=df.Pileup_nTrueInt)
-
-        if args.debug: set_trace()
-        output[self.sample_name]['nEvents'] += events.size
-
-        genWeights = df.genWeight
-        output[self.sample_name]['nWeightedEvts'] += (genWeights != 0).sum()
-        output[self.sample_name]['sumGenWeights'] += genWeights.sum()
-
-        if 'LHEpdfWeight' in df.columns:
-                ## check if there's the same number of pdf weights in every event
-            if not np.equal(df.nLHEPdfWeight, df.nLHEPdfWeight[0]).all():
-                raise IOError("Events don't have the same number of LHE PDF weights!")
-            LHEpdfWeights = df.LHEPdfWeight
-                ## reshape because it's just a single fucking array instead of array of weights per event
-            LHEpdfWeights = LHEpdfWeights.reshape((df.nLHEPdfWeight[0], events.size))
-                ## get sum of each pdf weight over all events
-            sumLHEpdfWeights = LHEpdfWeights.sum(axis=1)
-            output[self.sample_name]['sumLHEpdfWeights'] += sumLHEpdfWeights
-
-
-            ## create mtt vs cos theta* dists for nominal ttJets
-        if (self.sample_name == 'ttJets_PS' or self.sample_name == 'ttJets'):
-            genParts = Partons.process_genParts(df)
-
-                ## pick gen particles whose mother index == 0
-            gps = genParts[(genParts.momIdx == 0)]
-            tops = gps[(gps.pdgId == 6)]
-            antitops = gps[(gps.pdgId == -6)]
-
-            mtt = (tops+antitops).p4.mass.flatten()
-            top_ctstar, tbar_ctstar = ctstar(tops.p4, antitops.p4)
+        if self.sample_name.startswith('data_Single'):
+            runs = df.run
+            runs_list = list(set(runs))
+            lumis = df.luminosityBlock
+            lumi_map = {str(run):list(set(lumis[np.where(runs == run)])) for run in runs_list}
+            valid_lumi_map = {}
+            for run_list, lumi_list in lumi_map.items():
+                ## initialize list of lumiBlocks that are in lumimask
+                if run_list in self.lumi_mask.keys(): ## check if runs in file are in lumimask
+                        ## find lumis from file that are within the range of valid lumis from the lumimask
+                    valid_lumi_map[run_list] = sorted([lumi_from_df for lumi_from_df in lumi_list for lumi_min, lumi_max in self.lumi_mask[run_list] if lumi_from_df >= lumi_min and lumi_from_df <= lumi_max])
+                    
             #set_trace()
+            valid_lumis = [item for sublist in list(valid_lumi_map.values()) for item in sublist]
+            evt_mask = np.isin(lumis, valid_lumis)
+            output['%s_runs_to_lumis' % self.sample_name].add(list(valid_lumi_map.items()))
 
-            for idx in range(10):
-                output['mtt_topctstar'].fill(dataset=self.sample_name, evtIdx='%s' % idx, mtt=mtt[(events % 10) == idx], ctstar=top_ctstar[(events % 10) == idx])
+            output[self.sample_name]['nEvents'] += events[evt_mask].size
 
-        #lumiBlocks = df.luminosityBlock
-        #runs = df.run
-        #set_trace()
-        #nom_LHEweight = df.LHEWeight_originalXWGTUP
-        #eff_lumi = expected_evts/xsection
+            output[self.sample_name]['nWeightedEvts'] += events[evt_mask].size
+
+
+        else:
+
+            output['PUDistribution'].fill(dataset=self.sample_name, pu=df.Pileup_nTrueInt)
+
+            output[self.sample_name]['nEvents'] += events.size
+
+            genWeights = df.genWeight
+            output[self.sample_name]['nWeightedEvts'] += (genWeights != 0).sum()
+            output[self.sample_name]['sumGenWeights'] += genWeights.sum()
+
+
+                ## create mtt vs cos theta* dists for nominal ttJets
+            if (self.sample_name == 'ttJets_PS' or self.sample_name == 'ttJets'):
+                genParts = Partons.process_genParts(df)
+
+                    ## pick gen particles whose mother index == 0
+                gps = genParts[(genParts.momIdx == 0)]
+                tops = gps[(gps.pdgId == 6)]
+                antitops = gps[(gps.pdgId == -6)]
+
+                mtt = (tops+antitops).p4.mass.flatten()
+                top_ctstar, tbar_ctstar = ctstar(tops.p4, antitops.p4)
+                #set_trace()
+
+                for idx in range(10):
+                    output['mtt_topctstar'].fill(dataset=self.sample_name, evtIdx='%s' % idx, mtt=mtt[(events % 10) == idx], ctstar=top_ctstar[(events % 10) == idx])
+                    output[self.sample_name]['nWeightedEvts_%s' % idx] += (genWeights[(events % 10) == idx] != 0).sum()
+
+            if 'LHEPdfWeight' in df.columns:
+                    ## check if there's the same number of pdf weights in every event
+                pdf_wt_list = list(set(df.nLHEPdfWeight))
+                #print(pdf_wt_list)
+                if len(pdf_wt_list) > 1:
+                    print(pdf_wt_list)
+                    return output
+                    #raise IOError("Events don't have the same number of LHE PDF weights!")
+                #set_trace()
+                #if not np.equal(df.nLHEPdfWeight, df.nLHEPdfWeight[0]).all():
+                #    raise IOError("Events don't have the same number of LHE PDF weights!")
+                LHEpdfWeights = df.LHEPdfWeight
+                    ## reshape because it's just a single fucking array instead of array of weights per event
+                LHEpdfWeights = LHEpdfWeights.reshape((df.nLHEPdfWeight[0], events.size))
+                    ## get sum of each pdf weight over all events
+                sumLHEpdfWeights = LHEpdfWeights.sum(axis=1)
+                output[self.sample_name]['sumLHEpdfWeights'] += sumLHEpdfWeights
+
 
         return output
 
@@ -172,22 +200,31 @@ class Meta_Analyzer(processor.ProcessorABC):
     def postprocess(self, accumulator):
         return accumulator
 
+# define columns to use (not working currently)
+columns = [
+    'event', 'dataset', 'Pileup_nTrueInt',
+    'genWeight', 'nLHEPdfWeight', 'LHEPdfWeight',
+    'nGenPart', 'GenPart_pt', 'GenPart_eta', 'GenPart_phi', 'GenPart_mass', 'GenPart_genPartIdxMother', 'GenPart_pdgId'
+]
 
+#set_trace()
 proc_executor = processor.iterative_executor if args.debug else processor.futures_executor
 output = processor.run_uproot_job(fileset,
     treename='Events',
-    processor_instance=Meta_Analyzer(),
+    processor_instance=Meta_Analyzer(columns=columns),
     executor=proc_executor,
     executor_args={
-        'workers': 4,
+        'workers': 8,
+        #'workers': 4,
         'flatten' : True,
-        'compression' : 5,
+        'processor_compression' : 5,
     },
     chunksize=50000,
     #chunksize=500000,
 )
 
 if args.debug: print(output)
+#print(output)
 
     ## save output to coffea pkl file
 if (args.frange).lower() == 'all':
