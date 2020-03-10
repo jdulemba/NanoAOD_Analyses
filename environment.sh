b115f8416c4d957d33192a821179d6bdd5aa5dbe
jdulemba <josdrw9@gmail.com>
2020-03-10 13:24:53 -0500

    library paths updated

--- environment.sh
+++ environment.sh
@@ -4,11 +4,11 @@ NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Tue/x86_64-centos7-gcc8-opt/setup.sh
 export LIBRARY_PATH=$LD_LIBRARY_PATH
 
-#unset PYTHONPATH
-if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
-    echo "Adding coffea to PYTHONPATH to run on the LPC normal nodes"
-    export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
-fi
-
+##unset PYTHONPATH
+#if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
+#    echo "Adding coffea to PYTHONPATH to run on the LPC normal nodes"
+#fi
+#
+#export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
 export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled:$LD_LIBRARY_PATH
