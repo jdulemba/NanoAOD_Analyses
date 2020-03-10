978c74539e0963aa8b46cdeb038262c1db839bd1
jdulemba <josdrw9@gmail.com>
2020-03-10 09:26:01 -0500

    Added cvmfs source

--- environment.sh
+++ environment.sh
@@ -1,6 +1,9 @@
 # Project-related environment variables
 NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
+source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Tue/x86_64-centos7-gcc8-opt/setup.sh
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+
 #unset PYTHONPATH
 if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
     echo "Adding coffea to PYTHONPATH to run on the LPC normal nodes"
@@ -8,4 +11,4 @@ if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
 fi
 
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
-export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled
+export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled:$LD_LIBRARY_PATH
