c9b0cdd87be1890e5d4a4dc76c663f84cb3f8e08
jdulemba <josdrw9@gmail.com>
2020-03-17 06:54:33 -0500

    Added rebinning and axis limits. Plots for full results now available

--- Analysis/Plotting_Scripts/presel_analyzer_plotter.py
+++ Analysis/Plotting_Scripts/presel_analyzer_plotter.py
@@ -15,6 +15,7 @@ from coffea import hist
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument('year', choices=['2016', '2017', '2018'], help='What year is the ntuple from.')
+parser.add_argument('lepton', choices=['Electron', 'Muon'], help='Choose which lepton to make plots for')
 parser.add_argument('--sample', type=str, help='Input sample to use.')
 parser.add_argument('--testing', action='store_true', help='Determines where input file is.')
 parser.add_argument('--use_combined', action='store_true', help='Used file that has multiple datasets.')
@@ -25,10 +26,7 @@ proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
 analyzer = 'presel_analyzer'
 
-if not args.testing:
-    raise IOError("only testing supported for plotting at the moment")
-
-input_dir = proj_dir if args.testing else '/'.join([proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer])
+input_dir = proj_dir if args.testing else '/'.join([proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer, args.lepton])
 f_ext = '%s.test.coffea' % analyzer if args.testing else '.coffea'
 outdir = '/'.join([proj_dir, 'plots', '%s_%s' % (args.year, jobid), analyzer, 'Test']) if args.testing else '/'.join([proj_dir, 'plots', '%s_%s' % (args.year, jobid), analyzer])
 if not os.path.isdir(outdir):
@@ -49,14 +47,6 @@ hdict = plt_tools.add_coffea_files(fnames) if len(fnames) > 1 else load(fnames[0
 
 data_lumi_year = prettyjson.loads(open('%s/inputs/lumis_data.json' % proj_dir).read())[args.year]
 
-variables = {
-    'pt' : '$p_{T}$',
-    'eta' : '$\\eta$',
-    'phi' : '$\\phi$',
-    'energy' : 'E',
-    'njets' : '$n_{jets}$',
-}
-
 jet_mults = {
     '3Jets' : '3 jets',
     '4PJets' : '4+ jets'
@@ -68,6 +58,19 @@ objtypes = {
         'Muon' : '$\\mu$',
         'Electron' : '$e$',
     }
+}
+
+variables = {
+    'Jets_pt' : ('$p_{T}$(jets) [GeV]', 2, (0., 300.)),
+    'Jets_eta' : ('$\\eta$(jets)', 1, (-2.5, 2.5)),
+    'Jets_phi' : ('$\\phi$(jets)', 1, (-4., 4.)),
+    'Jets_energy' : ('E(jets) [GeV]', 2, (0., 500.)),
+    'Jets_njets' : ('$n_{jets}$', 1, (0, 15)),
+    'Lep_pt' : ('$p_{T}$(%s) [GeV]' % objtypes['Lep'][args.lepton], 2, (0., 300.)),
+    'Lep_eta' : ('$\\eta$(%s)' % objtypes['Lep'][args.lepton], 1, (-2.5, 2.5)),
+    'Lep_phi' : ('$\\phi$(%s)' % objtypes['Lep'][args.lepton], 1, (-4., 4.)),
+    'Lep_energy' : ('E(%s) [GeV]' % objtypes['Lep'][args.lepton], 2, (0., 500.)),
+    'Lep_iso' : ('pfRelIso, %s' % objtypes['Lep'][args.lepton], 1, (0., 1.)),
 }
 
     ## get plotting colors/settings
@@ -106,6 +109,8 @@ for hname in hdict.keys():
     histo = hdict[hname]
     #set_trace()
 
+    xtitle, rebinning, x_lims = variables[hname]
+
     if histo.dense_dim() == 1:
         ## hists should have 3 category axes (dataset, jet multiplicity, lepton type) followed by variable
         for jmult in histo.axes()[1]._sorted:
@@ -113,6 +118,10 @@ for hname in hdict.keys():
                 fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
                 fig.subplots_adjust(hspace=.07)
                 hslice = histo[:, jmult, lep].integrate('jmult').integrate('leptype')
+
+                if rebinning != 1:
+                    xaxis_name = hslice.dense_axes()[0].name
+                    hslice = hslice.rebin(xaxis_name, rebinning)
 
                     ## plot MC and data
                 plot.plot1d(hslice[mc_samples],
@@ -133,13 +142,12 @@ for hname in hdict.keys():
                 ax.autoscale(axis='x', tight=True)
                 ax.set_ylim(0, None)
                 ax.set_xlabel(None)
+                ax.set_xlim(x_lims)
 
                     ## set legend and corresponding colors
                 handles, labels = ax.get_legend_handles_labels()
                 for idx, sample in enumerate(labels):
                     if sample == 'data' or sample == 'Observed': continue
-                    #print(sample)
-                    #set_trace()
                     facecolor, legname = plt_tools.get_styles(sample, hstyles)
                     handles[idx].set_facecolor(facecolor)
                     labels[idx] = legname
@@ -157,21 +165,11 @@ for hname in hdict.keys():
                 )
                 rax.set_ylabel('data/MC')
                 rax.set_ylim(0,2)
+                rax.set_xlim(x_lims)
 
 
                     ## set axes labels and titles
-                obj, kvar = hname.split('_')
-                if kvar in variables.keys():
-                    if kvar == 'njets':
-                        xtitle = variables[kvar]
-                    elif kvar == 'pt' or kvar == 'energy':
-                        xtitle = '%s(%s) [GeV]' % (variables[kvar], objtypes[obj]) if obj == 'Jets' else '%s(%s) [GeV]' % (variables[kvar], objtypes[obj][lep])
-                    else:
-                        xtitle = '%s(%s)' % (variables[kvar], objtypes[obj]) if obj == 'Jets' else '%s(%s)' % (variables[kvar], objtypes[obj][lep])
-                    plt.xlabel(xtitle)
-                else:
-                    plt.xlabel('$%s$' % histo.axes()[-1].label)
-
+                plt.xlabel(xtitle)
                 cms_blurb = plt.text(
                     0., 1., r"CMS Preliminary",
                     fontsize=12, 
@@ -181,19 +179,19 @@ for hname in hdict.keys():
                     style='italic'
                 )
                 lumi_blurb = plt.text(
-                    1., 1., r"(13 TeV %.2f fb$^{-1}$, %s + %s)" % (data_lumi_year['%ss' % lep]/1000., jet_mults[jmult], objtypes['Lep'][lep]),
+                    1., 1., r"(13 TeV %.2f fb$^{-1}$, %s/%s)" % (data_lumi_year['%ss' % lep]/1000., objtypes['Lep'][lep], jet_mults[jmult]),
                     fontsize=12, 
                     horizontalalignment='right', 
                     verticalalignment='bottom', 
                     transform=ax.transAxes
                 )
 
-                pltdir = outdir if args.testing else '/'.join([outdir, jmult, lep_type])
+                pltdir = outdir if args.testing else '/'.join([outdir, jmult, lep])
                 if not os.path.isdir(pltdir):
                     os.makedirs(pltdir)
                 figname = '%s/%s.png' % (pltdir, '_'.join([jmult, lep, hname]))
-                #figname = '%s/%s_%s.png' % (pltdir, args.sample, hname)
 
+                #set_trace()
                 fig.savefig(figname)
                 print('%s written' % figname)
                 plt.close()
