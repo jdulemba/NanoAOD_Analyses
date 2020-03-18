772e39ad40d9de181a3385dcad77241720ce3a26
jdulemba <josdrw9@gmail.com>
2020-03-18 19:28:41 +0100

    grid init removed

--- environment.sh
+++ environment.sh
@@ -1,16 +1,8 @@
 # Project-related environment variables
-NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
-
-voms-proxy-init -voms cms --valid 168:00
+export NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
 source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Tue/x86_64-centos7-gcc8-opt/setup.sh
 export LIBRARY_PATH=$LD_LIBRARY_PATH
 
-##unset PYTHONPATH
-#if [[ $NANODIR ==  "/uscms_data/d3/jdulemba/NanoAOD_Analyses" ]]; then
-#    echo "Adding coffea to PYTHONPATH to run on the LPC normal nodes"
-#fi
-#
-#export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
 export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled:$LD_LIBRARY_PATH
