4c79fa1b0dd2c16cb3b72a28dffc2cbabc103708
jdulemba <josdrw9@gmail.com>
2020-03-18 19:25:05 +0100

    Merge output coffea files into one combined file

--- Analysis/Utilities/track_jobs.py
+++ Analysis/Utilities/track_jobs.py
@@ -1,20 +1,16 @@
 import os, subprocess, time
 from pdb import set_trace
+import Utilities.plot_tools as plot_tools
 
-#from argparse import ArgumentParser
-#parser = ArgumentParser()
-#parser.add_argument('indir', help='specify name of input directory')
-#
-#args = parser.parse_args()
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('jobdir', help='specify name of directory where jobs were submitted')
+args = parser.parse_args()
 
 proj_dir = os.environ['PROJECT_DIR']
 user = os.environ['USER']
 
-##set_trace()
-#jobdir = args.indir if (args.indir).startswith('/afs/cern.ch/work/j/%s/' % user) else '%s/%s' % (proj_dir, args.indir)
-#if not os.path.isdir(jobdir):
-#    raise IOError('Directory: %s does not exist' % jobdir)
-
+#set_trace()
 escape = False
 start = time.time()
 totjobs = -1
@@ -37,10 +33,34 @@ while not escape:
         totjobs = njobs
 
     if njobs == 0:
-        print('Jobs completed')
+        print('Jobs completed: runtime = %i' % elapsed)
         escape = True
 
     else:
         print("%i jobs are still running, checking again in 30 seconds. ETA: %s" % (njobs, eta))
         time.sleep(30)
 
+
+## once jobs are finished, merge all output coffea files for each sample into one
+jobdir = args.jobdir if (args.jobdir).startswith('/afs/cern.ch/work/j/%s/' % user) else '%s/%s' % (proj_dir, args.jobdir)
+if not os.path.isdir(jobdir):
+    raise IOError('Directory: %s does not exist' % jobdir)
+
+set_trace()
+samples = [dirname for dirname in os.listdir(jobdir) if os.path.isdir('%s/%s' % (jobdir, dirname))]
+
+tot_files = []
+for sample in samples:
+    output_files = ['%s/%s/%s' % (jobdir, sample, fname) for fname in os.listdir('%s/%s' % (jobdir, sample)) if fname.endswith('.coffea')]
+    if len(output_files) > 1:
+        ## merge files
+        output_acc = plot_tools.add_coffea_files(output_files)
+        plot_tools.save_accumulator(output_acc, '%s/%s/%s_TOT.coffea' % (jobdir, sample, sample))
+    else:
+        ## mv file to
+        os.system('mv %s %s/%s/%s_TOT.coffea' % (output_files[0], jobdir, sample, sample))
+    tot_files.append('%s/%s/%s_TOT.coffea' % (jobdir, sample, sample))
+
+## merge all TOT files from each sample into one
+tot_acc = plot_tools.add_coffea_files(tot_files)
+plot_tools.save_accumulator(tot_acc, '%s/total_output.coffea' % jobdir)
