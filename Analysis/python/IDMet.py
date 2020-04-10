a08e92f9f7d8a9d9fb5d37a3f10c9978908d1726
jdulemba <josdrw9@gmail.com>
2020-04-10 14:58:11 +0200

    MET object now JaggedArray instead of collection of TLorentzVectors

--- Analysis/python/IDMet.py
+++ Analysis/python/IDMet.py
@@ -1,11 +1,20 @@
-from coffea.arrays import Initialize
+import numpy as np
+#from pdb import set_trace
 
-def SetMET(df):
-    met = Initialize({
-        'pt' : df['MET_pt'],
-        'eta' : 0,
-        'phi' : df['MET_phi'],
-        'mass' : 0
-    })
+def process_met(df):
+    from coffea.analysis_objects import JaggedCandidateArray
+    met = JaggedCandidateArray.candidatesfromcounts(
+        counts=np.ones(df.size, dtype=int),
+        pt=df['MET_pt'],
+        eta=0,
+        phi=df['MET_phi'],
+        mass=0,
+        MetUnclustEnUpDeltaX=df['MET_MetUnclustEnUpDeltaX'],
+        MetUnclustEnUpDeltaY=df['MET_MetUnclustEnUpDeltaY'],
+        covXX=df['MET_covXX'],
+        covXY=df['MET_covXY'],
+        covYY=df['MET_covYY'],
+        sumEt=df['MET_sumEt'],
+    )
 
     return met
