9322a8acd486bebbe45e297202d1eae968c45efe
jdulemba <josdrw9@gmail.com>
2020-04-21 20:57:14 +0200

    Local coffea environment used so updates can be implemented faster

--- environment.sh
+++ environment.sh
@@ -4,5 +4,6 @@ export NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Tue/x86_64-centos7-gcc8-opt/setup.sh
 export LIBRARY_PATH=$LD_LIBRARY_PATH
 
+export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
 export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled:$LD_LIBRARY_PATH
