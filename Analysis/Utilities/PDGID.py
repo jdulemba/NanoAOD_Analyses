eabc6a5b59fcbe2bb6774f6365270f364e4cb051
jdulemba <josdrw9@gmail.com>
2020-04-21 20:50:35 +0200

    Has pdgid values for particles used for constructing ttbar system

---
+++ Analysis/Utilities/PDGID.py
@@ -0,0 +1,36 @@
+PDGID = {
+    'd' : 1,
+    'u' : 2,
+    's' : 3, 
+    'c' : 4,
+    'b' : 5,
+    't' : 6,
+
+    'dbar' : -1,
+    'ubar' : -2,
+    'sbar' : -3,
+    'cbar' : -4,
+    'bbar' : -5,
+    'tbar' : -6,
+
+    'e' :      11,
+    'nu_e' :   12,
+    'mu' :     13,
+    'nu_mu' :  14,
+    'tau' :    15,
+    'nu_tau' : 16,
+
+    'ebar' :      -11,
+    'nubar_e' :   -12,
+    'mubar' :     -13,
+    'nubar_mu' :  -14,
+    'taubar' :    -15,
+    'nubar_tau' : -16,
+
+    'gluon' : 21,
+    'gamma' : 22,
+    'Z' : 23,
+    'Wplus' : 24,
+    'Wminus' : -24,
+    'H' : 25,
+}
