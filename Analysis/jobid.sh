40bb30f6ba2cea595a2f514351bae98c0efeebdd
jdulemba <josdrw9@gmail.com>
2020-05-01 11:58:24 +0200

    Made new jobid using cutBased electron ID

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -1,4 +1,4 @@
 #export jobid=NanoAODv6
 #export jobid=Testing
-export jobid=jpt30_ljpt0_leppt30
-#export jobid=jpt30_ljpt0_leppt30_NoJER
+#export jobid=jpt30_ljpt0_leppt30
+export jobid=jpt30_ljpt0_leppt30_cutBasedEl
