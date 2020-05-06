e2b69012d0c89924c944e528562a8e6b52fdfe3b
jdulemba <josdrw9@gmail.com>
2020-05-06 21:27:33 +0200

    Table of permutation attributes similar to old permuation is returned

--- Analysis/python/TTGenMatcher.py
+++ Analysis/python/TTGenMatcher.py
@@ -2,7 +2,8 @@ from pdb import set_trace
 import awkward
 from coffea.analysis_objects import JaggedCandidateArray
 import numpy as np
-
+import compiled.pynusolver as pynusolver
+from python.Permutations import make_perm_table
 
 remove_fast = lambda x : x.split('fast_')[-1]
 
@@ -53,37 +54,52 @@ def best_match(gen_hyp=None, jets=None, leptons=None, met=None):
     matched_dict_counts.update({'Lepton' : matched_leps.counts})
     matched_dict_vars.update({'Lepton' : {remove_fast(key) : matched_leps[key].flatten() for key in matched_leps.columns if key != 'p4'}})
     
-    #set_trace()
-
         ## create matched perm objects
     matched_BHad = JaggedCandidateArray.candidatesfromcounts(
         counts= matched_dict_counts['Had_B'],
         **matched_dict_vars['Had_B']
-    )
+    ).pad(1)
     matched_BLep = JaggedCandidateArray.candidatesfromcounts(
         counts= matched_dict_counts['Lep_B'],
         **matched_dict_vars['Lep_B']
-    )
+    ).pad(1)
     matched_WJa = JaggedCandidateArray.candidatesfromcounts(
         counts= matched_dict_counts['First_Had'],
         **matched_dict_vars['First_Had']
-    )
+    ).pad(1)
     matched_WJb = JaggedCandidateArray.candidatesfromcounts(
         counts= matched_dict_counts['Second_Had'],
         **matched_dict_vars['Second_Had']
-    )
+    ).pad(1)
     matched_Lep = JaggedCandidateArray.candidatesfromcounts(
         counts= matched_dict_counts['Lepton'],
         **matched_dict_vars['Lepton']
-    )
+    ).pad(1)
 
-    matched_perm = awkward.Table(
-        BHad  = matched_BHad,
-        BLep  = matched_BLep,
-        WJa   = matched_WJa,
-        WJb   = matched_WJb,
-        Lepton= matched_Lep,
-        MET   = met,
-    )
+        # solve for neutrino
+    nu_array = np.zeros((jets.counts.size, 4), dtype='float64')
+    for idx, blep in enumerate(matched_BLep):
+        if blep.size < 1: continue
+        if blep.size > 1: raise ValueError("More than one matched blep. Investigate")
+
+            # must specify same dtype for all arrays
+        blep_inputs = np.array([blep.p4.x[0], blep.p4.y[0], blep.p4.z[0], blep.p4.energy[0]], dtype='float64')
+        lep_inputs = np.array([matched_Lep[idx].p4.x[0], matched_Lep[idx].p4.y[0], matched_Lep[idx].p4.z[0], matched_Lep[idx].p4.energy[0]], dtype='float64')
+        met_inputs = np.array([met[idx].p4.x[0], met[idx].p4.y[0]], dtype='float64')
+        nu = nu_array[idx]
+        pynusolver.run_nu_solver(lep_inputs, blep_inputs, met_inputs, nu) # input order for nusolver is (lepton, jet, met, nu), returns Nu(px, py, pz, chi2)
+
+    valid_nu = ~((nu_array[:, 3] > 1e20) | (nu_array[:, 3] == 0)) # events that have a solution and matched blep
+    matched_Nu = JaggedCandidateArray.candidatesfromcounts(
+        counts = valid_nu.astype(int),
+        px = nu_array[:, 0][valid_nu],
+        py = nu_array[:, 1][valid_nu],
+        pz = nu_array[:, 2][valid_nu],
+        mass = np.zeros(valid_nu.sum()),
+        chi2 = nu_array[:, 3][valid_nu],
+    ).pad(1)
+
+    matched_perm = make_perm_table(bhad=matched_BHad, blep=matched_BLep, wja=matched_WJa, wjb=matched_WJb, lepton=matched_Lep, met=met, nu=matched_Nu)
+    #set_trace()
 
     return matched_perm
