ef6cf091000ad1b401cadfa6a966ee77642d1045
jdulemba <josdrw9@gmail.com>
2020-05-13 12:04:54 +0200

    New jobid using mvaV2 electron ID

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -1,4 +1,5 @@
 #export jobid=NanoAODv6
 #export jobid=Testing
 #export jobid=jpt30_ljpt0_leppt30
-export jobid=jpt30_ljpt0_leppt30_cutBasedEl
+#export jobid=jpt30_ljpt0_leppt30_cutBasedEl
+export jobid=jpt30_ljpt0_leppt30_mvaEl
