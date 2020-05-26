61fd162ba6d90ecc015c2da7edd9613d3c296bd9
jdulemba <josdrw9@gmail.com>
2020-05-26 12:30:55 +0200

    New jobid with MT and leading jet pT cuts

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -2,4 +2,5 @@
 #export jobid=Testing
 #export jobid=jpt30_ljpt0_leppt30
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
-export jobid=jpt30_ljpt0_leppt30_mvaEl
+#export jobid=jpt30_ljpt0_leppt30_mvaEl
+export jobid=jpt30_ljpt50_MT40_cutBasedEl
