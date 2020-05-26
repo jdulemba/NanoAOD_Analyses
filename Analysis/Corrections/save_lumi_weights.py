e5943e455b6e81751eed00782b6adaedea53a491
jdulemba <josdrw9@gmail.com>
2020-05-26 16:31:31 +0200

    Ignoring ttbar events used for signal generation

--- Analysis/Corrections/save_lumi_weights.py
+++ Analysis/Corrections/save_lumi_weights.py
@@ -29,10 +29,18 @@ lumi_weights = {
 
 # for each year, read nWeightedEvts from all meta.json files
 for year in ['2016', '2017', '2018']:
+    if year == '2016':
+        Nominal_ttJets = ['ttJets_PS', 'ttJets']
+    else:
+        Nominal_ttJets = ['ttJetsSL', 'ttJetsHad', 'ttJetsDiLep']
     xsec_file = prettyjson.loads(open('%s/inputs/samples_%s.json' % (proj_dir, year)).read()) # file with cross sections
     samples = sorted([fname.split('.')[0] for fname in os.listdir('%s/inputs/%s_%s/' % (proj_dir, year, jobid)) if fname.endswith('.meta.json')])
     for sample in samples:
-        nWeightedEvts = prettyjson.loads(open('%s/inputs/%s_%s/%s.meta.json' % (proj_dir, year, jobid, sample)).read())["nWeightedEvts"]
+        if sample in Nominal_ttJets:
+            meta_json = prettyjson.loads(open('%s/inputs/%s_%s/%s.meta.json' % (proj_dir, year, jobid, sample)).read())
+            nWeightedEvts = sum([meta_json["nWeightedEvts_%i" % idx] for idx in range(3, 10)])
+        else:
+            nWeightedEvts = prettyjson.loads(open('%s/inputs/%s_%s/%s.meta.json' % (proj_dir, year, jobid, sample)).read())["nWeightedEvts"]
         xsec = [info['xsection'] for info in xsec_file if info['name'] == sample ][0]
         for lep in ['Electrons', 'Muons']:
             lumi_weights[year][lep][sample] = data_lumi[year][lep]/(nWeightedEvts/xsec)
@@ -40,6 +48,6 @@ for year in ['2016', '2017', '2018']:
     print("%s calculated" % year)
 
     # save files
-mcweights_name = '%s/MC_LumiWeights.coffea' % outdir
+mcweights_name = '%s/MC_LumiWeights_IgnoreSigEvts.coffea' % outdir
 save(lumi_weights, mcweights_name)
 print('\n', mcweights_name, 'written')
