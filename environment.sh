b3d391832153ee9ec410f35a60b20418f43174f9
jdulemba <josdrw9@gmail.com>
2020-06-03 22:38:14 +0200

    Use stable LCG_97 release for python3 instead of nighlies

--- environment.sh
+++ environment.sh
@@ -1,10 +1,11 @@
 # Project-related environment variables
 export NANODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
-source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Tue/x86_64-centos7-gcc8-opt/setup.sh
-#source /cvmfs/sft.cern.ch/lcg/views/dev3python3/Thu/x86_64-centos7-gcc8-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97python3/x86_64-centos7-gcc8-opt/setup.sh
 export LIBRARY_PATH=$LD_LIBRARY_PATH
 
 export PYTHONPATH="$NANODIR/coffea:$PYTHONPATH"
 export PYTHONPATH="$NANODIR/Analysis:$PYTHONPATH"
 export LD_LIBRARY_PATH=$NANODIR/Analysis/compiled:$LD_LIBRARY_PATH
+
+python --version
