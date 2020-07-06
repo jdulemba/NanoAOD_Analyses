5785cb3534130882b7de08106a6dc8da594a87a2
jdulemba <josdrw9@gmail.com>
2020-07-06 14:26:04 +0200

    File with groupings of systematics

---
+++ Analysis/Utilities/systematics.py
@@ -0,0 +1,18 @@
+'''
+List of systematics
+'''
+
+lep_sys = [
+    'Lep_RECOUp', 'Lep_RECODown',
+    'Lep_TRIGUp', 'Lep_TRIGDown'
+]
+
+btag_sys = [
+    'btag_bc_UP', 'btag_bc_DW',
+    'btag_l_UP', 'btag_l_DW',
+]
+
+misc_sys = [
+    'PileupUp', 'PileupDown',
+    'PrefireUp', 'PrefireDown'
+]
