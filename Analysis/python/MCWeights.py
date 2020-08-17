1d84974fc36daa78765dbe8aa5545d3d151e4839
jdulemba <josdrw9@gmail.com>
2020-08-17 10:08:02 +0200

    Added PS and LHE weights for ttJets sample

--- Analysis/python/MCWeights.py
+++ Analysis/python/MCWeights.py
@@ -2,15 +2,16 @@ from pdb import set_trace
 import coffea.processor.dataframe
 import numpy as np
 import coffea.processor as processor
+import Utilities.systematics as systematics
 
-def get_event_weights(df, year: str, corrections, BTagSFs = []):
+def get_event_weights(df, year: str, corrections, BTagSFs=[], isTTbar=False):
     weights = processor.Weights(df.size, storeIndividual=True)# store individual variations
 
         ## only apply to MC
     if not df.dataset.startswith('data_Single'):
             ## Prefire Corrections
         if (year != '2018') and (corrections['Prefire'] == True):
-            weights.add('prefire_weight',
+            weights.add('Prefire',
                 df['L1PreFiringWeight_Nom'],
                 df['L1PreFiringWeight_Up'],
                 df['L1PreFiringWeight_Dn']
@@ -21,52 +22,55 @@ def get_event_weights(df, year: str, corrections, BTagSFs = []):
         genWeights[df.genWeight < 0] = -1.
         genWeights[df.genWeight == 0] = 0.
         weights.add('genweight', genWeights)
-
-            ## Initialize Lepton Scale Factors
-        if 'LeptonSF' in corrections.keys():
-            weights.add('Muon_SF',
-                np.ones(df.genWeight.size),
-                np.ones(df.genWeight.size),
-                np.ones(df.genWeight.size),
-                shift=True # makes up/down variations relative to nominal
-            )
-            weights.add('Electron_SF',
-                np.ones(df.genWeight.size),
-                np.ones(df.genWeight.size),
-                np.ones(df.genWeight.size),
-                shift=True # makes up/down variations relative to nominal
-            )
     
             ## Pileup Reweighting
         if 'Pileup' in corrections.keys():
-            weights.add('pileup_weight',
+            weights.add('Pileup',
                 corrections['Pileup'][year][df['dataset']]['central'](df['Pileup_nTrueInt']),
                 corrections['Pileup'][year][df['dataset']]['up'](df['Pileup_nTrueInt']),
                 corrections['Pileup'][year][df['dataset']]['down'](df['Pileup_nTrueInt'])
             )
     
-            ## BTag SFs
-        if corrections['BTagSF'] == True:
-            if BTagSFs:
-                for btagSF in BTagSFs:
-                    weights.add(btagSF, np.ones(df.genWeight.size))
-            else:
-                weights.add('Btag_SF',
-                    np.ones(df.genWeight.size),
-                    #np.ones(df.genWeight.size),
-                    #np.ones(df.genWeight.size),
-                    #shift=True # makes up/down variations relative to nominal
-                )
-
-            ## Signal Reweighting
-        if 'Signal' in corrections.keys():
-            weights.add('Signal_Reweight',
-                np.zeros(df.size)
+        ## PS and LHE weights for ttbar events
+        if isTTbar:
+            import awkward
+            ## PS Weight variations
+            # PSWeight definitions can be found here: https://cms-nanoaod-integration.web.cern.ch/integration/master/mc94X_doc.html#PSWeight
+            psweights = awkward.JaggedArray.fromcounts(df['nPSWeight'], df['PSWeight'])
+            weights.add('ISR',
+                np.ones(df.size),
+                psweights[:, 2], # (ISR=2, FSR=1)
+                psweights[:, 0], # (ISR=0.5, FSR=1)
+            )
+            weights.add('FSR',
+                np.ones(df.size),
+                psweights[:, 3], # (ISR=1, FSR=2)
+                psweights[:, 1], # (ISR=1, FSR=0.5)
             )
 
-    ## Need to add at some point
             ## LHEScale Weight Variations
-            ## PS Weight variations
+            # LHEScaleWeight definitions can be found here: https://cms-nanoaod-integration.web.cern.ch/integration/master/mc94X_doc.html#LHE
+            lheweights = awkward.JaggedArray.fromcounts(df['nLHEScaleWeight'], df['LHEScaleWeight'])
+            weights.add('FACTOR',
+                np.ones(df.size),
+                lheweights[:, 5], # (muR=1, muF=2)
+                lheweights[:, 3], # (muR=1, muF=0.5)
+            )
+            weights.add('RENORM',
+                np.ones(df.size),
+                lheweights[:, 7], # (muR=2, muF=1)
+                lheweights[:, 1], # (muR=0.5, muF=1)
+            )
+            weights.add('RENORM_FACTOR_SAME',
+                np.ones(df.size),
+                lheweights[:, 8], # (muR=2, muF=2), RENORM_UP_FACTOR_UP
+                lheweights[:, 0], # (muR=0.5, muF=0.5), RENORM_DW_FACTOR_DW
+            )
+            weights.add('RENORM_FACTOR_DIFF',
+                np.ones(df.size),
+                lheweights[:, 6], # (muR=2, muF=0.5), RENORM_UP_FACTOR_DW
+                lheweights[:, 2], # (muR=0.5, muF=2), RENORM_DW_FACTOR_UP
+            )
 
     return weights    
 
@@ -76,34 +80,34 @@ def get_gen_weights(df, shift=None, mask=None):
     gen_weight = df.genWeight[(mask)]
 
         ## LHEScale Weight Variations
-    if shift == 'FACTOR_DW':
-        weight = df['LHEScaleWeight'][3][(mask)] # (muR=1, muF=0.5)
-    elif shift == 'FACTOR_UP':
-        weight = df['LHEScaleWeight'][5][(mask)] # (muR=1, muF=2)
-    if shift == 'RENORM_DW':
-        weight = df['LHEScaleWeight'][1][(mask)] # (muR=0.5, muF=1)
-    elif shift == 'RENORM_UP':
-        weight = df['LHEScaleWeight'][7][(mask)] # (muR=2, muF=1)
-    elif shift == 'RENFACTOR_DW':
-        weight = df['LHEScaleWeight'][0][(mask)] # (muR=0.5, muF=0.5)
-    elif shift == 'RENFACTOR_UP':
-        weight = df['LHEScaleWeight'][8][(mask)] # (muR=2, muF=2)
-    elif shift == 'RENORM_UP_FACTOR_DW':
-        weight = df['LHEScaleWeight'][6][(mask)] # (muR=2, muF=0.5)
-    elif shift == 'RENORM_DW_FACTOR_UP':
-        weight = df['LHEScaleWeight'][2][(mask)] # (muR=0.5, muF=2)
+    #if shift == 'FACTOR_DW':
+    #    weight = df['LHEScaleWeight'][3][(mask)] # (muR=1, muF=0.5)
+    #elif shift == 'FACTOR_UP':
+    #    weight = df['LHEScaleWeight'][5][(mask)] # (muR=1, muF=2)
+    #if shift == 'RENORM_DW':
+    #    weight = df['LHEScaleWeight'][1][(mask)] # (muR=0.5, muF=1)
+    #elif shift == 'RENORM_UP':
+    #    weight = df['LHEScaleWeight'][7][(mask)] # (muR=2, muF=1)
+    #elif shift == 'RENFACTOR_DW':
+    #    weight = df['LHEScaleWeight'][0][(mask)] # (muR=0.5, muF=0.5)
+    #elif shift == 'RENFACTOR_UP':
+    #    weight = df['LHEScaleWeight'][8][(mask)] # (muR=2, muF=2)
+    #elif shift == 'RENORM_UP_FACTOR_DW':
+    #    weight = df['LHEScaleWeight'][6][(mask)] # (muR=2, muF=0.5)
+    #elif shift == 'RENORM_DW_FACTOR_UP':
+    #    weight = df['LHEScaleWeight'][2][(mask)] # (muR=0.5, muF=2)
 
-        ## PS Weight variations
-    elif shift == 'ISR_DW':
-        weight = df['PSWeight'][0][(mask)] # (ISR=0.5, FSR=1)
-    elif shift == 'ISR_UP':
-        weight = df['PSWeight'][2][(mask)] # (ISR=2, FSR=1)
-    if shift == 'FSR_DW':
-        weight = df['PSWeight'][1][(mask)] # (ISR=1, FSR=0.5)
-    elif shift == 'FSR_UP':
-        weight = df['PSWeight'][3][(mask)] # (ISR=1, FSR=2)
-    else: #nominal
-        weight = 1.
+    #    ## PS Weight variations
+    #elif shift == 'ISR_DW':
+    #    weight = df['PSWeight'][0][(mask)] # (ISR=0.5, FSR=1)
+    #elif shift == 'ISR_UP':
+    #    weight = df['PSWeight'][2][(mask)] # (ISR=2, FSR=1)
+    #if shift == 'FSR_DW':
+    #    weight = df['PSWeight'][1][(mask)] # (ISR=1, FSR=0.5)
+    #elif shift == 'FSR_UP':
+    #    weight = df['PSWeight'][3][(mask)] # (ISR=1, FSR=2)
+    #else: #nominal
+    #    weight = 1.
 
     return gen_weight*weight        
 
@@ -132,9 +136,10 @@ def get_toppt_weights(pt1=np.array([-1.]), pt2=np.array([-1.]), shift=None):
     return weight
 
 
-def get_lepton_sf(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.ndarray, shift='Central'):
+def get_comb_lepSF(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.ndarray, shift='Central'):
     if not (shift == 'Central' or shift == 'Error'):
         raise ValueError('Shift value %s not defined' % shift)
+    #set_trace()
     sf_dict = corrections[year][lepton]
     eta_ranges = sf_dict['eta_ranges']
     lepSFs = np.ones(pt.size)
@@ -143,8 +148,39 @@ def get_lepton_sf(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.n
         mask = (eta >= eta_range[0]) & (eta < eta_range[1]) # find inds that are within given eta range
         if not mask.any(): continue # no values fall within eta range
         #set_trace()
-        recoSF = sf_dict['Reco_ID'][shift]['eta_bin%i' % idx]
-        trigSF = sf_dict['Trig'][shift]['eta_bin%i' % idx]
+        recoSF = sf_dict['Reco_ID']['Central']['eta_bin%i' % idx]
+        trigSF = sf_dict['Trig']['Central']['eta_bin%i' % idx]
         lepSFs[mask] = recoSF(pt[mask])*trigSF(pt[mask])
 
     return lepSFs
+
+
+def get_lepton_sf(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.ndarray):
+    #set_trace()
+    sf_dict = corrections[year][lepton]
+    eta_ranges = sf_dict['eta_ranges']
+    reco_cen = np.ones(pt.size)
+    reco_err = np.ones(pt.size)
+    trig_cen = np.ones(pt.size)
+    trig_err = np.ones(pt.size)
+
+    for idx, eta_range in enumerate(eta_ranges):
+        mask = (eta >= eta_range[0]) & (eta < eta_range[1]) # find inds that are within given eta range
+        if not mask.any(): continue # no values fall within eta range
+        recoSF_cen = sf_dict['Reco_ID']['Central']['eta_bin%i' % idx]
+        recoSF_err = sf_dict['Reco_ID']['Error']['eta_bin%i' % idx]
+        trigSF_cen = sf_dict['Trig']['Central']['eta_bin%i' % idx]
+        trigSF_err = sf_dict['Trig']['Error']['eta_bin%i' % idx]
+        reco_cen[mask] = recoSF_cen(pt[mask])
+        reco_err[mask] = recoSF_err(pt[mask])
+        trig_cen[mask] = trigSF_cen(pt[mask])
+        trig_err[mask] = trigSF_err(pt[mask])
+
+    output_SFs = {
+        'RECO_CEN' : reco_cen,
+        'RECO_ERR' : reco_err,
+        'TRIG_CEN' : trig_cen,
+        'TRIG_ERR' : trig_err,
+    }
+
+    return output_SFs
