b4509656e909ba1b2d498714772a030cbd88769d
jdulemba <josdrw9@gmail.com>
2020-10-30 15:49:42 +0100

    Added new jobid for 2017 nanoAODv7 testing

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -3,4 +3,5 @@
 #export jobid=jpt30_ljpt0_leppt30
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
 #export jobid=jpt30_ljpt0_leppt30_mvaEl
-export jobid=jpt30_ljpt50_MT40_cutBasedEl
+#export jobid=jpt30_ljpt50_MT40_cutBasedEl
+export jobid=NanoAODv7
