d81c2a6eb266778b8356f5ed1dbaf2a058dd1f65
jdulemba <josdrw9@gmail.com>
2020-11-03 18:18:51 +0100

    Added ULnanoAOD_jpt30_ljpt50_MT40_cutBasedEl jobid

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -4,4 +4,5 @@
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
 #export jobid=jpt30_ljpt0_leppt30_mvaEl
 #export jobid=jpt30_ljpt50_MT40_cutBasedEl
-export jobid=UL_NanoAOD
+#export jobid=UL_NanoAOD
+export jobid=ULnanoAOD_jpt30_ljpt50_MT40_cutBasedEl
