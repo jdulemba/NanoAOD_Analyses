68805685599158491f8cddc04fe32dd2debf6d7f
jdulemba <josdrw9@gmail.com>
2020-11-25 13:43:07 +0100

    New jobid for testing new gen particle selection using NanoAODv6

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -3,6 +3,7 @@
 #export jobid=jpt30_ljpt0_leppt30
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
 #export jobid=jpt30_ljpt0_leppt30_mvaEl
-export jobid=jpt30_ljpt50_MT40_cutBasedEl
+#export jobid=jpt30_ljpt50_MT40_cutBasedEl
 #export jobid=UL_NanoAOD
 #export jobid=ULnanoAOD_jpt30_ljpt50_MT40_cutBasedEl
+jobid=NanoAODv6_Testing
