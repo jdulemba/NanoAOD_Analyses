2520e8d4adbba7de1c0951b114490caee7fede7b
jdulemba <josdrw9@gmail.com>
2020-12-14 12:24:44 +0100

    Created new jobid for running analysis with jet corrections for MC and data

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -3,7 +3,8 @@
 #export jobid=jpt30_ljpt0_leppt30
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
 #export jobid=jpt30_ljpt0_leppt30_mvaEl
-export jobid=NanoAODv6_jpt30_ljpt50_MT40_cutBasedEl
+#export jobid=NanoAODv6_jpt30_ljpt50_MT40_cutBasedEl
+export jobid=NanoAODv6_newJCs
 #export jobid=ULnanoAOD
 #export jobid=ULnanoAOD_jpt30_ljpt50_MT40_cutBasedEl
 #jobid=NanoAODv6_Testing
