b755ebd7b867e898cf4cc53b392395e33f78c1d9
jdulemba <josdrw9@gmail.com>
2021-02-03 20:52:14 +0100

    Added base_jobid var, alpha axis limits lowered to 5

--- Analysis/bin/ttbar_alpha_reco.py
+++ Analysis/bin/ttbar_alpha_reco.py
@@ -18,12 +18,13 @@ from python.Permutations import compare_matched_best_perms
 
 proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
+base_jobid = os.environ['base_jobid']
 analyzer = 'ttbar_alpha_reco'
 
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument('fset', type=str, help='Fileset dictionary (in string form) to be used for the processor')
-parser.add_argument('year', choices=['2016', '2017', '2018'], help='Specify which year to run over')
+parser.add_argument('year', choices=['2016APV', '2016', '2017', '2018'] if base_jobid == 'ULnanoAOD' else ['2016', '2017', '2018'], help='Specify which year to run over')
 parser.add_argument('outfname', type=str, help='Specify output filename, including directory and file extension')
 parser.add_argument('--debug', action='store_true', help='Uses iterative_executor for debugging purposes, otherwise futures_excutor will be used (faster)')
 
@@ -34,31 +35,32 @@ fdict = (args.fset).replace("\'", "\"")
 fileset = prettyjson.loads(fdict)
 
     ## specify ttJets samples
-if args.year == '2016':
-    Nominal_ttJets = ['ttJets_PS', 'ttJets']
+if (args.year == '2016') and (base_jobid == 'NanoAODv6'):
+    Nominal_ttJets = ['ttJets_PS']
+#if args.year == '2016':
+#    Nominal_ttJets = ['ttJets_PS', 'ttJets']
 else:
-    Nominal_ttJets = ['ttJetsSL', 'ttJetsHad', 'ttJetsDiLep']
+    Nominal_ttJets = ['ttJetsSL']#, 'ttJetsHad', 'ttJetsDiLep']
 if not Nominal_ttJets:
     raise ValueError("This should only be run on nominal ttbar events!")
 
-init_btag = ~(np.array([key.startswith('data') for key in fileset.keys()]).all())
 
 ## init tt probs for likelihoods
 ttpermutator.year_to_run(year=args.year)
 
 ## load corrections for event weights
-pu_correction = load('%s/Corrections/%s/MC_PU_Weights.coffea' % (proj_dir, jobid))
-lepSF_correction = load('%s/Corrections/leptonSFs.coffea' % proj_dir)
-jet_corrections = load('%s/Corrections/JetCorrections.coffea' % proj_dir)[args.year]
+pu_correction = load(os.path.join(proj_dir, 'Corrections', jobid, 'MC_PU_Weights.coffea'))
+lepSF_correction = load(os.path.join(proj_dir, 'Corrections', jobid, 'leptonSFs.coffea'))
+jet_corrections = load(os.path.join(proj_dir, 'Corrections', jobid, 'JetCorrections.coffea'))[args.year]
 corrections = {
     'Pileup' : pu_correction,
     'Prefire' : True,
     'LeptonSF' : lepSF_correction,
-    'BTagSF' : init_btag,
+    'BTagSF' : True,
     'JetCor' : jet_corrections,
 }
 
-cfg_pars = prettyjson.loads(open('%s/cfg_files/cfg_pars_%s.json' % (proj_dir, jobid)).read())
+cfg_pars = prettyjson.loads(open(os.path.join(proj_dir, 'cfg_files', 'cfg_pars_%s.json' % jobid)).read())
     ## parameters for b-tagging
 jet_pars = cfg_pars['Jets']
 btaggers = ['DeepCSV']
@@ -69,7 +71,7 @@ if not( len(wps_to_use) == 1):
 btag_wps = ['DeepCSVMedium']
 
 if corrections['BTagSF'] == True:
-    sf_file = '%s/Corrections/%s/%s' % (proj_dir, jobid, jet_pars['btagging']['btagSF_file'])
+    sf_file = os.path.join(proj_dir, 'Corrections', jobid, jet_pars['btagging']['btagSF_file'])
     if not os.path.isfile(sf_file):
         raise IOError("BTag SF file %s doesn't exist" % sf_file)
 
@@ -84,13 +86,14 @@ if corrections['BTagSF'] == True:
 MTcut = jet_pars['MT']
 
 
-# 0 == '' (no gen matching), 1 == 'right', 2 == 'matchable', 3 == 'unmatchable', 4 == 'other' (not semilep)
+# 0 == '' (no gen matching), 1 == 'right', 2 == 'matchable', 3 == 'unmatchable', 4 == 'sl_tau', 5 == 'other' (not semilep)
 perm_cats = {
     0 : '',
     1 : 'right',
     2 : 'matchable',
     3 : 'unmatchable',
-    4 : 'other'
+    4 : 'sl_tau',
+    5 : 'other',
 }
 
 
@@ -103,8 +106,10 @@ class ttbar_alpha_reco(processor.ProcessorABC):
         self.dataset_axis = hist.Cat("dataset", "Event Process")
         self.leptype_axis = hist.Cat("leptype", "Lepton Type")
         self.bp_mtt_axis  = hist.Bin("bp_mtt", "m($t\overline{t}$)", 36, 200., 2000.)
-        self.alpha_P_axis   = hist.Bin("alpha_p", "Gen/Reco P($t_{h}$)", 500, 0., 10.)
-        self.alpha_E_axis   = hist.Bin("alpha_e", "Gen/Reco E($t_{h}$)", 500, 0., 10.)
+        #self.alpha_P_axis   = hist.Bin("alpha_p", "Gen/Reco P($t_{h}$)", 500, 0., 10.)
+        #self.alpha_E_axis   = hist.Bin("alpha_e", "Gen/Reco E($t_{h}$)", 500, 0., 10.)
+        self.alpha_P_axis   = hist.Bin("alpha_p", "Gen/Reco P($t_{h}$)", 1000, 0., 5.)
+        self.alpha_E_axis   = hist.Bin("alpha_e", "Gen/Reco E($t_{h}$)", 1000, 0., 5.)
         self.norm_mthad_axis= hist.Bin("norm_mthad", "172.5/m($t_{had}$)", 500, 0., 5.)
 
             ## make dictionary of hists
@@ -140,7 +145,8 @@ class ttbar_alpha_reco(processor.ProcessorABC):
 
             ## make event weights
                 # data or MC distinction made internally
-        evt_weights = MCWeights.get_event_weights(df, year=args.year, corrections=self.corrections, BTagSFs=btaggers)
+        mu_evt_weights = MCWeights.get_event_weights(df, year=args.year, corrections=self.corrections, BTagSFs=btaggers)
+        el_evt_weights = MCWeights.get_event_weights(df, year=args.year, corrections=self.corrections, BTagSFs=btaggers)
 
             ## initialize selections and regions
         selection = processor.PackedSelection()
@@ -159,6 +165,9 @@ class ttbar_alpha_reco(processor.ProcessorABC):
         selection.add('objselection', objsel_evts)
         selection.add('btag_pass', df['Jet'][btag_wps[0]].sum() >= 2)
 
+            # sort jets by btag value
+        df['Jet'] = df['Jet'][df['Jet']['btagDeepB'].argsort(ascending=False)] if btagger == 'DeepCSV' else df['Jet'][df['Jet']['btagDeepFlavB'].argsort(ascending=False)]
+
             ## add different selections
                 ## muons
         selection.add('tight_MU', df['Muon']['TIGHTMU'].sum() == 1) # one muon passing TIGHT criteria
@@ -171,34 +180,54 @@ class ttbar_alpha_reco(processor.ProcessorABC):
         if 'LeptonSF' in corrections.keys():
             tight_mu_cut = selection.require(objselection=True, tight_MU=True) # find events passing muon object selection with one tight muon
             tight_muons = df['Muon'][tight_mu_cut][(df['Muon'][tight_mu_cut]['TIGHTMU'] == True)]
-            evt_weights._weights['Muon_SF'][tight_mu_cut] = MCWeights.get_lepton_sf(year=args.year, lepton='Muons', corrections=lepSF_correction,
+            muSFs_dict =  MCWeights.get_lepton_sf(year=args.year, lepton='Muons', corrections=lepSF_correction,
                 pt=tight_muons.pt.flatten(), eta=tight_muons.eta.flatten())
+            mu_reco_cen = np.ones(df.size)
+            mu_reco_err = np.zeros(df.size)
+            mu_trig_cen = np.ones(df.size)
+            mu_trig_err = np.zeros(df.size)
+            mu_reco_cen[tight_mu_cut] = muSFs_dict['RECO_CEN']
+            mu_reco_err[tight_mu_cut] = muSFs_dict['RECO_ERR']
+            mu_trig_cen[tight_mu_cut] = muSFs_dict['TRIG_CEN']
+            mu_trig_err[tight_mu_cut] = muSFs_dict['TRIG_ERR']
+            mu_evt_weights.add('Lep_RECO', mu_reco_cen, mu_reco_err, mu_reco_err, shift=True)
+            mu_evt_weights.add('Lep_TRIG', mu_trig_cen, mu_trig_err, mu_trig_err, shift=True)
+
             tight_el_cut = selection.require(objselection=True, tight_EL=True) # find events passing electron object selection with one tight electron
             tight_electrons = df['Electron'][tight_el_cut][(df['Electron'][tight_el_cut]['TIGHTEL'] == True)]
-            evt_weights._weights['Electron_SF'][tight_el_cut] = MCWeights.get_lepton_sf(year=args.year, lepton='Electrons', corrections=lepSF_correction,
+            elSFs_dict = MCWeights.get_lepton_sf(year=args.year, lepton='Electrons', corrections=lepSF_correction,
                 pt=tight_electrons.pt.flatten(), eta=tight_electrons.etaSC.flatten())
+            el_reco_cen = np.ones(df.size)
+            el_reco_err = np.zeros(df.size)
+            el_trig_cen = np.ones(df.size)
+            el_trig_err = np.zeros(df.size)
+            el_reco_cen[tight_el_cut] = elSFs_dict['RECO_CEN']
+            el_reco_err[tight_el_cut] = elSFs_dict['RECO_ERR']
+            el_trig_cen[tight_el_cut] = elSFs_dict['TRIG_CEN']
+            el_trig_err[tight_el_cut] = elSFs_dict['TRIG_ERR']
+            el_evt_weights.add('Lep_RECO', el_reco_cen, el_reco_err, el_reco_err, shift=True)
+            el_evt_weights.add('Lep_TRIG', el_trig_cen, el_trig_err, el_trig_err, shift=True)
 
         if corrections['BTagSF'] == True:
+            #set_trace()
+            deepcsv_cen = np.ones(df.size)
             threeJets_cut = selection.require(objselection=True, jets_3=True)
             deepcsv_3j_wts = self.corrections['BTag_Constructors']['DeepCSV']['3Jets'].get_scale_factor(jets=df['Jet'][threeJets_cut], passing_cut='DeepCSV'+wps_to_use[0])
-            evt_weights._weights['DeepCSV'][threeJets_cut] = deepcsv_3j_wts['central'].prod()
-            #set_trace()
+            deepcsv_cen[threeJets_cut] = deepcsv_3j_wts['central'].prod()
+        
+                # make dict of btag weights
+            btag_weights = {
+                'DeepCSV_CEN' : deepcsv_cen,
+            }
 
-        if self.sample_name in Nominal_ttJets:
-            events = df.event
-            selection.add('keep_ttbar', ~np.stack([((events % 10) == idx) for idx in [0, 1, 2]], axis=1).any(axis=1))
-            for lepton in regions.keys():
-                sel = regions[lepton]
-                sel.update({'keep_ttbar'})
 
             # find gen level particles for ttbar system
-        genp_mode = 'NORMAL'
-        GenTTbar = genpsel.select(df, systype='FINAL', mode=genp_mode)
-        #genpsel.select(df, mode='LHE')
+        GenTTbar = genpsel.select(df, mode='NORMAL')
         selection.add('semilep', GenTTbar['SL']['TTbar'].counts > 0)
 
         ## fill hists for each region
         for lepton in regions.keys():
+            evt_weights = mu_evt_weights if lepton == 'Muon' else el_evt_weights
             cut = selection.all(*regions[lepton])
 
             #set_trace()
@@ -213,27 +242,29 @@ class ttbar_alpha_reco(processor.ProcessorABC):
                 else:
                     raise ValueError("Not sure what lepton type to choose for event")
 
+                #set_trace()
+                    # find matched permutations
                 mp = ttmatcher.best_match(gen_hyp=GenTTbar[cut], jets=df['Jet'][cut], leptons=df[lepton][cut][lep_mask], met=df['MET'][cut])
 
                     # find best permutations
                 best_perms = ttpermutator.find_best_permutations(jets=df['Jet'][cut], leptons=df[lepton][cut][lep_mask], MET=df['MET'][cut], btagWP=btag_wps[0])
                 valid_perms = best_perms['TTbar'].counts > 0
 
-                bp_status = np.zeros(cut.size, dtype=int) # 0 == '' (no gen matching), 1 == 'right', 2 == 'matchable', 3 == 'unmatchable', 4 == 'noslep'
+                    # compare matched per to best perm
+                bp_status = np.zeros(cut.size, dtype=int) # 0 == '' (no gen matching), 1 == 'right', 2 == 'matchable', 3 == 'unmatchable', 4 == 'sl_tau', 5 == 'noslep'
                 perm_cat_array = compare_matched_best_perms(mp, best_perms, njets='3Jets')
                 bp_status[(cut)] = perm_cat_array
+                sl_tau_evts = (np.abs(GenTTbar['SL']['Lepton'].pdgId) == 15).pad(1).fillna(False).flatten() # fillna to make sure array is same size as df.size
+                bp_status[sl_tau_evts] = 4
 
                     ## create MT regions
                 MT = make_vars.MT(df[lepton][cut][lep_mask], df['MET'][cut])
                 MTHigh = (MT[valid_perms] >= MTcut).flatten()
 
-                evt_weights_to_use = evt_weights.weight()
-                ## apply lepton SFs to MC (only applicable to tight leptons)
-                if 'LeptonSF' in corrections.keys():
-                    evt_weights_to_use = evt_weights.partial_weight(exclude=['Electron_SF']) if lepton == 'Muon' else evt_weights.partial_weight(exclude=['Muon_SF']) # exclude SF from other lepton
+                evt_weights_to_use = (evt_weights.weight()*btag_weights['%s_CEN' % btaggers[0]])[cut][valid_perms][MTHigh]
 
                 #set_trace()
-                output = self.make_3j_categories(acc=output, leptype=lepton, permarray=bp_status[cut][valid_perms][MTHigh], genttbar=GenTTbar[cut][valid_perms][MTHigh], bp=best_perms, mtcut=MTHigh, evt_weights=evt_weights_to_use[cut][valid_perms][MTHigh])
+                output = self.make_3j_categories(acc=output, leptype=lepton, permarray=bp_status[cut][valid_perms][MTHigh], genttbar=GenTTbar[cut][valid_perms][MTHigh], bp=best_perms, mtcut=MTHigh, evt_weights=evt_weights_to_use)
 
         return output
 
@@ -251,6 +282,7 @@ class ttbar_alpha_reco(processor.ProcessorABC):
 
         for permval in np.unique(permarray).tolist():
             #set_trace()
+            if permval != 1: continue # only fill hists for 'right' events
             perm_inds = np.where(permarray == permval)
             dataset_name = '%s_%s' % (self.sample_name, perm_cats[permval]) if permval != 0 else self.sample_name
 
