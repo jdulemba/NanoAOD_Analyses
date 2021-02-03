8e40939be86f511f549d30fdb6386f8351d7b765
jdulemba <josdrw9@gmail.com>
2021-02-03 20:55:18 +0100

    Added more jobids for investigating nnlo reweighting

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -3,8 +3,11 @@
 #export jobid=jpt30_ljpt0_leppt30
 #export jobid=jpt30_ljpt0_leppt30_cutBasedEl
 #export jobid=jpt30_ljpt0_leppt30_mvaEl
-#export jobid=NanoAODv6_jpt30_ljpt50_MT40_cutBasedEl
-export jobid=NanoAODv6_newJCs
+#export jobid=NanoAODv6_newJCs
 #export jobid=ULnanoAOD
 #export jobid=ULnanoAOD_jpt30_ljpt50_MT40_cutBasedEl
 #jobid=NanoAODv6_Testing
+export jobid=NanoAODv6_jpt30_ljpt50_MT40_cutBasedEl
+#export jobid=NanoAODv6_pt_thad_reweighting
+#export jobid=NanoAODv6_mtt_thadctstar_reweighting
+#export jobid=NanoAODv6_mtt_thadctstar_Interp_reweighting
