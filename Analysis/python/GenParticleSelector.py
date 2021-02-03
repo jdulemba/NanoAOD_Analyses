b80048defcc2ebe83af91c9ffae98a6b77e9d1f4
jdulemba <josdrw9@gmail.com>
2021-02-03 21:04:06 +0100

    Gen W objects are now gen objects instead of constructed from wpartons

--- Analysis/python/GenParticleSelector.py
+++ Analysis/python/GenParticleSelector.py
@@ -4,8 +4,24 @@ import coffea.processor as processor
 import awkward
 import python.GenObjects as genobj
 from coffea.analysis_objects import JaggedCandidateArray
+import numba
 
 remove_fast = lambda x : x.split('fast_')[-1]
+
+@numba.njit
+def find_Wdecay_prod_inds(wminus_idx_array, wplus_idx_array, gp_offsets, gp_momInds, wdecay_prod_momIdx_array):
+    ## finds particle Idx of first W boson for input W decay products
+    wdecay_firstW_idx = np.zeros(wdecay_prod_momIdx_array.size)
+
+    for wdecay_idx in range(wdecay_prod_momIdx_array.size):
+        wdecay_momIdx = wdecay_prod_momIdx_array[wdecay_idx]
+        while (wdecay_momIdx != wplus_idx_array[wdecay_idx]) and (wdecay_momIdx != wminus_idx_array[wdecay_idx]):
+            wdecay_momIdx = gp_momInds[gp_offsets[wdecay_idx]+wdecay_momIdx]
+        wdecay_firstW_idx[wdecay_idx] = wdecay_momIdx
+    
+    return wdecay_firstW_idx
+
+
 
 def process_genParts(df):
     genParts = JaggedCandidateArray.candidatesfromcounts(
@@ -102,6 +118,45 @@ def select_normal(df, w_decay_momid):
     gen_taus = hard_gps[(abspdg == 15) & (hard_gps.mompdgId == sgn * -24)]
     gen_nu_taus = hard_gps[(abspdg == 16) & (hard_gps.mompdgId == sgn * 24)]
 
+    # find Idx of first W boson for up/dw partons and leptons for reconstructing gen W bosons
+        # wparton up
+    Wminus_WPup_idx_array = np.repeat(gen_wminus.Idx.content, gen_wpartons_up.counts)
+    Wplus_WPup_idx_array = np.repeat(gen_wplus.Idx.content, gen_wpartons_up.counts)
+    gp_offsets_WPup_array = np.repeat(genparts.offsets[:-1], gen_wpartons_up.counts)
+    wpartons_up_momIdx_array = gen_wpartons_up.momIdx.content
+
+        # wparton dw
+    Wminus_WPdw_idx_array = np.repeat(gen_wminus.Idx.content, gen_wpartons_dw.counts)
+    Wplus_WPdw_idx_array = np.repeat(gen_wplus.Idx.content, gen_wpartons_dw.counts)
+    gp_offsets_WPdw_array = np.repeat(genparts.offsets[:-1], gen_wpartons_dw.counts)
+    wpartons_dw_momIdx_array = gen_wpartons_dw.momIdx.content
+
+        # charged lep
+    Wminus_ChLep_idx_array = np.repeat(gen_wminus.Idx.content, gen_charged_leps.counts)
+    Wplus_ChLep_idx_array = np.repeat(gen_wplus.Idx.content, gen_charged_leps.counts)
+    gp_offsets_ChLep_array = np.repeat(genparts.offsets[:-1], gen_charged_leps.counts)
+    ch_lep_momIdx_array = gen_charged_leps.momIdx.content
+
+        # neutral lep
+    Wminus_NuLep_idx_array = np.repeat(gen_wminus.Idx.content, gen_neutral_leps.counts)
+    Wplus_NuLep_idx_array = np.repeat(gen_wplus.Idx.content, gen_neutral_leps.counts)
+    gp_offsets_NuLep_array = np.repeat(genparts.offsets[:-1], gen_neutral_leps.counts)
+    nu_lep_momIdx_array = gen_neutral_leps.momIdx.content
+
+    gp_momInds_array = genparts.momIdx.content
+    
+    #set_trace()
+    wpart_up_firstW_idx = find_Wdecay_prod_inds(Wminus_WPup_idx_array, Wplus_WPup_idx_array, gp_offsets_WPup_array, gp_momInds_array, wpartons_up_momIdx_array)
+    wpart_dw_firstW_idx = find_Wdecay_prod_inds(Wminus_WPdw_idx_array, Wplus_WPdw_idx_array, gp_offsets_WPdw_array, gp_momInds_array, wpartons_dw_momIdx_array)
+    charged_lep_firstW_idx = find_Wdecay_prod_inds(Wminus_ChLep_idx_array, Wplus_ChLep_idx_array, gp_offsets_ChLep_array, gp_momInds_array, ch_lep_momIdx_array)
+    neutral_lep_firstW_idx = find_Wdecay_prod_inds(Wminus_NuLep_idx_array, Wplus_NuLep_idx_array, gp_offsets_NuLep_array, gp_momInds_array, nu_lep_momIdx_array)
+
+    if not np.array_equal(wpart_up_firstW_idx, wpart_dw_firstW_idx):
+        raise ValueError("Wpartons not matched to same W!")
+    if not np.array_equal(charged_lep_firstW_idx, neutral_lep_firstW_idx):
+        raise ValueError("Leptons not matched to same W!")
+
+
 
             # get direct tau decay products from hard processes (subset of gen_taus events above)
     isDirectHardProcessTauDecayProduct = genparts.statusFlags >> 10 & 1 == 1
@@ -112,7 +167,6 @@ def select_normal(df, w_decay_momid):
     tau_TO_charged_lep = tau_decay_prods[(np.abs(tau_decay_prods.pdgId) == 11) | (np.abs(tau_decay_prods.pdgId) == 13)]
     tau_TO_neutral_lep = tau_decay_prods[(np.abs(tau_decay_prods.pdgId) == 12) | (np.abs(tau_decay_prods.pdgId) == 14)]
 
-    #set_trace()
         # set decaytype for gen taus
     charged_lep_decaytype_array = gen_charged_leps.decaytype.content
 
@@ -172,8 +226,6 @@ def select_normal(df, w_decay_momid):
     charged_lep_decaytype_array[dl_evts_mask] = dilep_decaytype_array.flatten()
 
 
-
-
     # reconstruct gen objects with new charges
         # tops
     gen_tops_dict = {remove_fast(key) : gen_tops[key].flatten() for key in gen_tops.columns if key != 'p4'}
@@ -188,6 +240,20 @@ def select_normal(df, w_decay_momid):
     gen_tbars = JaggedCandidateArray.candidatesfromcounts(
         counts = gen_tbars.counts,
         **gen_tbars_dict
+    )
+        # wplus
+    gen_wplus_dict = {remove_fast(key) : gen_wplus[key].flatten() for key in gen_wplus.columns if key != 'p4'}
+    gen_wplus_dict['charge'] = (gen_wplus.charge.ones_like()).flatten()
+    gen_wplus = JaggedCandidateArray.candidatesfromcounts(
+        counts = gen_wplus.counts,
+        **gen_wplus_dict
+    )
+        # wminus
+    gen_wminus_dict = {remove_fast(key) : gen_wminus[key].flatten() for key in gen_wminus.columns if key != 'p4'}
+    gen_wminus_dict['charge'] = (gen_wminus.charge.ones_like()*(-1.)).flatten()
+    gen_wminus = JaggedCandidateArray.candidatesfromcounts(
+        counts = gen_wminus.counts,
+        **gen_wminus_dict
     )
         # bs
     gen_bs_dict = {remove_fast(key) : gen_bs[key].flatten() for key in gen_bs.columns if key != 'p4'}
@@ -207,6 +273,7 @@ def select_normal(df, w_decay_momid):
         # wpartons up
     gen_wpartons_up_dict = {remove_fast(key) : gen_wpartons_up[key].flatten() for key in gen_wpartons_up.columns if key != 'p4'}
     gen_wpartons_up_dict['charge'] = (sgn[(np.mod(hard_gps.pdgId, 2) == 0) & (abspdg < 6) & (hard_gps.mompdgId == sgn * 24)]*(2./3.)).flatten()
+    gen_wpartons_up_dict['firstW_momIdx'] = wpart_up_firstW_idx
     gen_wpartons_up = JaggedCandidateArray.candidatesfromcounts(
         counts = gen_wpartons_up.counts,
         **gen_wpartons_up_dict
@@ -215,6 +282,7 @@ def select_normal(df, w_decay_momid):
         # wpartons dw
     gen_wpartons_dw_dict = {remove_fast(key) : gen_wpartons_dw[key].flatten() for key in gen_wpartons_dw.columns if key != 'p4'}
     gen_wpartons_dw_dict['charge'] = (sgn[(np.mod(hard_gps.pdgId, 2) == 1) & (abspdg < 6) & (hard_gps.mompdgId == sgn * -24)]*(-1./3.)).flatten()
+    gen_wpartons_dw_dict['firstW_momIdx'] = wpart_dw_firstW_idx
     gen_wpartons_dw = JaggedCandidateArray.candidatesfromcounts(
         counts = gen_wpartons_dw.counts,
         **gen_wpartons_dw_dict
@@ -223,6 +291,7 @@ def select_normal(df, w_decay_momid):
         # neutral leps
     gen_neutral_leps_dict = {remove_fast(key) : gen_neutral_leps[key].flatten() for key in gen_neutral_leps.columns if key != 'p4'}
     gen_neutral_leps_dict['charge'] = (gen_neutral_leps.charge.zeros_like()).flatten()
+    gen_neutral_leps_dict['firstW_momIdx'] = neutral_lep_firstW_idx
     gen_neutral_leps = JaggedCandidateArray.candidatesfromcounts(
         counts = gen_neutral_leps.counts,
         **gen_neutral_leps_dict
@@ -232,20 +301,83 @@ def select_normal(df, w_decay_momid):
     gen_charged_leps_dict = {remove_fast(key) : gen_charged_leps[key].flatten() for key in gen_charged_leps.columns if key != 'p4'}
     gen_charged_leps_dict['decaytype'] = charged_lep_decaytype_array
     gen_charged_leps_dict['charge'] = (sgn[( (abspdg == 11) | (abspdg == 13) | (abspdg == 15) ) & (hard_gps.mompdgId == sgn * -24)]* -1).flatten()
+    gen_charged_leps_dict['firstW_momIdx'] = charged_lep_firstW_idx
     gen_charged_leps = JaggedCandidateArray.candidatesfromcounts(
         counts = gen_charged_leps.counts,
         **gen_charged_leps_dict
     )
 
+        # jagged array of (wplus, wminus)
+        # initialize variables to become attributes of GenW objecs
+    genWpt       = np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+    genWeta      = np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+    genWphi      = np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+    genWmass     = np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+    genWcharge   =np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+    genWdecaytype=np.zeros(gen_wplus.counts.sum()+gen_wminus.counts.sum())
+
+    wplus_inds = np.arange(genWpt.size)[(np.mod(np.arange(genWpt.size), 2) == 0)] # wplus are defined as first W
+    wminus_inds = np.arange(genWpt.size)[(np.mod(np.arange(genWpt.size), 2) == 1)] # wminus are defined as second W
+        # wplus
+    genWpt[wplus_inds] = gen_wplus.pt.flatten()
+    genWeta[wplus_inds] = gen_wplus.eta.flatten()
+    genWphi[wplus_inds] = gen_wplus.phi.flatten()
+    genWmass[wplus_inds] = gen_wplus.mass.flatten()
+    genWcharge[wplus_inds] = gen_wplus.charge.flatten()
+    #set_trace()
+            ## set decaytype
+    wplus_decaytype = np.zeros(gen_wplus.pt.content.size)
+                # SL events
+    SL_hadWplus = np.where(gen_wplus[semilep_evts].Idx.flatten() == gen_wpartons_up[semilep_evts].firstW_momIdx.flatten())[0]
+    SL_lepWplus = np.where(gen_wplus[semilep_evts].Idx.flatten() == gen_charged_leps[semilep_evts].firstW_momIdx.flatten())[0]
+    SL_wplus_decaytype = np.zeros(semilep_evts.sum())
+    SL_wplus_decaytype[SL_hadWplus] = np.ones(SL_hadWplus.size)*2 # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    SL_wplus_decaytype[SL_lepWplus] = np.ones(SL_lepWplus.size) # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    wplus_decaytype[semilep_evts] = SL_wplus_decaytype
+                # DiLep events
+    wplus_decaytype[dilep_evts] = np.ones(dilep_evts.sum()) # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+                # Had events
+    wplus_decaytype[~(dilep_evts | semilep_evts)] = np.ones((~(dilep_evts | semilep_evts)).sum())*2 # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    genWdecaytype[wplus_inds] = wplus_decaytype
+        # wminus
+    genWpt[wminus_inds] = gen_wminus.pt.flatten()
+    genWeta[wminus_inds] = gen_wminus.eta.flatten()
+    genWphi[wminus_inds] = gen_wminus.phi.flatten()
+    genWmass[wminus_inds] = gen_wminus.mass.flatten()
+    genWcharge[wminus_inds] = gen_wminus.charge.flatten()
+            ## set decaytype
+    wminus_decaytype = np.zeros(gen_wminus.pt.content.size)
+                # SL events
+    SL_hadWminus = np.where(gen_wminus[semilep_evts].Idx.flatten() == gen_wpartons_up[semilep_evts].firstW_momIdx.flatten())[0]
+    SL_lepWminus = np.where(gen_wminus[semilep_evts].Idx.flatten() == gen_charged_leps[semilep_evts].firstW_momIdx.flatten())[0]
+    SL_wminus_decaytype = np.zeros(semilep_evts.sum())
+    SL_wminus_decaytype[SL_hadWminus] = np.ones(SL_hadWminus.size)*2 # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    SL_wminus_decaytype[SL_lepWminus] = np.ones(SL_lepWminus.size) # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    wminus_decaytype[semilep_evts] = SL_wminus_decaytype
+                # DiLep events
+    wminus_decaytype[dilep_evts] = np.ones(dilep_evts.sum()) # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+                # Had events
+    wminus_decaytype[~(dilep_evts | semilep_evts)] = np.ones((~(dilep_evts | semilep_evts)).sum())*2 # 0 is INVALID (shouldn't happen), 1 is LEPTONIC, 2 is HADRONIC
+    genWdecaytype[wminus_inds] = wminus_decaytype
+
+    Gen_W = JaggedCandidateArray.candidatesfromcounts(
+        counts=gen_wplus.counts+gen_wminus.counts,
+        pt       = genWpt,
+        eta      = genWeta,
+        phi      = genWphi,
+        mass     = genWmass,
+        charge   = genWcharge,
+        decaytype= genWdecaytype,
+    )
 
 
     #set_trace()
-    #ttbar = genobj.from_collections(wpartons_up=gen_wpartons_up, wpartons_dw=gen_wpartons_dw, charged_leps=gen_els, neutral_leps=gen_el_nus, bs=gen_bs, bbars=gen_bbars, tops=gen_tops, tbars=gen_tbars)
     GenObjects = awkward.Table(
-        GenTop = gen_tops,
-        GenTbar = gen_tbars,
-        GenB = gen_bs,
-        GenBbar = gen_bbars,
+        GenTop      = gen_tops,
+        GenTbar     = gen_tbars,
+        GenW        = Gen_W,
+        GenB        = gen_bs,
+        GenBbar     = gen_bbars,
         GenWPartsUp = gen_wpartons_up,
         GenWPartsDw = gen_wpartons_dw,
         ChargedLeps = gen_charged_leps,
@@ -255,7 +387,6 @@ def select_normal(df, w_decay_momid):
     return GenObjects
 
 def select(df, mode='NORMAL'):
-#def select(df, systype='', mode='NORMAL'):
     modes_to_choose = {
         'NORMAL' : select_normal,
         'LHE' : select_lhe,
@@ -268,7 +399,8 @@ def select(df, mode='NORMAL'):
 
     GenObjs = modes_to_choose[mode](df, w_decay_momid)
 
-    ttbar = genobj.from_collections(wpartons_up=GenObjs['GenWPartsUp'], wpartons_dw=GenObjs['GenWPartsDw'], charged_leps=GenObjs['ChargedLeps'], neutral_leps=GenObjs['NeutralLeps'], bs=GenObjs['GenB'], bbars=GenObjs['GenBbar'], tops=GenObjs['GenTop'], tbars=GenObjs['GenTbar'])
+    ttbar = genobj.from_collections(wpartons_up=GenObjs['GenWPartsUp'], wpartons_dw=GenObjs['GenWPartsDw'], charged_leps=GenObjs['ChargedLeps'], neutral_leps=GenObjs['NeutralLeps'], bs=GenObjs['GenB'], bbars=GenObjs['GenBbar'], wbosons=GenObjs['GenW'], tops=GenObjs['GenTop'], tbars=GenObjs['GenTbar'])
 
+    #set_trace()
     return ttbar
 
