44f08001de38c389b1e7b31945b10ac21f6ea6b8
jdulemba <josdrw9@gmail.com>
2021-02-12 16:47:44 +0100

    Updated to run with NanoAODSchema for coffea 0.7 and awkward1

--- Analysis/python/ObjectSelection.py
+++ Analysis/python/ObjectSelection.py
@@ -1,65 +1,82 @@
 from pdb import set_trace
-import coffea.processor.dataframe
-import awkward
+import awkward as ak
 import python.Filters_and_Triggers as Filters_and_Triggers
 import python.IDJet as IDJet
 import python.IDMuon as IDMuon
 import python.IDElectron as IDElectron
-import python.IDMet as IDMet
+#import python.IDMet as IDMet
 import numpy as np
-import coffea.processor as processor
-import Utilities.systematics as systematics
+from coffea.analysis_tools import PackedSelection
 
 def select_jets(jets, muons, electrons, year, cutflow=None):
     
-    if isinstance(jets, awkward.array.base.AwkwardArray):
-            ## pt and eta cuts
-        pass_pt_eta_cuts = IDJet.make_pt_eta_cuts(jets)
-        if cutflow is not None: cutflow['jets pass pT and eta cuts'] += pass_pt_eta_cuts.sum().sum()
-
-        #    ## HEM issue
-        #if year == '2018':
-        #    hem_region = IDJet.HEM_15_16_issue(jets)
-
-            ## tightID
-        # check https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookNanoAOD for definitions
-        if year == '2016':
-            #jetId = 7 # pass loose, tight, tightLepVeto ID
-            jetId = 3 # pass loose and tight, but not tightLepVeto ID
-        else:
-            #jetId = 4 # pass tight and tightLepVeto ID
-            jetId = 2 # pass tight but not tightLepVeto ID
-        jet_ID = (jets.Id >= jetId) # pass at least tight
-        #jet_ID = (jets.Id == jetId)
-        if cutflow is not None: cutflow['jets pass ID'] += jet_ID.sum().sum()
-
-            ## remove jets that don't pass ID and pt/eta cuts
-        jets = jets[(jet_ID & pass_pt_eta_cuts)]
-
-            ## clean jets wrt loose+tight el and mu
-        clean_j_tightMu = ~(jets.match(muons[muons['TIGHTMU'] == True], deltaRCut=0.4))
-        clean_j_looseMu = ~(jets.match(muons[muons['LOOSEMU'] == True], deltaRCut=0.4))
-        clean_j_tightEl = ~(jets.match(electrons[electrons['TIGHTEL'] == True], deltaRCut=0.4))
-        clean_j_looseEl = ~(jets.match(electrons[electrons['LOOSEEL'] == True], deltaRCut=0.4))
-            ## clean jets wrt veto el and mu
-        clean_j_vetoMu = ~(jets.match(muons[muons['VETOMU'] == True], deltaRCut=0.4))
-        clean_j_vetoEl = ~(jets.match(electrons[electrons['VETOEL'] == True], deltaRCut=0.4))
-        jets = jets[(clean_j_looseEl & clean_j_tightEl & clean_j_looseMu & clean_j_tightMu) & (clean_j_vetoMu & clean_j_vetoEl)]
-
-            ## leading jet pt cut
-        leadpt_cut = IDJet.make_leadjet_pt_cut(jets)
-        if cutflow is not None: cutflow['jets pass lead jet pT cut'] += leadpt_cut.sum()
-
-            ## 3 or more jets
-        njet_restriction = 3
-        njets_cuts = (jets.counts >= njet_restriction)
-        #njets_cuts = (jets.counts == njet_restriction)
-        if cutflow is not None: cutflow['nEvts with %s+ clean jets passing ID and kin selection' % njet_restriction] += njets_cuts.sum()
-
-        passing_jets = (leadpt_cut & njets_cuts)
-
+        ## pt and eta cuts
+    pass_pt_eta_cuts = IDJet.make_pt_eta_cuts(jets)
+    if cutflow is not None: cutflow['jets pass pT and eta cuts'] += ak.sum(pass_pt_eta_cuts)
+    #if cutflow is not None: cutflow['jets pass pT and eta cuts'] += pass_pt_eta_cuts.sum().sum()
+    
+    #    ## HEM issue
+    #if year == '2018':
+    #    hem_region = IDJet.HEM_15_16_issue(jets)
+    
+        ## tightID
+    # check https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookNanoAOD for definitions
+    if year == '2016':
+        #jetId = 7 # pass loose, tight, tightLepVeto ID
+        jetId = 3 # pass loose and tight, but not tightLepVeto ID
     else:
-        raise ValueError("Only AwkwardArrays are supported")
+        #jetId = 4 # pass tight and tightLepVeto ID
+        jetId = 2 # pass tight but not tightLepVeto ID
+    jet_ID = (jets['jetId'] >= jetId) # pass at least tight
+    if cutflow is not None: cutflow['jets pass ID'] += ak.sum(jet_ID)
+    #jet_ID = (jets.Id >= jetId) # pass at least tight
+    #if cutflow is not None: cutflow['jets pass ID'] += jet_ID.sum().sum()
+    
+        ## remove jets that don't pass ID and pt/eta cuts
+    jets = jets[(jet_ID & pass_pt_eta_cuts)]
+   
+        ## clean jets wrt veto+loose+tight el and mu
+    jets_ak = ak.with_name(jets[["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    tight_muons_ak = ak.with_name(muons[muons['TIGHTMU'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    loose_muons_ak = ak.with_name(muons[muons['LOOSEMU'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    veto_muons_ak = ak.with_name(muons[muons['VETOMU'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    tight_electrons_ak = ak.with_name(electrons[electrons['TIGHTEL'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    loose_electrons_ak = ak.with_name(electrons[electrons['LOOSEEL'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+    veto_electrons_ak = ak.with_name(electrons[electrons['VETOEL'] == True][["pt", "eta", "phi", "mass"]],"PtEtaPhiMLorentzVector")
+
+            # tight mu
+    jets_akc, tight_muons_akc = ak.unzip(ak.cartesian([jets_ak, tight_muons_ak], nested=True))
+    clean_j_tightMu_mask = ak.all((jets_akc.delta_r(tight_muons_akc) >= 0.4), axis=2)
+            # loose mu
+    jets_akc, loose_muons_akc = ak.unzip(ak.cartesian([jets_ak, loose_muons_ak], nested=True))
+    clean_j_looseMu_mask = ak.all((jets_akc.delta_r(loose_muons_akc) >= 0.4), axis=2)
+            # veto mu
+    jets_akc, veto_muons_akc = ak.unzip(ak.cartesian([jets_ak, veto_muons_ak], nested=True))
+    clean_j_vetoMu_mask = ak.all((jets_akc.delta_r(veto_muons_akc) >= 0.4), axis=2)
+            # tight el
+    jets_akc, tight_electrons_akc = ak.unzip(ak.cartesian([jets_ak, tight_electrons_ak], nested=True))
+    clean_j_tightEl_mask = ak.all((jets_akc.delta_r(tight_electrons_akc) >= 0.4), axis=2)
+            # loose el
+    jets_akc, loose_electrons_akc = ak.unzip(ak.cartesian([jets_ak, loose_electrons_ak], nested=True))
+    clean_j_looseEl_mask = ak.all((jets_akc.delta_r(loose_electrons_akc) >= 0.4), axis=2)
+            # veto el
+    jets_akc, veto_electrons_akc = ak.unzip(ak.cartesian([jets_ak, veto_electrons_ak], nested=True))
+    clean_j_vetoEl_mask = ak.all((jets_akc.delta_r(veto_electrons_akc) >= 0.4), axis=2)
+
+    clean_jets_mask = (clean_j_vetoMu_mask & clean_j_looseMu_mask & clean_j_tightMu_mask & clean_j_vetoEl_mask & clean_j_looseEl_mask & clean_j_tightEl_mask)
+    jets = jets[clean_jets_mask]
+
+        ## leading jet pt cut
+    leadpt_cut = IDJet.make_leadjet_pt_cut(jets)
+    if cutflow is not None: cutflow['jets pass lead jet pT cut'] += ak.sum(leadpt_cut)
+    
+        ## 3 or more jets
+    njet_restriction = 3
+    njets_cuts = (ak.num(jets) >= njet_restriction)
+    if cutflow is not None: cutflow['nEvts with %s+ clean jets passing ID and kin selection' % njet_restriction] += ak.sum(njets_cuts)
+    
+    passing_jets = (leadpt_cut & njets_cuts)
+    if cutflow is not None: cutflow['nEvts with jets passing selection'] += ak.sum(passing_jets)
 
     if cutflow is not None:
         return jets, passing_jets, cutflow
@@ -67,33 +84,31 @@ def select_jets(jets, muons, electrons, year, cutflow=None):
         return jets, passing_jets
 
 
-def select(df, year, corrections, noIso=False, cutflow=None, shift=None):
+def select(events, year, corrections, noIso=False, cutflow=None, shift=None):
 
-    if not isinstance(df, coffea.processor.dataframe.LazyDataFrame):
-        raise IOError("This function only works for LazyDataFrame objects")
-    evt_sel = processor.PackedSelection()
+    evt_sel = PackedSelection()
 
     ### trigger selection
-    evt_sel.add('mu_triggers', Filters_and_Triggers.get_triggers(df=df, leptype='Muon', year=year, noIso=noIso))
-    evt_sel.add('el_triggers', Filters_and_Triggers.get_triggers(df=df, leptype='Electron', year=year, noIso=noIso))
+    evt_sel.add('mu_triggers', Filters_and_Triggers.get_triggers(HLT=events['HLT'], leptype='Muon', year=year, noIso=noIso))
+    evt_sel.add('el_triggers', Filters_and_Triggers.get_triggers(HLT=events['HLT'], leptype='Electron', year=year, noIso=noIso))
 
     ### filter selection
-    evt_sel.add('pass_filters', Filters_and_Triggers.get_filters(df=df, year=year))
+    evt_sel.add('pass_filters', Filters_and_Triggers.get_filters(Flags=events['Flag'], year=year))
 
     ### lepton selection
-    df['Muon'] = IDMuon.process_muons(df, year)
-    df['Electron'] = IDElectron.process_electrons(df, year)
-    evt_sel.add('single_lep', np.logical_xor(( (df['Muon']['TIGHTMU'].sum() + df['Muon']['LOOSEMU'].sum()) == 1 ), ( (df['Electron']['TIGHTEL'].sum() + df['Electron']['LOOSEEL'].sum()) == 1 ))) # only single LOOSE or TIGHT el or mu (not counting vetos)
-    evt_sel.add('single_looseMu', df['Muon']['LOOSEMU'].sum() == 1)
-    evt_sel.add('single_tightMu', df['Muon']['TIGHTMU'].sum() == 1)
-    evt_sel.add('single_looseEl', df['Electron']['LOOSEEL'].sum() == 1)
-    evt_sel.add('single_tightEl', df['Electron']['TIGHTEL'].sum() == 1)
-    evt_sel.add('zero_vetoMu', df['Muon']['VETOMU'].sum() == 0)
-    evt_sel.add('zero_vetoEl', df['Electron']['VETOEL'].sum() == 0)
-    evt_sel.add('tightMu_pass_vetoMu', (((df['Muon']['TIGHTMU']*1 + df['Muon']['VETOMU']*1) > 0).sum() == 1)) # only veto mu (if it exists) is tight
-    evt_sel.add('looseMu_pass_vetoMu', (((df['Muon']['LOOSEMU']*1 + df['Muon']['VETOMU']*1) > 0).sum() == 1)) # only veto mu (if it exists) is loose
-    evt_sel.add('tightEl_pass_vetoEl', (((df['Electron']['TIGHTEL']*1 + df['Electron']['VETOEL']*1) > 0).sum() == 1)) # only veto el (if it exists) is tight
-    evt_sel.add('looseEl_pass_vetoEl', (((df['Electron']['LOOSEEL']*1 + df['Electron']['VETOEL']*1) > 0).sum() == 1)) # only veto el (if it exists) is loose
+    events['Muon'] = IDMuon.process_muons(events['Muon'], year)
+    events['Electron'] = IDElectron.process_electrons(events['Electron'], year)
+    evt_sel.add('single_lep', np.logical_xor(( (ak.sum(events['Muon']['TIGHTMU'], axis=1) + ak.sum(events['Muon']['LOOSEMU'], axis=1)) == 1 ), ( (ak.sum(events['Electron']['TIGHTEL'], axis=1) + ak.sum(events['Electron']['LOOSEEL'], axis=1)) == 1 ))) # only single LOOSE or TIGHT el or mu (not counting vetos)
+    evt_sel.add('single_looseMu', ak.sum(events['Muon']['LOOSEMU'], axis=1) == 1)
+    evt_sel.add('single_tightMu', ak.sum(events['Muon']['TIGHTMU'], axis=1) == 1)
+    evt_sel.add('single_looseEl', ak.sum(events['Electron']['LOOSEEL'], axis=1) == 1)
+    evt_sel.add('single_tightEl', ak.sum(events['Electron']['TIGHTEL'], axis=1) == 1)
+    evt_sel.add('zero_vetoMu', ak.sum(events['Muon']['VETOMU'], axis=1) == 0)
+    evt_sel.add('zero_vetoEl', ak.sum(events['Electron']['VETOEL'], axis=1) == 0)
+    evt_sel.add('tightMu_pass_vetoMu', (ak.sum(((events['Muon']['TIGHTMU']*1 + events['Muon']['VETOMU']*1) > 0), axis=1) == 1)) # only veto mu (if it exists) is tight
+    evt_sel.add('looseMu_pass_vetoMu', (ak.sum(((events['Muon']['LOOSEMU']*1 + events['Muon']['VETOMU']*1) > 0), axis=1) == 1)) # only veto mu (if it exists) is loose
+    evt_sel.add('tightEl_pass_vetoEl', (ak.sum(((events['Electron']['TIGHTEL']*1 + events['Electron']['VETOEL']*1) > 0), axis=1) == 1)) # only veto el (if it exists) is tight
+    evt_sel.add('looseEl_pass_vetoEl', (ak.sum(((events['Electron']['LOOSEEL']*1 + events['Electron']['VETOEL']*1) > 0), axis=1) == 1)) # only veto el (if it exists) is loose
 
         # leptons pass loose/tight requirements and triggers (mu only pass mu triggers, el only pass el triggers)
     evt_sel.add('tightMu_pass', evt_sel.require(single_lep=True, single_tightMu=True, zero_vetoEl=True, tightMu_pass_vetoMu=True, mu_triggers=True) & ~evt_sel.require(el_triggers=True))
@@ -106,22 +121,20 @@ def select(df, year, corrections, noIso=False, cutflow=None, shift=None):
     evt_sel.add('passing_lep', evt_sel.require(passing_mu=True) | evt_sel.require(passing_el=True))
 
     evt_sel.add('lep_and_filter_pass', evt_sel.require(passing_lep=True, pass_filters=True))
-    #set_trace()
     if cutflow is not None: cutflow['lep_and_filter_pass'] += evt_sel.require(lep_and_filter_pass=True).sum()
 
         ## SetMET
-    df['MET'] = IDMet.process_met(df)
+    #df['MET'] = IDMet.process_met(df)
 
-    ### jets selection
-    df['Jet'] = IDJet.process_jets(df, year, corrections['JetCor'], shift=shift)# initialize jets
-    #df['Jet'] = IDJet.process_jets(df, year, corrections['JetCor'], shift=shift) if shift in systematics.jet_sys else IDJet.process_jets(df, year, corrections['JetCor'])# initialize jets
+        ## get corrected jets and MET
+    events['Jet'], events['MET'] = IDJet.process_jets(events, year, corrections['JetCor'])
     if cutflow is not None:
-        new_jets, passing_jets, cutflow = select_jets(df['Jet'], df['Muon'], df['Electron'], year, cutflow)
+        new_jets, passing_jets, cutflow = select_jets(events['Jet'], events['Muon'], events['Electron'], year, cutflow)
     else:
-        new_jets, passing_jets = select_jets(df['Jet'], df['Muon'], df['Electron'], year)
+        new_jets, passing_jets = select_jets(events['Jet'], events['Muon'], events['Electron'], year)
     
         ## substitute jets for ones that pass requirements from select_jets
-    df['Jet'] = new_jets
+    events['Jet'] = new_jets
 
     passing_evts = evt_sel.require(lep_and_filter_pass=True) & passing_jets
 
