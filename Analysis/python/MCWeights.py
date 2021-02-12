aeb305d1291a36a381bbf75836898eb4739a466d
jdulemba <josdrw9@gmail.com>
2021-02-12 16:35:00 +0100

    Updated to run with NanoAODSchema for coffea 0.7 and awkward1, cleaned up and got rid of some functions

--- Analysis/python/MCWeights.py
+++ Analysis/python/MCWeights.py
@@ -1,71 +1,70 @@
 from pdb import set_trace
-import coffea.processor.dataframe
 import numpy as np
-import coffea.processor as processor
+from coffea.analysis_tools import Weights
 import Utilities.systematics as systematics
-import awkward
+import awkward as ak
 import Utilities.make_variables as make_vars
 
-def get_event_weights(df, year: str, corrections, BTagSFs=[], isTTbar=False):
-    weights = processor.Weights(df.size, storeIndividual=True)# store individual variations
+def get_event_weights(events, year: str, corrections, BTagSFs=[], isTTbar=False):
+    weights = Weights(len(events), storeIndividual=True) # store individual variations
 
         ## only apply to MC
-    if not df.dataset.startswith('data_Single'):
+    if not events.metadata['dataset'].startswith('data_Single'):
             ## Prefire Corrections
         if (year != '2018') and (corrections['Prefire'] == True):
             weights.add('Prefire',
-                df['L1PreFiringWeight_Nom'],
-                df['L1PreFiringWeight_Up'],
-                df['L1PreFiringWeight_Dn']
+                events['L1PreFiringWeight']['Nom'],
+                events['L1PreFiringWeight']['Up'],
+                events['L1PreFiringWeight']['Dn']
             )
 
-            ## Generator Weights (normalize them)
-        weights.add('genweight', df.genWeight)
+            ## Generator Weights
+        weights.add('genweight', events['genWeight'])
     
             ## Pileup Reweighting
         if 'Pileup' in corrections.keys():
             weights.add('Pileup',
-                corrections['Pileup'][year][df['dataset']]['central'](df['Pileup_nTrueInt']),
-                corrections['Pileup'][year][df['dataset']]['up'](df['Pileup_nTrueInt']),
-                corrections['Pileup'][year][df['dataset']]['down'](df['Pileup_nTrueInt'])
+                corrections['Pileup'][events.metadata['dataset']]['central'](events['Pileup']['nTrueInt']),
+                corrections['Pileup'][events.metadata['dataset']]['up'](events['Pileup']['nTrueInt']),
+                corrections['Pileup'][events.metadata['dataset']]['down'](events['Pileup']['nTrueInt'])
             )
 
         ## PS and LHE weights for ttbar events
         if isTTbar:
             ## PS Weight variations
             # PSWeight definitions can be found here: https://cms-nanoaod-integration.web.cern.ch/integration/master/mc94X_doc.html#PSWeight
-            psweights = awkward.JaggedArray.fromcounts(df['nPSWeight'], df['PSWeight'])
+            psweights = events['PSWeight']
             weights.add('ISR',
-                np.ones(df.size),
+                np.ones(len(events)),
                 psweights[:, 2], # (ISR=2, FSR=1)
                 psweights[:, 0], # (ISR=0.5, FSR=1)
             )
             weights.add('FSR',
-                np.ones(df.size),
+                np.ones(len(events)),
                 psweights[:, 3], # (ISR=1, FSR=2)
                 psweights[:, 1], # (ISR=1, FSR=0.5)
             )
 
             ## LHEScale Weight Variations
             # LHEScaleWeight definitions can be found here: https://cms-nanoaod-integration.web.cern.ch/integration/master/mc94X_doc.html#LHE
-            lheweights = awkward.JaggedArray.fromcounts(df['nLHEScaleWeight'], df['LHEScaleWeight'])
+            lheweights = events['LHEScaleWeight']
             weights.add('FACTOR',
-                np.ones(df.size),
+                np.ones(len(events)),
                 lheweights[:, 5], # (muR=1, muF=2)
                 lheweights[:, 3], # (muR=1, muF=0.5)
             )
             weights.add('RENORM',
-                np.ones(df.size),
+                np.ones(len(events)),
                 lheweights[:, 7], # (muR=2, muF=1)
                 lheweights[:, 1], # (muR=0.5, muF=1)
             )
             weights.add('RENORM_FACTOR_SAME',
-                np.ones(df.size),
+                np.ones(len(events)),
                 lheweights[:, 8], # (muR=2, muF=2), RENORM_UP_FACTOR_UP
                 lheweights[:, 0], # (muR=0.5, muF=0.5), RENORM_DW_FACTOR_DW
             )
             weights.add('RENORM_FACTOR_DIFF',
-                np.ones(df.size),
+                np.ones(len(events)),
                 lheweights[:, 6], # (muR=2, muF=0.5), RENORM_UP_FACTOR_DW
                 lheweights[:, 2], # (muR=0.5, muF=2), RENORM_DW_FACTOR_UP
             )
@@ -76,7 +75,7 @@ def get_event_weights(df, year: str, corrections, BTagSFs=[], isTTbar=False):
 def get_pdf_weights(df):
     if len(sorted(set(df['nLHEPdfWeight']))) > 1:
         raise ValueError('Differing number of PDF weights for events')
-    pdfweights = awkward.JaggedArray.fromcounts(df['nLHEPdfWeight'], df['LHEPdfWeight'])
+    pdfweights = ak.JaggedArray.fromcounts(df['nLHEPdfWeight'], df['LHEPdfWeight'])
     df['PDFWeights'] = pdfweights
     
 
@@ -125,34 +124,11 @@ def get_kin_weights(correction, genttbar):
     
 
 
-def get_toppt_weights(pt1=np.array([-1.]), pt2=np.array([-1.]), shift=None):
-    if pt1.any() < 0. or pt2.any() < 0.:
-        raise IOError("top pt inputs not valid")
-
-    if shift == 'TOPPT_UP_NOM':
-        nu_1, nu_2 = 1., 0.
-    elif shift == 'TOPPT_DW_NOM':
-        nu_1, nu_2 = -1., 0.
-    elif shift == 'TOPPT_NOM_UP':
-        nu_1, nu_2 = 0., 1.
-    elif shift == 'TOPPT_NOM_DW':
-        nu_1, nu_2 = 0., -1.
-    else:
-        nu_1, nu_2 = 0., 0.
-
-    # top pT reweighting formula based on section 5.5 of AN-16-272
-    p0 =  6.15025*np.power(10., -2.) + 3.243*np.power(10., -2.)*nu_1 - 4.353*np.power(10., -7.)*nu_2
-    p1 = -5.17833*np.power(10., -4.) - 1.404*np.power(10., -4.)*nu_1 - 1.005*np.power(10., -4.)*nu_2
-
-    weight = exp(p0+p1*( (pt1+pt2)/2 ))
-    return weight
-
-
 def get_comb_lepSF(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.ndarray, shift='Central'):
     if not (shift == 'Central' or shift == 'Error'):
         raise ValueError('Shift value %s not defined' % shift)
     #set_trace()
-    sf_dict = corrections[year][lepton]
+    sf_dict = corrections[lepton]
     eta_ranges = sf_dict['eta_ranges']
     lepSFs = np.ones(pt.size)
 
@@ -167,18 +143,17 @@ def get_comb_lepSF(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.
     return lepSFs
 
 
-def get_lepton_sf(year: str, lepton: str, corrections, pt: np.ndarray, eta: np.ndarray):
-    #set_trace()
-    sf_dict = corrections[year][lepton]
+def get_lepton_sf(year: str, lepton: str, corrections, pt, eta):
+    sf_dict = corrections[lepton]
     eta_ranges = sf_dict['eta_ranges']
-    reco_cen = np.ones(pt.size)
-    reco_err = np.ones(pt.size)
-    trig_cen = np.ones(pt.size)
-    trig_err = np.ones(pt.size)
+    reco_cen = np.ones(len(pt))
+    reco_err = np.ones(len(pt))
+    trig_cen = np.ones(len(pt))
+    trig_err = np.ones(len(pt))
 
     for idx, eta_range in enumerate(eta_ranges):
         mask = (eta >= eta_range[0]) & (eta < eta_range[1]) # find inds that are within given eta range
-        if not mask.any(): continue # no values fall within eta range
+        if not ak.any(mask): continue # no values fall within eta range
         recoSF_cen = sf_dict['Reco_ID']['Central']['eta_bin%i' % idx]
         recoSF_err = sf_dict['Reco_ID']['Error']['eta_bin%i' % idx]
         trigSF_cen = sf_dict['Trig']['Central']['eta_bin%i' % idx]
