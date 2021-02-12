b9da062451a2652b83dd7d3a70914d7faec9cb8e
jdulemba <josdrw9@gmail.com>
2021-02-12 16:31:58 +0100

    Updated to run with NanoAODSchema for coffea 0.7 and awkward1

--- Analysis/python/IDElectron.py
+++ Analysis/python/IDElectron.py
@@ -3,38 +3,11 @@ from pdb import set_trace
 import Utilities.prettyjson as prettyjson
 import os
 
-def process_electrons(df, year):
-    from coffea.analysis_objects import JaggedCandidateArray
-    electrons = JaggedCandidateArray.candidatesfromcounts(
-        df['nElectron'],
-        pt=df['Electron_pt'],
-        eta=df['Electron_eta'],
-        phi=df['Electron_phi'],
-        mass=df['Electron_mass'],
-        charge=df['Electron_charge'],
-        dxy=df['Electron_dxy'],
-        dz=df['Electron_dz'],
-        mvaTightID=df['Electron_mvaFall17V2noIso_WP80'],
-        mvaVetoID=df['Electron_mvaFall17V2noIso_WPL'],
-        #mvaVetoID=df['Electron_mvaFall17V2noIso_WPL'],
-        deltaEtaSC=df['Electron_deltaEtaSC'],
-        pfRelIso=df['Electron_pfRelIso03_all'],
-        minipfRelIso=df['Electron_miniPFRelIso_all'],
-        cutBasedID=df['Electron_cutBased'],
-        bitmap=df['Electron_vidNestedWPBitmap'],
-        sieie=df['Electron_sieie'],
-        hoe=df['Electron_hoe'],
-        eInvMpInv=df['Electron_eInvMinusPInv'],
-        missingHits=df['Electron_lostHits'],
-        convVeto=df['Electron_convVeto'],
-    )
-
+def process_electrons(electrons, year):
         # makes etaSC
-    electrons['etaSC'] = electrons.deltaEtaSC + electrons.eta
-    electrons['ECAL_GAP'] = (np.abs(electrons.etaSC) <= 1.4442) | (np.abs(electrons.etaSC) >= 1.5660)
-
-    electrons['IPCuts'] = ((np.abs(electrons.etaSC) < 1.479) & (np.abs(electrons.dxy) < 0.05) & (np.abs(electrons.dz) < 0.10)) | ((np.abs(electrons.etaSC) >= 1.479) & (np.abs(electrons.dxy) < 0.10) & (np.abs(electrons.dz) < 0.20))
-
+    electrons['etaSC'] = electrons['deltaEtaSC'] + electrons['eta']
+    electrons['ECAL_GAP'] = (np.abs(electrons['etaSC']) <= 1.4442) | (np.abs(electrons['etaSC']) >= 1.5660)
+    electrons['IPCuts'] = ((np.abs(electrons['etaSC']) < 1.479) & (np.abs(electrons['dxy']) < 0.05) & (np.abs(electrons['dz']) < 0.10)) | ((np.abs(electrons['etaSC']) >= 1.479) & (np.abs(electrons['dxy']) < 0.10) & (np.abs(electrons['dz']) < 0.20))
 
         ## make electron ID/Iso categories
     electrons = make_electron_ids(electrons, year)
@@ -48,24 +21,15 @@ def process_electrons(df, year):
 '''
 
 
-#def fail(electrons):
-#    return electrons
-#
 def veto_15(electrons):
-    ID = (electrons.cutBasedID == 1) # 0 is Fail, 1 Veto, 2 Loose, 3 Medium, 4 Tight
-    #ID = (electrons.mvaVetoID)
-    ##Iso = (electrons.pfRelIso < 0.25) # based on muon loose Iso def
+    ID = (electrons['cutBased'] == 1) # 0 is Fail, 1 Veto, 2 Loose, 3 Medium, 4 Tight
     return ID
 
 def tight_15_NoECAL_Gap(electrons):
-    ID = (electrons.cutBasedID == 4) # 0 is Fail, 1 Veto, 2 Loose, 3 Medium, 4 Tight
-    #ID = (electrons.mvaTightID)
-    #Iso = (electrons.pfRelIso < 0.15) #*
-    ecalgap = (electrons.ECAL_GAP)
-    ipcuts = (electrons.IPCuts)
-
+    ID = (electrons['cutBased'] == 4) # 0 is Fail, 1 Veto, 2 Loose, 3 Medium, 4 Tight
+    ecalgap = (electrons['ECAL_GAP'])
+    ipcuts = (electrons['IPCuts'])
     return (ID & ecalgap & ipcuts)
-    #return (ID & Iso & ecalgap & ipcuts)
 
 def fakes(electrons):
     '''
@@ -80,58 +44,15 @@ def fakes(electrons):
         
         invertedIso_bitvals = [613566692, 613566564, 613566628, 613566500]
     '''
-    #    # testing electron iso inversion
-    #basic_cut = (electrons.pt > 30) & (np.abs(electrons.eta) < 2.4) & (electrons.ECAL_GAP) & (electrons.IPCuts)
-    #els = electrons[basic_cut]
-
-    #barrel = (np.abs(els.etaSC) <= 1.479)
-    #endcap = (np.abs(els.etaSC) > 1.479)
-    #bitmap = els.bitmap
-
-    #rho = els.pt.ones_like()*df['fixedGridRhoFastjetAll']
-    #tightID_barrel = barrel & (els.sieie < 0.0104) & (els.hoe < 0.026+1.15/els.etaSC+0.0324*rho/els.etaSC) & (np.abs(els.eInvMpInv) < 0.159) & (els.missingHits <= 1) & els.convVeto
-    #tightID_endcap = endcap & (els.sieie < 0.0353) & (els.hoe < 0.0188+2.06/els.etaSC+0.183*rho/els.etaSC) & (np.abs(els.eInvMpInv) < 0.0197) & (els.missingHits <= 1) & els.convVeto
-    #tightID_els = tightID_barrel | tightID_endcap
-
-    #barrel_els = els[tightID_barrel]
-    #endcap_els = els[tightID_endcap]
-    #tightIso_barrel = (barrel_els.pfRelIso < (0.0287 + 0.506/barrel_els.pt))
-    #tightIso_endcap = (endcap_els.pfRelIso < (0.0445 + 0.963/endcap_els.pt))
-    #tightIso_bitvals = set(bitmap[tightID_barrel][tightIso_barrel].flatten().tolist()+bitmap[tightID_endcap][tightIso_endcap].flatten().tolist())
-    ##tightIso_bitvals = list(set(bitmap[tightID_barrel][tightIso_barrel].flatten().tolist()+bitmap[tightID_endcap][tightIso_endcap].flatten().tolist()))
-    #invTightIso_barrel = (barrel_els.pfRelIso >= (0.0287 + 0.506/barrel_els.pt))
-    #invTightIso_endcap = (endcap_els.pfRelIso >= (0.0445 + 0.963/endcap_els.pt))
-    #invTightIso_bitvals = set(bitmap[tightID_barrel][invTightIso_barrel].flatten().tolist()+bitmap[tightID_endcap][invTightIso_endcap].flatten().tolist())
-    ##invTightIso_bitvals = list(set(bitmap[tightID_barrel][invTightIso_barrel].flatten().tolist()+bitmap[tightID_endcap][invTightIso_endcap].flatten().tolist()))
-
-    #only_inv_bits = list(invTightIso_bitvals - tightIso_bitvals)
-    #import re
-    #binary_nums = ['{0:0b}'.format(num) for num in only_inv_bits]
-    #split_binary_nums = [re.findall('...', num) for num in binary_nums]
-    #diff_inds = [[idx for idx, val in enumerate(re.findall('...', num)) if val != '100'] for num in binary_nums]
-    #flat_list = [item for sublist in diff_inds for item in sublist]
-    #flat_set = set(flat_list)
-    #set_trace()
-    #invest_barrel_els = els[(bitmap == 611469604) & (np.abs(els.etaSC) <= 1.479)]
-    #invest_endcap_els = els[(bitmap == 611469604) & (np.abs(els.etaSC) > 1.479)]
-
-    #invIso_barrel = invest_barrel_els.pfRelIso < 0.0287 + 0.506/invest_barrel_els.pt
-    #invIso_endcap = invest_endcap_els.pfRelIso < 0.0445 + 0.963/invest_endcap_els.pt
-        ##
-
-    ID = ((electrons.bitmap == 609372452) | (electrons.bitmap == 607275300) | (electrons.bitmap == 605178148)) #| (electrons.bitmap == 611469604))
-    #ID = (electrons.mvaTightID)
-    #Iso = (electrons.pfRelIso >= 0.15) #*
-    ecalgap = (electrons.ECAL_GAP)
-    ipcuts = (electrons.IPCuts)
-
+    ID = ((electrons['vidNestedWPBitmap'] == 609372452) | (electrons['vidNestedWPBitmap'] == 607275300) | (electrons['vidNestedWPBitmap'] == 605178148)) #| (electrons.bitmap == 611469604))
+    ecalgap = (electrons['ECAL_GAP'])
+    ipcuts = (electrons['IPCuts'])
     return (ID & ecalgap & ipcuts)
-    #return (ID & Iso & ecalgap & ipcuts)
 
 
 def make_electron_ids(electrons, year):
 
-    el_pars = prettyjson.loads(open('%s/cfg_files/cfg_pars_%s.json' % (os.environ['PROJECT_DIR'], os.environ['jobid'])).read())['Electrons'][year]
+    el_pars = prettyjson.loads(open(os.path.join(os.environ['PROJECT_DIR'], 'cfg_files', 'cfg_pars_%s.json' % os.environ['jobid'])).read())['Electrons'][year]
 
     id_names = {
         #'FAIL' : fail,
@@ -151,11 +72,10 @@ def make_electron_ids(electrons, year):
     if el_pars['TIGHTEL']['id'] not in id_names.keys():
         raise IOError("tight Electron ID name not valid")
 
-    #set_trace()
     for elID in el_pars.keys():
-        pt_cut = (electrons.pt >= el_pars[elID]['ptmin'])
-        #etaSC_cut = (np.abs(electrons.etaSC) <= el_pars[elID]['etascmax'])
-        eta_cut = (np.abs(electrons.eta) <= el_pars[elID]['etamax'])
+        pt_cut = (electrons['pt'] >= el_pars[elID]['ptmin'])
+        #etaSC_cut = (np.abs(electrons['etaSC']) <= el_pars[elID]['etascmax'])
+        eta_cut = (np.abs(electrons['eta']) <= el_pars[elID]['etamax'])
         pass_id = id_names[el_pars[elID]['id']](electrons)
         electrons[elID] = (pass_id) & (pt_cut) & (eta_cut)
         #electrons[elID] = (pass_id) & (pt_cut) & (etaSC_cut)
--- Analysis/python/IDMuon.py
+++ Analysis/python/IDMuon.py
@@ -3,92 +3,62 @@ from pdb import set_trace
 import Utilities.prettyjson as prettyjson
 import os
 
-def process_muons(df, year):
-    from coffea.analysis_objects import JaggedCandidateArray
-    muons = JaggedCandidateArray.candidatesfromcounts(
-        df['nMuon'],
-        pt=df['Muon_pt'],
-        eta=df['Muon_eta'],
-        phi=df['Muon_phi'],
-        mass=df['Muon_mass'],
-        charge=df['Muon_charge'],
-        looseId=df['Muon_looseId'],
-        tightId=df['Muon_tightId'],
-        pfRelIso=df['Muon_pfRelIso04_all'],
-        tkRelIso=df['Muon_tkRelIso'],
-    )
-
-        ## add muon ID+Iso categories
-    muons = make_muon_ids(muons, year)
-
-    return muons
-
-
 
     ## Muon ID types
 def fail(muons):
     return muons
 
 def loose_12(muons):
-    ID  = (muons.looseId)
-    Iso = (muons.tkRelIso < 0.10)
-
+    ID  = (muons['looseId'])
+    Iso = (muons['tkRelIso'] < 0.10)
     return (ID & Iso)
 
 def tight_12(muons):
-    ID  = (muons.tightId)
-    Iso = (muons.tkRelIso < 0.05)
-
+    ID  = (muons['tightId'])
+    Iso = (muons['tkRelIso'] < 0.05)
     return (ID & Iso)
 
 def loose_12Db(muons):
-    ID  = (muons.looseId)
-    Iso = (muons.pfRelIso < 0.12)
-
+    ID  = (muons['looseId'])
+    Iso = (muons['pfRelIso04_all'] < 0.12)
     return (ID & Iso)
 
 def tight_12Db(muons):
-    ID  = (muons.tightId)
-    Iso = (muons.pfRelIso < 0.12)
-
+    ID  = (muons['tightId'])
+    Iso = (muons['pfRelIso04_all'] < 0.12)
     return (ID & Iso)
 
 def loose_15(muons):
-    ID  = (muons.looseId)
-    Iso = (muons.tkRelIso < 0.10)
-
+    ID  = (muons['looseId'])
+    Iso = (muons['tkRelIso'] < 0.10)
     return (ID & Iso)
 
 def tight_15(muons):
-    ID  = (muons.tightId)
-    Iso = (muons.tkRelIso < 0.05)
-
+    ID  = (muons['tightId'])
+    Iso = (muons['tkRelIso'] < 0.05)
     return (ID & Iso)
 
 def loose_15Db(muons):
-    ID  = (muons.looseId)
-    Iso = (muons.pfRelIso < 0.25)
-
+    ID  = (muons['looseId'])
+    Iso = (muons['pfRelIso04_all'] < 0.25)
     return (ID & Iso)
 
 def tight_15Db(muons):
-    ID  = (muons.tightId)
-    Iso = (muons.pfRelIso < 0.15)
-
+    ID  = (muons['tightId'])
+    Iso = (muons['pfRelIso04_all'] < 0.15)
     return (ID & Iso)
 
 def tight_noIso(muons):
-    return (muons.tightId)
+    return (muons['tightId'])
 
 def antiloose_15Db(muons):
-    ID  = (muons.tightId)
-    Iso = (muons.pfRelIso >= 0.15)# & (muons.pfRelIso < 0.43)
-
+    ID  = (muons['tightId'])
+    Iso = (muons['pfRelIso04_all'] >= 0.15)# & (muons.pfRelIso04_all < 0.43)
     return (ID & Iso)
 
-def make_muon_ids(muons, year):
 
-    mu_pars = prettyjson.loads(open('%s/cfg_files/cfg_pars_%s.json' % (os.environ['PROJECT_DIR'], os.environ['jobid'])).read())['Muons'][year]
+def process_muons(muons, year):
+    mu_pars = prettyjson.loads(open(os.path.join(os.environ['PROJECT_DIR'], 'cfg_files', 'cfg_pars_%s.json' % os.environ['jobid'])).read())['Muons'][year]
 
     id_names = {
         'FAIL' : fail,
@@ -112,8 +82,8 @@ def make_muon_ids(muons, year):
         raise IOError("tight Muon ID name not valid")
 
     for muID in mu_pars.keys():
-        pt_cut = (muons.pt >= mu_pars[muID]['ptmin'])
-        eta_cut = (np.abs(muons.eta) <= mu_pars[muID]['etamax'])
+        pt_cut = (muons['pt'] >= mu_pars[muID]['ptmin'])
+        eta_cut = (np.abs(muons['eta']) <= mu_pars[muID]['etamax'])
         pass_id = id_names[mu_pars[muID]['id']](muons)
         muons[muID] = (pass_id) & (pt_cut) & (eta_cut)
 
