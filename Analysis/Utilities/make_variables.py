c047d64eee270c8db1651673da23c791a165001a
jdulemba <josdrw9@gmail.com>
2021-02-24 19:26:01 +0100

    Updated 'ctstar' to be used with awkward Arrays

--- Analysis/Utilities/make_variables.py
+++ Analysis/Utilities/make_variables.py
@@ -2,29 +2,17 @@ from pdb import set_trace
 import numpy as np
 import awkward as ak
 
-def ctstar(top_p4, tbar_p4, debug=False):
-
-    if debug:
-        ## for testing
-        tops = top_p4
-        tbars = tbar_p4
-    else:
-        # convert 4vecs to cartesian
-        tops = top_p4._to_cartesian().flatten()
-        tbars = tbar_p4._to_cartesian().flatten()
-
-    ttbar = (tops+tbars)
+def ctstar(top1, top2):
+    ttbar = (top1+top2)
 
     # find top quarks in rest frame
-    tt_boost = ttbar.boostp3
-    rf_tops = tops.boost(tt_boost*-1)
-    rf_tbars = tbars.boost(tt_boost*-1)
+    tt_boost = ttbar.boostvec
+    rf_top1, rf_top2 = top1.boost(tt_boost*-1), top2.boost(tt_boost*-1)
 
     # find ctstar
-    top_ctstar = ttbar.p3.unit.dot(rf_tops.p3.unit)
-    tbar_ctstar = ttbar.p3.unit.dot(rf_tbars.p3.unit)
+    top1_ctstar, top2_ctstar = ttbar.unit.dot(rf_top1.unit), ttbar.unit.dot(rf_top2.unit)
 
-    return top_ctstar, tbar_ctstar
+    return top1_ctstar, top2_ctstar
 
 def MT(leptons, met, debug=False):
     if debug: set_trace()
