cea6cb65502801feff9cfce05febcb847c392bc3
jdulemba <josdrw9@gmail.com>
2021-02-26 18:35:21 +0100

    Changed data_lumi and lumi_correction files, compare data to MC in kinematic plots now.

--- Analysis/Plotting_Scripts/best_perms_plotter.py
+++ Analysis/Plotting_Scripts/best_perms_plotter.py
@@ -18,26 +18,25 @@ import numpy as np
 import fnmatch
 import Utilities.Plotter as Plotter
 
-from argparse import ArgumentParser
-parser = ArgumentParser()
-parser.add_argument('year', choices=['2016', '2017', '2018'], help='What year is the ntuple from.')
-parser.add_argument('lepton', choices=['Electron', 'Muon'], help='Choose which lepton to make plots for')
-
-args = parser.parse_args()
-
 proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
+base_jobid = os.environ['base_jobid']
 analyzer = 'best_perms'
 
-input_dir = '/'.join([proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer])
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('year', choices=['2016APV', '2016', '2017', '2018'] if base_jobid == 'ULnanoAOD' else ['2016', '2017', '2018'], help='Specify which year to run over')
+parser.add_argument('lepton', choices=['Electron', 'Muon'], help='Choose which lepton to make plots for')
+parser.add_argument('--plot', default='all', choices=['bp', 'kin', 'all'], help='Make plots best perm objects, kinematic plots, or both.')
+args = parser.parse_args()
+
+input_dir = os.path.join(proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer)
 f_ext = 'TOT.coffea'
-outdir = '/'.join([proj_dir, 'plots', '%s_%s' % (args.year, jobid), analyzer])
+outdir = os.path.join(proj_dir, 'plots', '%s_%s' % (args.year, jobid), analyzer)
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
 fnames = sorted(['%s/%s' % (input_dir, fname) for fname in os.listdir(input_dir) if fname.endswith(f_ext)])
-
-#set_trace()
 hdict = plt_tools.add_coffea_files(fnames) if len(fnames) > 1 else load(fnames[0])
 
 jet_mults = {
@@ -83,14 +82,16 @@ bp_variables = {
 }
 
 variables = {
-    'mtt' : ('m($t\\bar{t}$) [GeV]', 2, (200., 2000.), True),
+    'mtt' : ('m($t\\bar{t}$) [GeV]', 2, (200., 2000.), False),
+    #'mtt' : ('m($t\\bar{t}$) [GeV]', 2, (200., 2000.), True),
     'mthad' : ('m($t_{h}$) [GeV]', 2, (0., 300.), True),
     'pt_thad' : ('$p_{T}$($t_{h}$) [GeV]', 2, (0., 500.), True),
     'pt_tt' : ('$p_{T}$($t\\bar{t}$) [GeV]', 2, (0., 500.), True),
     'eta_thad' : ('$\\eta$($t_{h}$)', 2, (-4., 4.), True),
     'eta_tt' : ('$\\eta$($t\\bar{t}$)', 2, (-4., 4.), True),
     'tlep_ctstar' : ('cos($\\theta^{*}_{t_{l}}$)', 2, (-1., 1.), True),
-    'tlep_ctstar_abs' : ('|cos($\\theta^{*}_{t_{l}}$)|', 2, (0., 1.), True),
+    'tlep_ctstar_abs' : ('|cos($\\theta^{*}_{t_{l}}$)|', 2, (0., 1.), False),
+    #'tlep_ctstar_abs' : ('|cos($\\theta^{*}_{t_{l}}$)|', 2, (0., 1.), True),
     'full_disc' : ('$\\lambda_{C}$', 2, (5, 25.), True),
     'mass_disc' : ('$\\lambda_{M}$', 2, (0, 20.), True),
     'ns_disc' : ('$\\lambda_{NS}$', 2, (3., 10.), True),
@@ -103,140 +104,124 @@ variables = {
 }
 
 
-    ## get plotting colors/settings
-stack_fill_opts = {'alpha': 0.8, 'edgecolor':(0,0,0,.5)}
-stack_error_opts = {'edgecolor':(0,0,0,.5)}
-
     ## get data lumi and scale MC by lumi
-data_lumi_year = prettyjson.loads(open('%s/inputs/lumis_data.json' % proj_dir).read())[args.year]
-lumi_correction = load('%s/Corrections/%s/MC_LumiWeights_IgnoreSigEvts.coffea' % (proj_dir, jobid))[args.year]['%ss' % args.lepton]
+data_lumi_year = prettyjson.loads(open(os.path.join(proj_dir, 'inputs', '%s_lumis_data.json' % base_jobid)).read())[args.year]
+lumi_correction = load(os.path.join(proj_dir, 'Corrections', base_jobid, 'MC_LumiWeights.coffea'))[args.year]['%ss' % args.lepton]
         # scale ttJets events, split by reconstruction type, by normal ttJets lumi correction
-ttJets_permcats = ['*right', '*matchable', '*unmatchable', '*other']
-names = [dataset for dataset in list(set([key[0] for key in hdict[list(bp_variables.keys())[0]].values().keys()]))] # get dataset names in hists
+ttJets_permcats = ['*right', '*matchable', '*unmatchable', '*sl_tau', '*other']
+names = [dataset for dataset in sorted(set([key[0] for key in hdict[sorted(variables.keys())[0]].values().keys()]))] # get dataset names in hists
 ttJets_cats = [name for name in names if any([fnmatch.fnmatch(name, cat) for cat in ttJets_permcats])] # gets ttJets(_PS)_other, ...
 if len(ttJets_cats) > 0:
     for tt_cat in ttJets_cats:
-        ttJets_lumi_topo = '_'.join(tt_cat.split('_')[:-1]) # gets ttJets[SL, Had, DiLep] or ttJets_PS
+        ttJets_lumi_topo = '_'.join(tt_cat.split('_')[:-2]) if 'sl_tau' in tt_cat else '_'.join(tt_cat.split('_')[:-1]) # gets ttJets[SL, Had, DiLep] or ttJets_PS
         ttJets_eff_lumi = lumi_correction[ttJets_lumi_topo]
         lumi_correction.update({tt_cat: ttJets_eff_lumi})
-for hname in hdict.keys():
-    if hname == 'cutflow': continue
-    hdict[hname].scale(lumi_correction, axis='dataset')
 
 ## make groups based on process
 process = hist.Cat("process", "Process", sorting='placement')
 process_cat = "dataset"
 process_groups = plt_tools.make_dataset_groups(args.lepton, args.year, samples=names)
+
+    # scale and group hists by process
 for hname in hdict.keys():
     if hname == 'cutflow': continue
-    hdict[hname] = hdict[hname].group(process_cat, process, process_groups)
+    hdict[hname].scale(lumi_correction, axis='dataset') # scale hists
+    hdict[hname] = hdict[hname].group(process_cat, process, process_groups) # group by process
+    hdict[hname] = hdict[hname][:, :, args.lepton].integrate('leptype') # only pick out specified lepton
     
 
     ## make bp plots
-for hname in bp_variables.keys():
-    if hname not in hdict.keys():
-        raise ValueError("%s not found in file" % hname)
-    #set_trace()
-    histo = hdict[hname][:, :, args.lepton, :, :, :].integrate('leptype') # process, jmult, leptype, lepcat, btag, object type
-
-    if histo.dense_dim() == 1:
+if ((args.plot == 'all') or (args.plot == 'bp')):
+    for hname in bp_variables.keys():
+        if hname not in hdict.keys():
+            raise ValueError("%s not found in file" % hname)
+        histo = hdict[hname]
+    
         xtitle, rebinning, x_lims = bp_variables[hname]
         if rebinning != 1:
             xaxis_name = histo.dense_axes()[0].name
             histo = histo.rebin(xaxis_name, rebinning)
-
-        #set_trace()
-        ## hists should have 3 category axes (dataset, jet multiplicity, lepton category) followed by variable
-        for jmult in list(set([key[1] for key in histo.values().keys()])):
-            for lepcat in list(set([key[3] for key in histo.values().keys()])):
-                for btagregion in list(set([key[2] for key in histo.values().keys()])):
-                    pltdir = '/'.join([outdir, args.lepton, jmult, lepcat, btagregion])
+    
+        ## hists should have 3 category axes (process, jet multiplicity, lepton category) followed by variable
+        for jmult in sorted(set([key[1] for key in histo.values().keys()])):
+            for lepcat in sorted(set([key[3] for key in histo.values().keys()])):
+                for btagregion in sorted(set([key[2] for key in histo.values().keys()])):
+                    pltdir = os.path.join(outdir, args.lepton, jmult, lepcat, btagregion)
                     if not os.path.isdir(pltdir):
                         os.makedirs(pltdir)
-
-                    for obj in sorted(list(set([key[4] for key in histo.values().keys()]))):
+    
+                    for obj in sorted(set([key[4] for key in histo.values().keys()])):
                         objlabel, mass_range = objects[obj]
                         new_xtitle = xtitle.replace('obj', objlabel)
                         if ('mass' in hname) and (hname != 'Reso_mass'):
                             x_lims = mass_range
-
+    
                         fig, ax = plt.subplots()
                         fig.subplots_adjust(hspace=.07)
-
+    
                         #set_trace()
                         hslice = histo[:, jmult, btagregion, lepcat, obj].integrate('jmult').integrate('lepcat').integrate('btag').integrate('objtype')
-                        ax = Plotter.plot_mc1d(ax, hslice, xlabel=new_xtitle, xlimits=x_lims)
-
+                        #hslice = histo['ttJets*', jmult, btagregion, lepcat, obj].integrate('jmult').integrate('lepcat').integrate('btag').integrate('objtype')
+                        Plotter.plot_mc1d(ax, hslice, xlabel=new_xtitle, xlimits=x_lims, ylabel='Events')
+    
                             # add lepton/jet multiplicity label
-                        #set_trace()
                         ax.text(
-                            0.02, 0.90, "%s, %s\n%s" % (lep_cats[lepcat], jet_mults[jmult], btag_cats[btagregion]),
-                            fontsize=rcParams['font.size']*0.75, 
-                            horizontalalignment='left', 
-                            verticalalignment='bottom', 
-                            transform=ax.transAxes
+                            0.02, 0.88, "%s, %s\n%s" % (lep_cats[lepcat], jet_mults[jmult], btag_cats[btagregion]),
+                            horizontalalignment='left', verticalalignment='bottom', transform=ax.transAxes
                         )
-                        ax = hep.cms.cmslabel(ax=ax, data=False, paper=False, year=args.year, lumi=round(data_lumi_year['%ss' % args.lepton]/1000., 1))
-
+                        hep.cms.label(ax=ax, data=False, paper=False, year=args.year, lumi=round(data_lumi_year['%ss' % args.lepton]/1000., 1))
+    
                         #set_trace()
-                        figname = '%s/%s' % (pltdir, '_'.join([jmult, args.lepton, lepcat, btagregion, hname, obj]))
+                        figname = os.path.join(pltdir, '_'.join([jmult, args.lepton, lepcat, btagregion, hname, obj]))
                         fig.savefig(figname)
                         print('%s written' % figname)
                         plt.close()
 
+    ## make kinematic plots
+if ((args.plot == 'all') or (args.plot == 'kin')):
+    for hname in variables.keys():
+        if hname not in hdict.keys():
+            raise ValueError("%s not found in file" % hname)
+        #set_trace()
 
-    ## make other plots
-for hname in variables.keys():
-    if hname not in hdict.keys():
-        raise ValueError("%s not found in file" % hname)
-    #set_trace()
-    histo = hdict[hname][:, :, args.lepton, :, :].integrate('leptype') # process, jmult, leptype, lepcat, btag, object type
-
-    if histo.dense_dim() == 1:
+        histo = hdict[hname]
+    
         xtitle, rebinning, x_lims, withData = variables[hname]
         if rebinning != 1:
             xaxis_name = histo.dense_axes()[0].name
             histo = histo.rebin(xaxis_name, rebinning)
-
-        #set_trace()
+    
         ## hists should have 3 category axes (dataset, jet multiplicity, lepton category) followed by variable
-        for jmult in list(set([key[1] for key in histo.values().keys()])):
-            for lepcat in list(set([key[3] for key in histo.values().keys()])):
-                for btagregion in list(set([key[2] for key in histo.values().keys()])):
-                    pltdir = '/'.join([outdir, args.lepton, jmult, lepcat, btagregion])
+        for jmult in sorted(set([key[1] for key in histo.values().keys()])):
+            for lepcat in sorted(set([key[3] for key in histo.values().keys()])):
+                for btagregion in sorted(set([key[2] for key in histo.values().keys()])):
+                    pltdir = os.path.join(outdir, args.lepton, jmult, lepcat, btagregion)
                     if not os.path.isdir(pltdir):
                         os.makedirs(pltdir)
-
-                    fig, ax = plt.subplots()
+    
+                    fig, (ax, rax) = plt.subplots(2, 1, gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
                     fig.subplots_adjust(hspace=.07)
-
-                    #set_trace()
+    
                     hslice = histo[:, jmult, btagregion, lepcat].integrate('jmult').integrate('lepcat').integrate('btag')
-
+                    Plotter.plot_stack1d(ax, rax, hslice, xlabel=xtitle, xlimits=x_lims, **{'maskData': not withData})
+    
                     if hname == 'Jets_njets':
                         print(jmult)
                         yields_txt, yields_json = Plotter.get_samples_yield_and_frac(hslice, data_lumi_year['%ss' % args.lepton]/1000.)
-                        plt_tools.print_table(yields_txt, filename='%s/%s_%s_yields_and_fracs.txt' % (pltdir, jmult, args.lepton), print_output=True)
-                        with open('%s/%s_%s_yields_and_fracs.json' % (pltdir, jmult, args.lepton), 'w') as out:
+                        yields_fname = os.path.join(pltdir, '%s_%s_yields_and_fracs' % (jmult, args.lepton))
+                        plt_tools.print_table(yields_txt, filename='%s.txt' % yields_fname, print_output=True)
+                        with open('%s.json' % yields_fname, 'w') as out:
                             out.write(prettyjson.dumps(yields_json))
 
-                    ax = Plotter.plot_mc1d(ax, hslice, xlabel=xtitle, xlimits=x_lims)
-
                         # add lepton/jet multiplicity label
-                    #set_trace()
                     ax.text(
-                        0.02, 0.90, "%s, %s\n%s" % (lep_cats[lepcat], jet_mults[jmult], btag_cats[btagregion]),
-                        fontsize=rcParams['font.size']*0.75, 
-                        horizontalalignment='left', 
-                        verticalalignment='bottom', 
-                        transform=ax.transAxes
+                        0.02, 0.88, "%s, %s\n%s" % (lep_cats[lepcat], jet_mults[jmult], btag_cats[btagregion]),
+                        horizontalalignment='left', verticalalignment='bottom', transform=ax.transAxes
                     )
-                    ax = hep.cms.cmslabel(ax=ax, data=False, paper=False, year=args.year, lumi=round(data_lumi_year['%ss' % args.lepton]/1000., 1))
-
+                    hep.cms.label(ax=ax, data=withData, paper=False, year=args.year, lumi=round(data_lumi_year['%ss' % args.lepton]/1000., 1))
+    
                     #set_trace()
-                    figname = '%s/%s' % (pltdir, '_'.join([jmult, args.lepton, lepcat, btagregion, hname]))
+                    figname = os.path.join(pltdir, '_'.join([jmult, args.lepton, lepcat, btagregion, hname]))
                     fig.savefig(figname)
                     print('%s written' % figname)
                     plt.close()
-
-
