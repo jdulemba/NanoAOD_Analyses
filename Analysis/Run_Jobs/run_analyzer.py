e01526e7139768e3182351578c2d15e7e8fd73cc
jdulemba <josdrw9@gmail.com>
2021-04-09 17:19:05 +0200

    All options passed to analyzers through '--opts' argparse now. Work in progress

--- Analysis/Run_Jobs/run_analyzer.py
+++ Analysis/Run_Jobs/run_analyzer.py
@@ -6,28 +6,41 @@ import Run_Jobs.tools as tools
 import Utilities.prettyjson as prettyjson
 import fnmatch
 
-from argparse import ArgumentParser
-parser = ArgumentParser('submit analyzer to the batch queues')
+import argparse
+class ParseKwargs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, dict())
+        for value in values:
+            key, value = value.split('=')
+            getattr(namespace, self.dest)[key] = value
+
+parser = argparse.ArgumentParser('submit analyzer to the batch queues')
 parser.add_argument('analyzer', help='Analyzer to use.')
 parser.add_argument('frange', type=str, help='Specify start:stop indices for files, inclusive. 0:1 means valid files in indices 0 and 1 will be used')
 parser.add_argument('year', choices=['2016APV', '2016', '2017', '2018'], help='Specify which year to run over')
-parser.add_argument('--sample', type=str, help='String used to specify which samples to run, can be name or regex.')
-parser.add_argument('--outfname', type=str, help='Specify output filename, including directory and file extension')
-parser.add_argument('--debug', action='store_true', help='Uses iterative_executor for debugging purposes, otherwise futures_excutor will be used (faster)')
-parser.add_argument('--signal', type=str, default='.*', help='Signal sample to use, regex formatting.')
-parser.add_argument('--evt_sys', type=str, default='NONE', help='Specify event systematics to run, will be capitalized. Default is NONE')
-parser.add_argument('--rewt_sys', type=str, default='NONE', help='Specify reweighting systematics to run, will be capitalized. Default is NONE')
-parser.add_argument('--only_sys', type=int, default=0, help='Only run specified systematics and not nominal weights (nosys)')
+parser.add_argument('--opts', nargs='*', action=ParseKwargs, help='Options to pass to analyzers.')
+#parser.add_argument('--sample', type=str, help='String used to specify which samples to run, can be name or regex.')
+#parser.add_argument('--outfname', type=str, help='Specify output filename, including directory and file extension')
+#parser.add_argument('--debug', action='store_true', help='Uses iterative_executor for debugging purposes, otherwise futures_excutor will be used (faster)')
+#parser.add_argument('--signal', type=str, default='.*', help='Signal sample to use, regex formatting.')
+#parser.add_argument('--evt_sys', type=str, default='NONE', help='Specify event systematics to run, will be capitalized. Default is NONE')
+#parser.add_argument('--rewt_sys', type=str, default='NONE', help='Specify reweighting systematics to run, will be capitalized. Default is NONE')
+#parser.add_argument('--only_sys', type=int, default=0, help='Only run specified systematics and not nominal weights (nosys)')
 args = parser.parse_args()
 
 proj_dir = os.environ['PROJECT_DIR']
 jobid = os.environ['jobid']
 base_jobid = os.environ['base_jobid']
 analyzer=args.analyzer
+#set_trace()
+
+# define dictionary of options to pass
+opts_dict = args.opts
 
     ## get samples to use
 indir = os.path.join(proj_dir, 'inputs', '%s_%s' % (args.year, base_jobid))
-samples_to_use = tools.get_sample_list(indir=indir, sample=args.sample) if args.sample else tools.get_sample_list(indir=indir, text_file='analyzer_inputs.txt')
+samples_to_use = tools.get_sample_list(indir=indir, sample=opts_dict['sample']) if 'sample' in opts_dict.keys() else tools.get_sample_list(indir=indir, text_file='analyzer_inputs.txt')
+#samples_to_use = tools.get_sample_list(indir=indir, sample=args.sample) if args.sample else tools.get_sample_list(indir=indir, text_file='analyzer_inputs.txt')
 
 fileset = {}
 for sample in samples_to_use:
@@ -61,62 +74,92 @@ print(fileset)
 #set_trace()
 if (args.frange).lower() == 'all':
     outdir = os.path.join(proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer)
-    if args.outfname:
-        cfname = args.outfname
-    elif args.sample:
-        cfname = '%s/%s.coffea' % (outdir, args.sample)
+    if 'outfname' in opts_dict.keys():
+        cfname = opts_dict['outfname']
+    #if args.outfname:
+    #    cfname = args.outfname
+    elif 'sample' in opts_dict.keys():
+        cfname = os.path.join(outdir, '%s.coffea' % opts_dict['sample'])
+    #elif args.sample:
+    #    cfname = os.path.join(outdir, '%s.coffea' % args.sample)
     else:
-        cfname = '%s/test_%s.coffea' % (outdir, analyzer)
+        cfname = os.path.join(outdir,'test_%s.coffea' % analyzer)
 else:
     if ':' in args.frange:
         outdir = os.path.join(proj_dir, 'results', '%s_%s' % (args.year, jobid), analyzer)
-        if args.outfname:
-            cfname = args.outfname
-        elif args.signal and args.sample and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
-        #elif args.signal and args.sample and analyzer == 'htt_signal_reweight':
-            cfname = '%s/%s_%s_%sto%s.coffea' % (outdir, args.signal, args.sample, file_start, file_stop) if not args.signal == parser.get_default('signal') else '%s/AHtoTT_%s_%sto%s.coffea' % (outdir, args.sample, file_start, file_stop)
-        elif args.sample:
-            cfname = '%s/%s_%sto%s.coffea' % (outdir, args.sample, file_start, file_stop)
+        if 'outfname' in opts_dict.keys():
+            cfname = opts_dict['outfname']
+        #if args.outfname:
+        #    cfname = args.outfname
+        elif ('signal' in opts_dict.keys()) and ('sample' in opts_dict.keys()) and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
+            cfname = os.path.join(outdir,'%s_%s_%sto%s.coffea' % (opts_dict['signal'], opts_dict['sample'], file_start, file_stop)) if not opts_dict['signal'] == ".*" else os.path.join(outdir, 'AHtoTT_%s_%sto%s.coffea' % (opts_dict['sample'], file_start, file_stop))
+        #elif args.signal and args.sample and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
+        ##elif args.signal and args.sample and analyzer == 'htt_signal_reweight':
+        #    cfname = '%s/%s_%s_%sto%s.coffea' % (outdir, args.signal, args.sample, file_start, file_stop) if not args.signal == parser.get_default('signal') else '%s/AHtoTT_%s_%sto%s.coffea' % (outdir, args.sample, file_start, file_stop)
+        elif 'sample' in opts_dict.keys():
+            cfname = os.path.join(outdir, '%s_%sto%s.coffea' % (opts_dict['sample'], file_start, file_stop))
+        #elif args.sample:
+        #    cfname = '%s/%s_%sto%s.coffea' % (outdir, args.sample, file_start, file_stop)
         else:
-            cfname = '%s/test_%sto%s.coffea' % (outdir, file_start, file_stop)
+            cfname = os.path.join(outdir, 'test_%sto%s.coffea' % (file_start, file_stop))
     else:
         outdir = proj_dir
-        if args.outfname:
-            cfname = args.outfname
-        elif args.signal and args.sample and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
-        #elif args.signal and args.sample and analyzer == 'htt_signal_reweight':
-            cfname = '%s/%s_%s_%s_%s.test.coffea' % (outdir, args.signal, args.sample, args.year, analyzer) if not args.signal == parser.get_default('signal') else '%s/AHtoTT_%s_%s_%s.test.coffea' % (outdir, args.sample, args.year, analyzer)
-        elif args.sample:
-            cfname = '%s/%s_%s_%s.test.coffea' % (outdir, args.sample, args.year, analyzer)
+        if 'outfname' in opts_dict.keys():
+            cfname = opts_dict['outfname']
+        #if args.outfname:
+        #    cfname = args.outfname
+        elif ('signal' in opts_dict.keys()) and ('sample' in opts_dict.keys()) and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
+            cfname = os.path.join(outdir, '%s_%s_%s_%s.test.coffea' % (opts_dict['signal'], opts_dict['sample'], args.year, analyzer)) if not opts_dict['signal'] == ".*" else os.path.join(outdir, 'AHtoTT_%s_%s_%s.test.coffea' % (opts_dict['sample'], args.year, analyzer))
+        #elif args.signal and args.sample and ((analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation')):
+        ##elif args.signal and args.sample and analyzer == 'htt_signal_reweight':
+        #    cfname = '%s/%s_%s_%s_%s.test.coffea' % (outdir, args.signal, args.sample, args.year, analyzer) if not args.signal == parser.get_default('signal') else '%s/AHtoTT_%s_%s_%s.test.coffea' % (outdir, args.sample, args.year, analyzer)
+        elif 'sample' in opts_dict.keys():
+            cfname = os.path.join(outdir, '%s_%s_%s.test.coffea' % (opts_dict['sample'], args.year, analyzer))
+        #elif args.sample:
+        #    cfname = '%s/%s_%s_%s.test.coffea' % (outdir, args.sample, args.year, analyzer)
         else:
-            cfname = '%s/%s_%s.test.coffea' % (outdir, args.year, analyzer)
+            cfname = os.path.join(outdir, '%s_%s.test.coffea' % (args.year, analyzer))
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
+#set_trace()
     ## get string for passed options
+to_debug = bool(opts_dict.get('debug'))
+evt_sys = opts_dict.get('evt_sys', 'NONE')
+rewt_sys = opts_dict.get('rewt_sys', 'NONE')
+only_sys = opts_dict.get('only_sys', 0)
+
 opts = ""
-#if args.sample: opts += " --sample=%s" % args.sample
-if args.debug: opts += " --debug"
+##if args.sample: opts += " --sample=%s" % args.sample
+#if args.debug: opts += " --debug"
+if to_debug: opts += " --debug"
 
 if (analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation'):
 #if analyzer == 'htt_signal_reweight':
-    if args.signal is None:
+    signal = opts_dict.get('signal', None)
+    if signal is None:
+    #if args.signal is None:
         raise ValueError("Signal sample must be specified when running %s" % analyzer)
     opts += " --evt_sys={EVTSYS} --rewt_sys={REWTSYS}".format(
-            EVTSYS=args.evt_sys.upper(),
-            REWTSYS=args.rewt_sys.upper(),
+            #EVTSYS=args.evt_sys.upper(),
+            #REWTSYS=args.rewt_sys.upper(),
+            EVTSYS=evt_sys.upper(),
+            REWTSYS=rewt_sys.upper(),
         )
-    opts += " --only_sys=%i" % args.only_sys
+    opts += " --only_sys=%i" % only_sys
+    #opts += " --only_sys=%i" % args.only_sys
     run_cmd = """python {PROJDIR}/bin/{ANALYZER}.py "{FSET}" {YEAR} {OUTFNAME} --signal={SIGNAL} {OPTS}""".format(
             PROJDIR=proj_dir,
             ANALYZER=analyzer,
             FSET=fileset,
             YEAR=args.year,
-            SIGNAL=args.signal,
+            SIGNAL=signal,
+            #SIGNAL=args.signal,
             OUTFNAME=cfname,
             OPTS=opts
     )
-elif analyzer == 'htt_btag_iso_cut':
+#elif analyzer == 'htt_btag_iso_cut':
+elif (analyzer == 'htt_btag_iso_cut') or (analyzer == 'evtWeights'):
     opts += " --evt_sys={EVTSYS} --rewt_sys={REWTSYS}".format(
             EVTSYS=args.evt_sys.upper(),
             REWTSYS=args.rewt_sys.upper(),
@@ -143,13 +186,15 @@ elif analyzer == 'ttbar_post_alpha_reco':
             OPTS=opts
     )
 else:
-    run_cmd = """python {PROJDIR}/bin/{ANALYZER}.py "{FSET}" {YEAR} {OUTFNAME} {OPTS}""".format(
+    #run_cmd = """python {PROJDIR}/bin/{ANALYZER}.py "{FSET}" {YEAR} {OUTFNAME} {OPTS}""".format(
+    run_cmd = """python {PROJDIR}/bin/{ANALYZER}.py "{FSET}" {YEAR} {OUTFNAME} "{OPTS}" """.format(
             PROJDIR=proj_dir,
             ANALYZER=analyzer,
             FSET=fileset,
             YEAR=args.year,
             OUTFNAME=cfname,
-            OPTS=opts
+            OPTS=opts_dict
+            #OPTS=opts
     )
 
 print('Running command: %s' % run_cmd)
