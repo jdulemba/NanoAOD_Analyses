8ea898ccdd3319f48ff4aa5036503e90ebc9a3c2
jdulemba <josdrw9@gmail.com>
2021-09-09 10:12:07 +0200

    Removed input args that aren't needed anymore, cleaned up creation of condor.jdl file

--- Analysis/Run_Jobs/condor_submit.py
+++ Analysis/Run_Jobs/condor_submit.py
@@ -11,43 +11,38 @@ class ParseKwargs(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, dict())
         for value in values:
-            key, value = value.split('=')
+            key, value = value.split("=")
             getattr(namespace, self.dest)[key] = value
 
-parser = argparse.ArgumentParser('submit analyzer to the batch queues')
-parser.add_argument('analyzer', help='Analyzer to use.')
-parser.add_argument('jobdir', help='Directory name to be created in nobackup area.')
-parser.add_argument('year', choices=['2016APV', '2016', '2017', '2018'], help='Specify which year to run over')
-parser.add_argument('--opts', nargs='*', action=ParseKwargs, help='Options to pass to analyzers.')
-parser.add_argument('--submit', action='store_true', help='Submit jobs')
+parser = argparse.ArgumentParser("submit analyzer to the batch queues")
+parser.add_argument("analyzer", help="Analyzer to use.")
+parser.add_argument("jobdir", help="Directory name to be created in nobackup area.")
+parser.add_argument("year", choices=["2016APV", "2016", "2017", "2018"], help="Specify which year to run over")
+parser.add_argument("--opts", nargs="*", action=ParseKwargs, help="Options to pass to analyzers.")
+parser.add_argument("--submit", action="store_true", help="Submit jobs")
 args = parser.parse_args()
 
-#set_trace()
 # define dictionary of options to pass
 opts_dict = {} if args.opts is None else args.opts
-opts_dict['apply_hem'] = opts_dict.get('apply_hem', 'True')
-opts_dict['signal'] = opts_dict.get('signal', ".*")
-opts_dict['evt_sys'] = opts_dict.get('evt_sys', 'NONE')
-opts_dict['rewt_sys'] = opts_dict.get('rewt_sys', 'NONE')
-opts_dict['only_sys'] = opts_dict.get('only_sys', 'False')
-opts_dict['debug'] = opts_dict.get('debug', 'False')
+opts_dict["apply_hem"] = opts_dict.get("apply_hem", "True")
+opts_dict["evt_sys"] = opts_dict.get("evt_sys", "NONE")
+opts_dict["rewt_sys"] = opts_dict.get("rewt_sys", "NONE")
+opts_dict["only_sys"] = opts_dict.get("only_sys", "False")
+opts_dict["debug"] = opts_dict.get("debug", "False")
+sample = opts_dict.get("sample", None)
+if sample: opts_dict.pop("sample")
 
-opts_dict['base_jobid'] = opts_dict.get('base_jobid', os.environ['base_jobid'])
-opts_dict['jobid'] = opts_dict.get('jobid', os.environ['jobid'])
-if not opts_dict['base_jobid'] in opts_dict['jobid']:
-    raise ValueError("base_jobid=%s must be in jobid=%s" % (opts_dict['base_jobid'], opts_dict['jobid']))
-
-proj_dir = os.environ['PROJECT_DIR']
-jobid = opts_dict['jobid']
-base_jobid = opts_dict['base_jobid']
-nano_dir = os.environ['NANODIR']
+proj_dir = os.environ["PROJECT_DIR"]
+jobid = os.environ["jobid"]
+base_jobid = os.environ["base_jobid"]
+nano_dir = os.environ["NANODIR"]
 analyzer=args.analyzer
-proxy_path = '/afs/cern.ch/work/j/jdulemba/private/x509up_u81826'
+proxy_path = "/afs/cern.ch/work/j/jdulemba/private/x509up_u81826"
     # get jobdir
 year, month, day = time.localtime().tm_year, time.localtime().tm_mon, time.localtime().tm_mday
 dtime = datetime.datetime(year, month, day)
 dtime.strftime("%d%B%Y")
-jobdir = '_'.join([args.jobdir, dtime.strftime("%d%B%Y"), args.year, jobid])
+jobdir = "_".join([args.jobdir, dtime.strftime("%d%B%Y"), args.year, jobid])
 jobdir = "BATCH_%s" % jobdir if not jobdir.startswith("BATCH") else jobdir
 print("%s written" % (os.path.join(proj_dir, jobdir)))
 
@@ -82,99 +77,58 @@ Proxy_path = {PROXYPATH}
 """.format(BATCHDIR=batch_dir, PROXYPATH=proxy_path)
     return condorfile
 
-def add_signal_condor_jobs(idx, frange, sample, signal):
-    sig_outname = 'AHtoTT' if signal == parser.get_default('signal') else signal
-    condorfile = """
-Output = con_{IDX}.stdout
-Error = con_{IDX}.stderr
-Log = con_{IDX}.log
-Arguments = $(Proxy_path) {ANALYZER} {FRANGE} {YEAR} --sample={SAMPLE} --signal={SIGNAL} --evt_sys={EVTSYS} --rewt_sys={REWTSYS} --only_sys={ONLYSYS} --outfname={BATCHDIR}/{SAMPLE}_{SIGOUTNAME}_out_{IDX}.coffea
-Queue
-""".format(IDX=idx, ANALYZER=analyzer, FRANGE=frange, YEAR=args.year, SIGNAL=signal, SAMPLE=sample, EVTSYS=args.evt_sys, REWTSYS=args.rewt_sys, ONLYSYS=args.only_sys, BATCHDIR=batch_dir, SIGOUTNAME=sig_outname)
-    return condorfile
 
-def add_condor_jobs(idx, frange, opts):
+def add_condor_jobs(idx, frange, sample, opts):
     condorfile = """
 Output = con_{IDX}.stdout
 Error = con_{IDX}.stderr
 Log = con_{IDX}.log
-Arguments = $(Proxy_path) {ANALYZER} {FRANGE} {YEAR} --opts {OPTS}
+Arguments = $(Proxy_path) {ANALYZER} {FRANGE} {YEAR} {SAMPLE} --opts {OPTS}
 Queue
-""".format(IDX=idx, ANALYZER=analyzer, FRANGE=frange, YEAR=args.year, OPTS=opts)
+""".format(IDX=idx, ANALYZER=analyzer, FRANGE=frange, YEAR=args.year, SAMPLE=sample, OPTS=opts)
     return condorfile
 
     ## get samples to use
-indir = os.path.join(proj_dir, 'inputs', '%s_%s' % (args.year, base_jobid))
-#set_trace()
-samples_to_use = tools.get_sample_list(indir=indir, sample=opts_dict['sample']) if 'sample' in opts_dict.keys() else tools.get_sample_list(indir=indir, text_file='analyzer_inputs.txt')
+indir = os.path.join(proj_dir, "inputs", "%s_%s" % (args.year, base_jobid))
+samples_to_use = tools.get_sample_list(indir=indir, sample=sample) if sample else tools.get_sample_list(indir=indir, text_file="analyzer_inputs.txt")
 for sample in samples_to_use:
     if not os.path.isfile(sample):
-        raise IOError("Sample file %s.txt not found" % sample)
+        raise IOError(f"Sample file {sample}.txt not found")
 
-    sample_name = sample.split('/')[-1].split('.')[0]
+    sample_name = sample.split("/")[-1].split(".")[0]
 
         # add output, error, log, arguments for each job splitting
-    sfiles = open(sample, 'r')
-    file_inds = [idx for idx, fname in enumerate([fname.strip('\n') for fname in sfiles if not fname.startswith('#')])]
+    sfiles = open(sample, "r")
+    file_inds = [idx for idx, fname in enumerate([fname.strip("\n") for fname in sfiles if not fname.startswith("#")])]
     splitting = tools.get_file_splitting(sample=sample_name, analyzer=analyzer)
-    #splitting = tools.get_file_splitting('AtoTT') if analyzer == 'htt_signal_reweight' else tools.get_file_splitting(sample.split('/')[-1].split('.')[0])
     file_chunks = list(tools.get_file_range(file_inds, splitting))
 
-    if (analyzer == 'htt_signal_reweight') or (analyzer == 'signal_validation'):
-            ## make batch_job.sh file
-        batch_dir = '%s/%s/%s_%s' % (proj_dir, jobdir, sample_name, args.signal) if not args.signal == parser.get_default('signal') else '%s/%s/%s_AHtoTT' % (proj_dir, jobdir, sample_name)
-        if not os.path.isdir(batch_dir): os.makedirs(batch_dir)
-        batch_cmd = create_batch_job()
-        batch_conf = open(os.path.join(batch_dir, 'batch_job.sh'), 'w')
-        batch_conf.write(batch_cmd)
-        batch_conf.close()
-        
-            ## make condor.jdl file
-        condor_cmd = base_condor_jdl()
+        ## make batch_job.sh file
+    batch_dir = os.path.join(proj_dir, jobdir, sample_name)
+    if not os.path.isdir(batch_dir): os.makedirs(batch_dir)
+    batch_cmd = create_batch_job()
+    batch_conf = open(os.path.join(batch_dir, "batch_job.sh"), "w")
+    batch_conf.write(batch_cmd)
+    batch_conf.close()
+    
+        ## make condor.jdl file
+    condor_cmd = base_condor_jdl()
+    for idx, chunk in enumerate(file_chunks):
+            # make list of options to pass to analyzers
+        tmp_opts_dict = deepcopy(opts_dict)
+        tmp_opts_dict["outfname"] = os.path.join(batch_dir, "%s_out_%s.coffea" % (sample_name, idx))
+        opts_list = ["%s=%s" % (key, val) for key, val in tmp_opts_dict.items()]
 
-        for idx, chunk in enumerate(file_chunks):
-            condor_cmd += add_signal_condor_jobs(idx, chunk, sample.split('/')[-1].split('.')[0], args.signal)
+        condor_cmd += add_condor_jobs(idx, chunk, sample_name, " ".join(opts_list))
 
-        condor_conf = open(os.path.join(batch_dir, 'condor.jdl'), 'w')
-        condor_conf.write(condor_cmd)
-        condor_conf.close()
+    condor_conf = open(os.path.join(batch_dir, "condor.jdl"), "w")
+    condor_conf.write(condor_cmd)
+    condor_conf.close()
 
-        #set_trace()
-        # submit job
-        if args.submit:
-            orig_dir = os.getcwd()
-            print('\nSubmitting jobs for %s' % args.signal) if not args.signal == parser.get_default('signal') else print('\nSubmitting jobs for all signal')
-            os.system('cd ' + batch_dir + ' && condor_submit condor.jdl')
-            os.system('cd ' + orig_dir)
-
-    else:
-            ## make batch_job.sh file
-        batch_dir = os.path.join(proj_dir, jobdir, sample_name)
-        if not os.path.isdir(batch_dir): os.makedirs(batch_dir)
-        batch_cmd = create_batch_job()
-        batch_conf = open(os.path.join(batch_dir, 'batch_job.sh'), 'w')
-        batch_conf.write(batch_cmd)
-        batch_conf.close()
-        
-            ## make condor.jdl file
-        condor_cmd = base_condor_jdl()
-        for idx, chunk in enumerate(file_chunks):
-                # make list of options to pass to analyzers
-            tmp_opts_dict = deepcopy(opts_dict)
-            tmp_opts_dict['sample'] = sample_name
-            tmp_opts_dict['outfname'] = os.path.join(batch_dir, '%s_out_%s.coffea' % (sample_name, idx))
-            opts_list = ["%s=%s" % (key, val) for key, val in tmp_opts_dict.items()]
-
-            condor_cmd += add_condor_jobs(idx, chunk, " ".join(opts_list))
-
-        condor_conf = open(os.path.join(batch_dir, 'condor.jdl'), 'w')
-        condor_conf.write(condor_cmd)
-        condor_conf.close()
-
-        # submit job
-        if args.submit:
-            orig_dir = os.getcwd()
-            print('\nSubmitting jobs for %s' % sample_name)
-            os.system('cd ' + batch_dir + ' && condor_submit condor.jdl')
-            os.system('cd ' + orig_dir)
+    # submit job
+    if args.submit:
+        orig_dir = os.getcwd()
+        print(f"\nSubmitting jobs for {sample_name}")
+        os.system("cd " + batch_dir + " && condor_submit condor.jdl")
+        os.system("cd " + orig_dir)
 
