e77a872d3d63d53bbc4a95a9bed3051f1ddce166
jdulemba <josdrw9@gmail.com>
2022-01-10 12:03:18 +0100

    Have different job splittings for different analyzers

--- Analysis/Run_Jobs/tools.py
+++ Analysis/Run_Jobs/tools.py
@@ -13,17 +13,17 @@ def get_sample_list(indir, sample=None, text_file=None):
         ## get samples to use
     if sample:
         ## match sample to any available in 'indir'
-        fname_opts = [fname.split('.')[0] for fname in os.listdir(indir) if fname.endswith('.txt')]
-        samples = ['%s/%s.txt' % (indir, name) for name in fname_opts if fnmatch.fnmatch(name, sample)]
+        fname_opts = [fname.split(".")[0] for fname in os.listdir(indir) if fname.endswith(".txt")]
+        samples = ["%s/%s.txt" % (indir, name) for name in fname_opts if fnmatch.fnmatch(name, sample)]
 
     else:
             ## get file that has names for all datasets to use
         fpath = os.path.join(indir, text_file)
         if not os.path.isfile(fpath):
-            raise IOError("File %s not found" % fpath)
+            raise IOError(f"File {fpath} not found")
     
-        txt_file = open(fpath, 'r')
-        samples = ['%s/%s.txt' % (indir, sample.strip('\n')) for sample in txt_file if not sample.startswith('#')]
+        txt_file = open(fpath, "r")
+        samples = ["%s/%s.txt" % (indir, sample.strip("\n")) for sample in txt_file if not sample.startswith("#")]
 
     if not samples:
         raise ValueError("No samples found to match %s" % sample)
@@ -36,27 +36,31 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 def get_file_range(lst, n):
-    '''
+    """
     Input: list of integers to be broken up into groups of size n
-    '''
+    """
     file_chunks = list(chunks(lst, n))
-    franges = ['%i:%i' % (chunk[0], chunk[-1]) for chunk in file_chunks]
+    franges = ["%i:%i" % (chunk[0], chunk[-1]) for chunk in file_chunks]
     return franges
 
 def get_file_splitting(sample, analyzer):
-    if analyzer == 'meta_info':
+    if (analyzer == "meta_info") or ("check_" in analyzer):
         splittings = splitting_dicts.meta_dict
-    elif analyzer == 'htt_flav_effs':
+    elif analyzer == "htt_flav_effs":
         splittings = splitting_dicts.hflav_dict
+    elif (analyzer == "ttbar_alpha_reco") or (analyzer == "ttbar_post_alpha_reco"):
+        splittings = splitting_dicts.alpha_dict
+    elif analyzer == "permProbComputer":
+        splittings = splitting_dicts.perm_dict
     else:
         splittings = splitting_dicts.other_dict
 
-    if '*' in splittings.keys():
-        f_split = splittings['*']
-    elif b'*' in splittings.keys():
-        f_split = splittings[b'*']
+    if "*" in splittings.keys():
+        f_split = splittings["*"]
+    elif b"*" in splittings.keys():
+        f_split = splittings[b"*"]
     for pattern, splitting in splittings.items():
-        if pattern == b'*': continue
+        if pattern == b"*": continue
         if fnmatch.fnmatch(sample, pattern):
             f_split = splitting
     return f_split
