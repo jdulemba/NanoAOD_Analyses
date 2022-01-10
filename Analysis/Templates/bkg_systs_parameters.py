cc541107653bf759f6faa186b25224d7c1a22716
jdulemba <josdrw9@gmail.com>
2022-01-10 12:07:05 +0100

    Split systs_parameters into bkg and signal

---
+++ Analysis/Templates/bkg_systs_parameters.py
@@ -0,0 +1,81 @@
+what_to_do = {
+    "BTAG_BC" : {
+        "TT" : {"m3" : "raw", "m4" : "raw", "e3" : "raw", "e4" : "raw"},
+        "TB" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TW" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TQ" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "BTAG_L" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TB" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TW" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TQ" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+    },
+    "FACTOR" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "FSR" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "HDAMP" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "ISR" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "JER" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TB" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TW" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TQ" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+    },
+    "JES" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TB" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        #"TB" : {"m3" : "smooth", "m4" : "flat", "e3" : "smooth", "e4" : "flat"},
+        "TW" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TQ" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "LEP_RECO" : {
+        "TT" : {"m3" : "raw", "m4" : "raw", "e3" : "raw", "e4" : "raw"},
+        "TB" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TW" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TQ" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "LEP_TRIG" : {
+        "TT" : {"m3" : "raw", "m4" : "raw", "e3" : "raw", "e4" : "raw"},
+        "TB" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TW" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TQ" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "MET" : {
+        "TT" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TB" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TW" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TQ" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+    },
+    "MTOP3GEV" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "PILEUP" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TB" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TW" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+        "TQ" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+    },
+    "PREFIRE" : {
+        "TT" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TB" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TW" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+        "TQ" : {"m3" : "smooth", "m4" : "smooth", "e3" : "smooth", "e4" : "smooth"},
+    },
+    "RENORM" : {
+        "TT" : {"m3" : "raw", "m4" : "raw", "e3" : "raw", "e4" : "raw"},
+    },
+    "UE" : {
+        "TT" : {"m3" : "flat", "m4" : "flat", "e3" : "flat", "e4" : "flat"},
+    },
+    "deltaQCDdeltaEW" : {
+        "TT" : {"m3" : "raw", "m4" : "raw", "e3" : "raw", "e4" : "raw"},
+    },
+}
