bfb4a850866ab38470a0a9559bdd9d812e6ba7e4
jdulemba <josdrw9@gmail.com>
2022-01-10 15:47:50 +0100

    Sets environment paths when in virtual environment

---
+++ Analysis/venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Project-related environment variables
+export PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+
+#This part should be changed by the user(s)
+export jobid=NOTSET
+if [ -e $PROJECT_DIR/jobid.sh ]
+then
+        source $PROJECT_DIR/jobid.sh
+        echo "set jobid: $jobid"
+        echo "set nanoAOD version: $base_jobid"
+else
+        echo "I did not find jobid.sh, are you sure you do not want to set the jobid and leave it to $jobid?"
+fi
+
+#HERE ARE LIONS!
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+
+export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
+export LD_LIBRARY_PATH=$PROJECT_DIR/compiled:$LD_LIBRARY_PATH
+
+python --version
