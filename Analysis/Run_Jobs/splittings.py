84fd7cc14e3e87fc144f651e957da2d09f3ee466
jdulemba <josdrw9@gmail.com>
2022-01-10 12:02:59 +0100

    Have different job splittings for different analyzers

--- Analysis/Run_Jobs/splittings.py
+++ Analysis/Run_Jobs/splittings.py
@@ -24,9 +24,27 @@ hflav_dict = {
     "ZJets*" : 5,
     "[WZ][WZ]" : 5
 }
+perm_dict = {
+    "ttJetsSL" : 3,
+}
 other_dict = {
     "*" : 10,
     "ttJets*" : 5,
+    #"ttJetsSL" : 5,
+    "ttJetsSL" : 1,
+    "tt[WZ]*" : 5,
+    "QCD*" : 5,
+    "data*" : 2,
+    "single*" : 5,
+    "[AH]toTT*" : 5,
+    "WJets*" : 5,
+    "ZJets*" : 5,
+    "[WZ][WZ]" : 5
+}
+alpha_dict = {
+    "*" : 10,
+    "ttJets*" : 5,
+    #"ttJetsSL" : 5,
     "ttJetsSL" : 1,
     "tt[WZ]*" : 5,
     "QCD*" : 10,
