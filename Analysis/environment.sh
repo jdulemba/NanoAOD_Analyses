cf72a5fd5734b21e3f262c093137382a017df932
jdulemba <josdrw9@gmail.com>
2022-01-10 13:02:07 +0100

    Environment set based on whether it's local or on condor

--- Analysis/environment.sh
+++ Analysis/environment.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
+
 # Project-related environment variables
-whereIam=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+export PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+
+string_to_check="jdulemba"
+if [[ $PROJECT_DIR == *"$string_to_check"* ]]
+then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_97python3/x86_64-centos7-gcc8-opt/setup.sh
+fi 
 
 #This part should be changed by the user(s)
 export jobid=NOTSET
-if [ -e $whereIam/jobid.sh ]
+if [ -e $PROJECT_DIR/jobid.sh ]
 then
-        source $whereIam/jobid.sh
+        source $PROJECT_DIR/jobid.sh
         echo "set jobid: $jobid"
         echo "set nanoAOD version: $base_jobid"
 else
@@ -13,8 +21,6 @@ else
 fi
 
 #HERE ARE LIONS!
-export PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
-
 export LIBRARY_PATH=$LD_LIBRARY_PATH
 
 export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
