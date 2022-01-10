e55e4c37f26d7d712f1438443bdb8ef9cc530395
jdulemba <josdrw9@gmail.com>
2022-01-10 11:27:58 +0100

    Added option to specify dataset status

--- Analysis/MISC/search_das.py
+++ Analysis/MISC/search_das.py
@@ -16,23 +16,24 @@ import Utilities.das as das
 
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument('dataset_name', help='Dataset string name to search for in DAS')
-parser.add_argument('--txtname', default='tmp_dataset', help="Output txt file named 'tmp_dataset.txt'")
+parser.add_argument("dataset_name", help="Dataset string name to search for in DAS")
+parser.add_argument("--status", default="VALID", help="Output txt file named 'tmp_dataset.txt'")
+parser.add_argument("--txtname", default="tmp_dataset", help="Output txt file named 'tmp_dataset.txt'")
 
 args = parser.parse_args()
 
-jobid = os.environ['jobid']
-proj_dir = os.environ['PROJECT_DIR']
+jobid = os.environ["jobid"]
+proj_dir = os.environ["PROJECT_DIR"]
 
-dlist = das.query('dataset dataset=%s' % args.dataset_name)
-dnames = '\n'.join(dlist)
+dlist = das.query(f"dataset status={args.status} dataset={args.dataset_name}")
+dnames = "\n".join(dlist)
 
-txtname = os.path.join(proj_dir, '%s.txt' % args.txtname)
+txtname = os.path.join(proj_dir, f"{args.txtname}.txt")
 if os.path.isfile(txtname):
-   raise ValueError('File %s already exists!' % txtname)
+   raise ValueError(f"File {txtname} already exists!")
 
 #set_trace()
-txt_out = open(txtname, 'w')
+txt_out = open(txtname, "w")
 txt_out.write(dnames)
 txt_out.close()
-print("%s written" % txtname)
+print(f"{txtname} written")
