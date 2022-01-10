d32ea5093fec040ed6be1ab1dc115588c90469ce
jdulemba <josdrw9@gmail.com>
2022-01-10 11:37:56 +0100

    Only 1 reweighting type used now

--- Analysis/NNLO_files/check_nnlo_reweighting_yields_plotter.py
+++ Analysis/NNLO_files/check_nnlo_reweighting_yields_plotter.py
@@ -28,7 +28,7 @@ analyzer = "check_nnlo_reweighting_yields"
 
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument("year", choices=["2016", "2017", "2018"] if base_jobid == "NanoAODv6" else ["2016APV", "2016", "2017", "2018"], help="Specify which year to run over")
+parser.add_argument("year", choices=["2016APV", "2016", "2017", "2018"], help="Specify which year to run over")
 args = parser.parse_args()
 
 f_ext = "TOT.coffea"
@@ -41,12 +41,16 @@ if not os.path.isdir(outdir):
     os.makedirs(outdir)
     
 rewt_style_dict = {
-    "Nominal" : ("Nominal", "k"),
-    "thad_pt" : ("$W_{Orig}$($p_{T}$($t_{h}$))", "#e42a2c"), ## red
-    "thad_pt_Interp" : ("$W_{Int}$($p_{T}$($t_{h}$))", "#4daf4a"), ## green
-    "mtt_vs_thad_ctstar" : ("$W_{Orig}$($m_{t\\bar{t}}$, cos($\\theta^{*}_{t_{h}}$))", "#377eb8"), ## blue
-    "mtt_vs_thad_ctstar_Interp" : ("$W_{Int}$($m_{t\\bar{t}}$, cos($\\theta^{*}_{t_{h}}$))", "#ff7f00"), ## orange
+    "Nominal" : {"label" : "Nominal", "color" : "k", "linestyle" : "-"},
+    "mtt_vs_thad_ctstar" : {"label" : "$W$($m_{t\\bar{t}}$, cos($\\theta^{*}_{t_{h}}$))", "color" : "#377eb8", "linestyle" : "-"}, ## blue
 }
+#rewt_style_dict = {
+#    "Nominal" : ("Nominal", "k"),
+#    #"thad_pt" : ("$W_{Orig}$($p_{T}$($t_{h}$))", "#e42a2c"), ## red
+#    #"thad_pt_Interp" : ("$W_{Int}$($p_{T}$($t_{h}$))", "#4daf4a"), ## green
+#    "mtt_vs_thad_ctstar" : ("$W_{Orig}$($m_{t\\bar{t}}$, cos($\\theta^{*}_{t_{h}}$))", "#377eb8"), ## blue
+#    #"mtt_vs_thad_ctstar_Interp" : ("$W_{Int}$($m_{t\\bar{t}}$, cos($\\theta^{*}_{t_{h}}$))", "#ff7f00"), ## orange
+#}
 
 variables = {
         # thad
@@ -69,8 +73,20 @@ variables = {
     "phi_tt" : ("$\phi$($t\\bar{t}$)", 1, (-3.2, 3.2)),
     "mtt" : ("m($t\\bar{t}$) [GeV]", 10, (200., 4000.)),
     "mtt_vs_thad_ctstar" : ("m($t\\bar{t}$) $\otimes$ cos($\\theta^{*}_{t_{h}}$)", 1, None),
+    "mtt_vs_thad_ctstar_abs" : ("m($t\\bar{t}$) $\otimes$ |cos($\\theta^{*}_{t_{h}}$)|", 1, None),
 }
 
+mtt_vs_thad_ctstar_abs_binning = (
+    np.array([360.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0, 520.0, 540.0, 560.0, 580.0, 600.0, 625.0, 650.0, 675.0,
+        700.0, 730.0, 760.0, 800.0, 850.0, 900.0, 950., 1000.0, 1050., 1100.0, 1150., 1200., 1300., 1500., 2000.]),
+    np.array([0.0, 0.4, 0.6, 0.75, 0.9, 1.0])
+)
+
+mtt_vs_thad_ctstar_binning = (
+    np.array([300.0, 340.0, 360.0, 380.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0, 520.0, 540.0, 560.0, 580.0, 600.0, 625.0, 650.0, 675.0,
+        700.0, 730.0, 760.0, 800.0, 850.0, 900.0, 950., 1000.0, 1050., 1100.0, 1150., 1200., 1300., 1500., 2000.]),
+    np.array([-1.0, -0.6, -0.2, 0.2, 0.6, 1.0])
+)
 
 # make dict to check that yields for all dists are the same
 yields_dict = {}
@@ -98,17 +114,35 @@ for hname in variables.keys():
 
     #set_trace()
     if is2d:
+        if hname == "mtt_vs_thad_ctstar":
+            xrebinning, yrebinning = mtt_vs_thad_ctstar_binning
+        if hname == "mtt_vs_thad_ctstar_abs":
+            xrebinning, yrebinning = mtt_vs_thad_ctstar_abs_binning
+
+        xaxis_name = histo.dense_axes()[0].name
+        yaxis_name = histo.dense_axes()[1].name
+            ## rebin x axis
+        if isinstance(xrebinning, np.ndarray):
+            new_xbins = hist.Bin(xaxis_name, xaxis_name, xrebinning)
+        elif isinstance(xrebinning, float) or isinstance(xrebinning, int):
+            new_xbins = xrebinning
+        histo = histo.rebin(xaxis_name, new_xbins)
+            ## rebin y axis
+        if isinstance(yrebinning, np.ndarray):
+            new_ybins = hist.Bin(yaxis_name, yaxis_name, yrebinning)
+        elif isinstance(yrebinning, float) or isinstance(yrebinning, int):
+            new_ybins = yrebinning
+        histo = histo.rebin(yaxis_name, new_ybins)
+
             # vars only for linearized mtt ctstar hist
         mtt_binning, ctstar_binning = histo.axis(axes_to_sum[0]).edges(), histo.axis(axes_to_sum[1]).edges()
-        mtt_binlabels = ["%s $\leq$ m($t\\bar{t}$) $\leq$ %s" % (mtt_binning[bin], mtt_binning[bin+1]) for bin in range(len(mtt_binning)-1)]
-        ctstar_binlabels = ["%s $\leq$ cos($\\theta^{*}_{t_{h}}$) $\leq$ %s" % (ctstar_binning[bin], ctstar_binning[bin+1]) for bin in range(len(ctstar_binning)-1)]*len(mtt_binlabels)
-        mtt_bin_locs = np.linspace((len(ctstar_binning)-1)/2, (len(ctstar_binning)-1)*(len(mtt_binning)-1)-(len(ctstar_binning)-1)/2, len(mtt_binning)-1)
-        vlines = [len(mtt_binlabels)*ybin for ybin in range(1, len(mtt_binlabels))] # last element is hardcoded from binning
+        nbins = (len(mtt_binning)-1)*(len(ctstar_binning)-1)
+        vlines = [nbins*ybin/(len(ctstar_binning)-1) for ybin in range(1, len(ctstar_binning)-1)]
 
             # easier to rename sparse axis than change linearize()
         tmp_histo = histo.copy()
         tmp_histo = tmp_histo.group(histo.sparse_axes()[0].name, process_axis, {key[0]:key[0] for key in histo.values().keys()})
-        hline = Plotter.linearize_hist(tmp_histo, no_transpose=True)
+        hline = Plotter.linearize_hist(tmp_histo)
             # revert sparse axis name to original
         hline = hline.group(hline.sparse_axes()[0].name, reweighting_axis, {key[0]:key[0] for key in histo.values().keys()})
         histo = hline
@@ -122,37 +156,30 @@ for hname in variables.keys():
     fig.subplots_adjust(hspace=.07)
 
     for rewt_key in sorted(histo.values().keys()):
-            # plot yields    
-        ax = plot.plot1d(histo[rewt_key].integrate("rewt"),
-            ax=ax, clear=False,
-            line_opts={"linestyle" : "-", "color" : rewt_style_dict[rewt_key[0]][1]},
-        )
+        hep.plot.histplot(histo[rewt_key].integrate("rewt").values()[()], histo.dense_axes()[0].edges(), ax=ax, histtype="step", **rewt_style_dict[rewt_key[0]]) # nosys template
             ## plot ratios
         if rewt_key == ("Nominal",): continue
                 # orig
         ratio_masked_vals, ratio_bins = Plotter.get_ratio_arrays(num_vals=histo.values()[rewt_key], denom_vals=histo.values()[("Nominal",)], input_bins=histo.dense_axes()[0].edges())
-        rax.step(ratio_bins, ratio_masked_vals, where="post", **{"linestyle" : "-", "color" : rewt_style_dict[rewt_key[0]][1]})
+        rax.step(ratio_bins, ratio_masked_vals, where="post", **rewt_style_dict[rewt_key[0]])
 
     #set_trace()
     # plot yields
         # format axes
-    ax.autoscale(axis="x", tight=True)
-    ax.set_xlim(x_lims)
-    ax.set_ylim(None)
+    ax.autoscale()
+    ax.set_xlim((0, nbins) if is2d else x_lims)
+    ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1]*1.2)
     ax.set_xlabel(None)
     ax.set_ylabel("Events")
     
     rax.axhline(1, **{"linestyle": "--", "color": (0, 0, 0, 0.5), "linewidth": 1})
     rax.set_ylabel("W(var)/Nominal")
-    if not is2d: rax.set_xlabel(xtitle)
+    rax.set_xlabel(xtitle)
     
     ## set plotting styles
         ## set legend and corresponding colors
+    #set_trace()
     handles, labels = ax.get_legend_handles_labels()
-    for idx, handle in enumerate(handles):
-        hstyle = [val for val in rewt_style_dict.values() if handle.get_c() in val[1]]
-        labels[idx] = hstyle[0][0]
-    # call ax.legend() with the new values
     ax.legend(handles,labels, loc="upper right")
 
     ax.text(
@@ -162,72 +189,60 @@ for hname in variables.keys():
     )
 
     if is2d:
-        # plot ctstar labels
-        rax.set_xticks(np.arange(len(ctstar_binlabels)))
-        rax.set_xticklabels(ctstar_binlabels)
-        ax.tick_params(which="minor", bottom=False, top=False)
-        rax.tick_params(which="minor", bottom=False, top=False)
-
-        plt.setp(rax.get_xticklabels(), rotation=90, ha="left", fontsize=rcParams["font.size"]*0.5)
-
         # plot vertical lines for mtt vs ctstar
         for vline in vlines:
                 # orig
             ax.axvline(vline, color="k", linestyle="--")
             rax.axvline(vline, color="k", linestyle="--")
-    
-        # plot mtt labels
-        for idx, label in enumerate(mtt_binlabels):
-            rax.annotate(label, xy=(mtt_bin_locs[idx], 0), xycoords=("data", "axes fraction"),
-                xytext=(0, -120), textcoords="offset points", va="top", ha="center", fontsize=rcParams["font.size"]*0.5, rotation=45)
 
-    hep.cms.label(ax=ax, data=False, paper=False, year=args.year, lumi=round(lumi_to_use, 1))    
+    hep.cms.label(ax=ax, data=False, year=args.year, lumi=round(lumi_to_use, 1))    
 
-    figname = os.path.join(outdir, "SemilepTTbar_RewtComp_%s" % hname)
+    figname = os.path.join(outdir, f"SemilepTTbar_RewtComp_{hname}")
     fig.savefig(figname)
     #set_trace()
     print(f"{figname} written")
 
 # save yields dict    
-dec_tolerance = 7
-nom_same, pt_same, mtt_ct_same, pt_interp_same, mtt_ct_interp_same = (False,)*5
-    # lower decimal tolerance until all three yields have only one value
-while not (nom_same and pt_same and mtt_ct_same and pt_interp_same and mtt_ct_interp_same):
-    nom_yields = np.around(np.array([yields_dict[key][("Nominal",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
-    nom_same = np.all(nom_yields == nom_yields[0]) # values are all equal up to specified decimal place
-    if nom_same:
-        nom_yield = nom_yields[0]
-    
-    pt_yields = np.around(np.array([yields_dict[key][("thad_pt",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
-    pt_same = np.all(pt_yields == pt_yields[0]) # values are all equal up to specified decimal place
-    if pt_same:
-        pt_yield = pt_yields[0]
-    
-    mtt_ct_yields = np.around(np.array([yields_dict[key][("mtt_vs_thad_ctstar",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
-    mtt_ct_same = np.all(mtt_ct_yields == mtt_ct_yields[0]) # values are all equal up to specified decimal place
-    if mtt_ct_same:
-        mtt_ct_yield = mtt_ct_yields[0]
-
-    pt_interp_yields = np.around(np.array([yields_dict[key][("thad_pt_Interp",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
-    pt_interp_same = np.all(pt_interp_yields == pt_interp_yields[0]) # values are all equal up to specified decimal place
-    if pt_interp_same:
-        pt_interp_yield = pt_interp_yields[0]
-    
-    mtt_ct_interp_yields = np.around(np.array([yields_dict[key][("mtt_vs_thad_ctstar_Interp",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
-    mtt_ct_interp_same = np.all(mtt_ct_interp_yields == mtt_ct_interp_yields[0]) # values are all equal up to specified decimal place
-    if mtt_ct_interp_same:
-        mtt_ct_interp_yield = mtt_ct_interp_yields[0]
-
-    if not (nom_same and pt_same and mtt_ct_same and pt_interp_same and mtt_ct_interp_same): dec_tolerance -= 1
-
-rows = [("Lumi: %s fb^-1" % format(lumi_to_use, ".1f"), "", "Values are the same up to %i decimal places" % dec_tolerance)]
-rows += [("Reweighting", "Yield", "Yield/Nominal")]
-rows += [("Nominal", format(nom_yield, ".%if" % dec_tolerance), format(nom_yield/nom_yield, ".%if" % dec_tolerance))]
-rows += [("thad_pt", format(pt_yield, ".%if" % dec_tolerance), format(pt_yield/nom_yield, ".%if" % dec_tolerance))]
-rows += [("mtt_vs_thad_ctstar", format(mtt_ct_yield, ".%if" % dec_tolerance), format(mtt_ct_yield/nom_yield, ".%if" % dec_tolerance))]
-rows += [("thad_pt_interp", format(pt_interp_yield, ".%if" % dec_tolerance), format(pt_interp_yield/nom_yield, ".%if" % dec_tolerance))]
-rows += [("mtt_vs_thad_ctstar_interp", format(mtt_ct_interp_yield, ".%if" % dec_tolerance), format(mtt_ct_interp_yield/nom_yield, ".%if" % dec_tolerance))]
-
-yields_fname = os.path.join(outdir, "reweighting_yields.txt")
-plt_tools.print_table(rows, filename=yields_fname, print_output=True, header_line=1)
-print(f"{yields_fname} written")
+#dec_tolerance = 7
+#nom_same, pt_same, mtt_ct_same, pt_interp_same, mtt_ct_interp_same = (False,)*5
+#    # lower decimal tolerance until all three yields have only one value
+#while not (nom_same and pt_same and mtt_ct_same and pt_interp_same and mtt_ct_interp_same):
+#    nom_yields = np.around(np.array([yields_dict[key][("Nominal",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
+#    nom_same = np.all(nom_yields == nom_yields[0]) # values are all equal up to specified decimal place
+#    if nom_same:
+#        nom_yield = nom_yields[0]
+#    
+#    #pt_yields = np.around(np.array([yields_dict[key][("thad_pt",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
+#    #pt_same = np.all(pt_yields == pt_yields[0]) # values are all equal up to specified decimal place
+#    #if pt_same:
+#    #    pt_yield = pt_yields[0]
+#    
+#    mtt_ct_yields = np.around(np.array([yields_dict[key][("mtt_vs_thad_ctstar",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
+#    mtt_ct_same = np.all(mtt_ct_yields == mtt_ct_yields[0]) # values are all equal up to specified decimal place
+#    if mtt_ct_same:
+#        mtt_ct_yield = mtt_ct_yields[0]
+#
+#    #pt_interp_yields = np.around(np.array([yields_dict[key][("thad_pt_Interp",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
+#    #pt_interp_same = np.all(pt_interp_yields == pt_interp_yields[0]) # values are all equal up to specified decimal place
+#    #if pt_interp_same:
+#    #    pt_interp_yield = pt_interp_yields[0]
+#    #
+#    #mtt_ct_interp_yields = np.around(np.array([yields_dict[key][("mtt_vs_thad_ctstar_Interp",)] for key in yields_dict.keys()]), decimals=dec_tolerance)
+#    #mtt_ct_interp_same = np.all(mtt_ct_interp_yields == mtt_ct_interp_yields[0]) # values are all equal up to specified decimal place
+#    #if mtt_ct_interp_same:
+#    #    mtt_ct_interp_yield = mtt_ct_interp_yields[0]
+#
+#    #if not (nom_same and pt_same and mtt_ct_same and pt_interp_same and mtt_ct_interp_same): dec_tolerance -= 1
+#    if not (nom_same and mtt_ct_same): dec_tolerance -= 1
+#
+#rows = [("Lumi: %s fb^-1" % format(lumi_to_use, ".1f"), "", "Values are the same up to %i decimal places" % dec_tolerance)]
+#rows += [("Reweighting", "Yield", "Yield/Nominal")]
+#rows += [("Nominal", format(nom_yield, ".%if" % dec_tolerance), format(nom_yield/nom_yield, ".%if" % dec_tolerance))]
+##rows += [("thad_pt", format(pt_yield, ".%if" % dec_tolerance), format(pt_yield/nom_yield, ".%if" % dec_tolerance))]
+#rows += [("mtt_vs_thad_ctstar", format(mtt_ct_yield, ".%if" % dec_tolerance), format(mtt_ct_yield/nom_yield, ".%if" % dec_tolerance))]
+##rows += [("thad_pt_interp", format(pt_interp_yield, ".%if" % dec_tolerance), format(pt_interp_yield/nom_yield, ".%if" % dec_tolerance))]
+##rows += [("mtt_vs_thad_ctstar_interp", format(mtt_ct_interp_yield, ".%if" % dec_tolerance), format(mtt_ct_interp_yield/nom_yield, ".%if" % dec_tolerance))]
+#
+#yields_fname = os.path.join(outdir, "reweighting_yields.txt")
+#plt_tools.print_table(rows, filename=yields_fname, print_output=True, header_line=1)
+#print(f"{yields_fname} written")
