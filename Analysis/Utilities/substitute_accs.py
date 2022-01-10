7b130b2fdda86c17097ecfcfec2d7e1cc4888fea
jdulemba <josdrw9@gmail.com>
2022-01-10 12:30:23 +0100

    Import final analysis binning

--- Analysis/Utilities/substitute_accs.py
+++ Analysis/Utilities/substitute_accs.py
@@ -6,10 +6,10 @@ import coffea.hist
 
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument('dataset_to_sub', type=str, help='Name of dataset to be substituted from new_file to replace in orig_file.')
-parser.add_argument('output_fname', type=str, help='Name of output file with file extension.')
-parser.add_argument('orig_file', type=str, help="Input files separated by ':'")
-parser.add_argument('new_file', type=str, help="Input files separated by ':'")
+parser.add_argument("dataset_to_sub", type=str, help="Name of dataset to be substituted from new_file to replace in orig_file.")
+parser.add_argument("output_fname", type=str, help="Name of output file with file extension.")
+parser.add_argument("orig_file", type=str, help="Input files separated by ':'")
+parser.add_argument("new_file", type=str, help="Input files separated by ':'")
 args = parser.parse_args()
 
 orig_dict = load(args.orig_file)
@@ -17,14 +17,14 @@ new_dict = load(args.new_file)
 output_dict = orig_dict.copy()
 
     # get mask of all datasets that will remain unchanged
-non_dataset_mask = re.compile('(?!%s*)' % args.dataset_to_sub)
-dataset_mask = re.compile('(%s*)' % args.dataset_to_sub)
+non_dataset_mask = re.compile("(?!%s*)" % args.dataset_to_sub)
+dataset_mask = re.compile("(%s*)" % args.dataset_to_sub)
 
 for hname in new_dict.keys():
     if hname not in orig_dict.keys():
-        print('%s is in new_dict but not orig_dict, skipping')
+        print(f"{hname} is in new_dict but not orig_dict, skipping")
         continue
-    if 'cutflow' in hname: continue
+    if "cutflow" in hname: continue
     print(hname)
     new_obj = new_dict[hname]
     orig_obj = orig_dict[hname]
@@ -46,7 +46,7 @@ for hname in new_dict.keys():
         raise ValueError("type(new_obj) %s and type(orig_obj) %s are not currently supported or are different." % (type(new_obj), type(orig_obj)))
 
 #set_trace()
-outname = args.output_fname if args.output_fname.endswith('.coffea') else '%s.coffea' % args.output_fname
+outname = args.output_fname if args.output_fname.endswith(".coffea") else "%s.coffea" % args.output_fname
 output_acc = processor.dict_accumulator(output_dict)
 save(output_acc, outname)
-print('%s written' % outname)
+print(f"{outname} written")
