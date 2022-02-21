0eefdf759b9a0bbb2855ba4ebe0061bd560d1bdc
jdulemba <josdrw9@gmail.com>
2022-02-21 15:57:40 +0100

    Added UR trigger SFs for electrons to other SFs provided by POGs.

--- Analysis/Corrections/save_POGleptonSFs.py
+++ Analysis/Corrections/save_POGleptonSFs.py
@@ -1,5 +1,5 @@
 import numpy as np
-from coffea.util import save
+from coffea.util import save, load
 from coffea.lookup_tools.root_converters import convert_histo_root_file
 from coffea.lookup_tools.dense_lookup import dense_lookup
 from pdb import set_trace
@@ -19,13 +19,13 @@ leptons = {
         "Muons" : {
             "ID" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_ID.root", "NUM_TightID_DEN_TrackerMuons_abseta_pt"],
             "ISO" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_ISO.root", "NUM_TightRelIso_DEN_TightIDandIPCut_abseta_pt"],
-            "TRIGGER" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_SingleMuonTriggers.root", "NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
+            "TRIG" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_SingleMuonTriggers.root", "NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
             "RECO" : ["Efficiency_muon_generalTracks_Run2016preVFP_UL_trackerMuon.root", "NUM_TrackerMuons_DEN_genTracks"],
         },
         "Electrons" : {
             "ID" : ["egammaEffi.txt_Ele_Tight_preVFP_EGM2D.root", "EGamma_SF2D"],
             #"ISO" : ["", ""],
-            #"TRIGGER" : ["", ""],
+            "TRIG" : ["", ""],
             "RECO" : ["egammaEffi_ptAbove20.txt_EGM2D_UL2016preVFP.root", "EGamma_SF2D"],
         },
     },
@@ -33,13 +33,13 @@ leptons = {
         "Muons" : {
             "ID" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_ID.root", "NUM_TightID_DEN_TrackerMuons_abseta_pt"],
             "ISO" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_ISO.root", "NUM_TightRelIso_DEN_TightIDandIPCut_abseta_pt"],
-            "TRIGGER" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_SingleMuonTriggers.root", "NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
+            "TRIG" : ["Efficiencies_muon_generalTracks_Z_Run2016_UL_SingleMuonTriggers.root", "NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
             "RECO" : ["Efficiency_muon_generalTracks_Run2016postVFP_UL_trackerMuon.root", "NUM_TrackerMuons_DEN_genTracks"],
         },
         "Electrons" : {
             "ID" : ["egammaEffi.txt_Ele_Tight_postVFP_EGM2D.root", "EGamma_SF2D"],
             #"ISO" : ["", ""],
-            #"TRIGGER" : ["", ""],
+            "TRIG" : ["", ""],
             "RECO" : ["egammaEffi_ptAbove20.txt_EGM2D_UL2016postVFP.root", "EGamma_SF2D"],
         },
     },
@@ -47,13 +47,13 @@ leptons = {
         "Muons" : {
             "ID" : ["Efficiencies_muon_generalTracks_Z_Run2017_UL_ID.root", "NUM_TightID_DEN_TrackerMuons_abseta_pt"],
             "ISO" : ["Efficiencies_muon_generalTracks_Z_Run2017_UL_ISO.root", "NUM_TightRelIso_DEN_TightIDandIPCut_abseta_pt"],
-            "TRIGGER" : ["Efficiencies_muon_generalTracks_Z_Run2017_UL_SingleMuonTriggers.root", "NUM_IsoMu27_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
+            "TRIG" : ["Efficiencies_muon_generalTracks_Z_Run2017_UL_SingleMuonTriggers.root", "NUM_IsoMu27_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
             "RECO" : ["Efficiency_muon_generalTracks_Run2017_UL_trackerMuon.root", "NUM_TrackerMuons_DEN_genTracks"],
         },
         "Electrons" : {
             "ID" : ["egammaEffi.txt_EGM2D_Tight_UL17.root", "EGamma_SF2D"],
             #"ISO" : ["", ""],
-            #"TRIGGER" : ["", ""],
+            "TRIG" : ["", ""],
             "RECO" : ["egammaEffi_ptAbove20.txt_EGM2D_UL2017.root", "EGamma_SF2D"],
         },
     },
@@ -61,13 +61,13 @@ leptons = {
         "Muons" : {
             "ID" : ["Efficiencies_muon_generalTracks_Z_Run2018_UL_ID.root", "NUM_TightID_DEN_TrackerMuons_abseta_pt"],
             "ISO" : ["Efficiencies_muon_generalTracks_Z_Run2018_UL_ISO.root", "NUM_TightRelIso_DEN_TightIDandIPCut_abseta_pt"],
-            "TRIGGER" : ["Efficiencies_muon_generalTracks_Z_Run2018_UL_SingleMuonTriggers.root", "NUM_IsoMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
+            "TRIG" : ["Efficiencies_muon_generalTracks_Z_Run2018_UL_SingleMuonTriggers.root", "NUM_IsoMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt"],
             "RECO" : ["Efficiency_muon_generalTracks_Run2018_UL_trackerMuon.root", "NUM_TrackerMuons_DEN_genTracks"],
         },
         "Electrons" : {
             "ID" : ["egammaEffi.txt_Ele_Tight_EGM2D.root", "EGamma_SF2D"],
             #"ISO" : ["", ""],
-            #"TRIGGER" : ["", ""],
+            "TRIG" : ["", ""],
             "RECO" : ["egammaEffi_ptAbove20.txt_EGM2D_UL2018.root", "EGamma_SF2D"],
         },
     },
@@ -86,22 +86,37 @@ sf_output = {
     } for year in leptons.keys()
 }
 
+#set_trace()
+    # load UR lepton SFs to add electron triggers
+UR_SFs = load(os.path.join(proj_dir, "Corrections", jobid, "leptonSF_noTAGvariation.coffea"))
+
 for year in leptons.keys():
     for lep in leptons[year].keys():
         for sf_type, (sf_fname, sf_dist) in leptons[year][lep].items():
             print(f"Making SFs for {year}, {lep}, {sf_type}")
-            sf_file = convert_histo_root_file(os.path.join(indir, year, lep, sf_fname))
 
-            sf_output[year][lep][sf_type]["Central"] = dense_lookup(*sf_file[(sf_dist, "dense_lookup")])
-            sf_output[year][lep][sf_type]["Error_tot"] = dense_lookup(*sf_file[(f"{sf_dist}_error", "dense_lookup")])
-            if (sf_type != "RECO") and (lep == "Muons"):
-                sf_output[year][lep][sf_type]["Error_stat"] = dense_lookup(*sf_file[(f"{sf_dist}_stat_error", "dense_lookup")])
-                sf_output[year][lep][sf_type]["Error_syst"] = dense_lookup(*sf_file[(f"{sf_dist}_syst_error", "dense_lookup")])
-            else:
+                # save UR electron trigger SFs
+            if (lep == "Electrons") and (sf_type == "TRIG"):
+                #set_trace()
+                sf_output[year][lep][sf_type]["Central"] = UR_SFs[year][lep]["Trig"]["Central"]
+                sf_output[year][lep][sf_type]["Error_tot"] = UR_SFs[year][lep]["Trig"]["Error"]
+                sf_output[year][lep][sf_type]["eta_ranges"] = UR_SFs[year][lep]["eta_ranges"]
                 del sf_output[year][lep][sf_type]["Error_stat"]
                 del sf_output[year][lep][sf_type]["Error_syst"]
+            else:
+                sf_file = convert_histo_root_file(os.path.join(indir, year, lep, sf_fname))
 
-set_trace()
-lepSF_name = os.path.join(outdir, "lepton_POG_SFs.coffea")
+                sf_output[year][lep][sf_type]["Central"] = dense_lookup(*sf_file[(sf_dist, "dense_lookup")])
+                sf_output[year][lep][sf_type]["Error_tot"] = dense_lookup(*sf_file[(f"{sf_dist}_error", "dense_lookup")])
+                if (sf_type != "RECO") and (lep == "Muons"):
+                    sf_output[year][lep][sf_type]["Error_stat"] = dense_lookup(*sf_file[(f"{sf_dist}_stat_error", "dense_lookup")])
+                    sf_output[year][lep][sf_type]["Error_syst"] = dense_lookup(*sf_file[(f"{sf_dist}_syst_error", "dense_lookup")])
+                else:
+                    del sf_output[year][lep][sf_type]["Error_stat"]
+                    del sf_output[year][lep][sf_type]["Error_syst"]
+
+#set_trace()
+lepSF_name = os.path.join(outdir, f"lepton_{jobid}.coffea")
+#lepSF_name = os.path.join(outdir, "lepton_POG_SFs.coffea")
 save(sf_output, lepSF_name)    
 print(f"{lepSF_name} written")
