24cfb2153ab4461d0391a68f51132d9e5a41244c
jdulemba <josdrw9@gmail.com>
2022-02-23 13:08:25 +0100

    Added more dictionaries useful to btag sideband regions.

--- Analysis/Utilities/btag_sideband_regions.py
+++ Analysis/Utilities/btag_sideband_regions.py
@@ -47,3 +47,46 @@ btag_reg_names_dict = {
         "Up"     : {"reg" : "p2779p4168", "label" : "Up (0.2779-0.4168)", "color" : "r"},
     },
 }
+
+btag_cats = {
+    "btagPass" : "$n_{btags} \geq$ 2",
+    "p00p20" : "0.0 < max(btag discr) $\leq$ 0.2",
+    "p20p40" : "0.2 < max(btag discr) $\leq$ 0.4",
+    "p40p60" : "0.4 < max(btag discr) $\leq$ 0.6",
+    "p00p1949" : "0.0 < max(btag discr) $\leq$ 0.1949",
+    "p1949p3898" : "0.1949 < max(btag discr) $\leq$ 0.3898",
+    "p3898p5847" : "0.3898 < max(btag discr) $\leq$ 0.5847",
+    "p00p1502" : "0.0 < max(btag discr) $\leq$ 0.1502",
+    "p1502p3004" : "0.1502 < max(btag discr) $\leq$ 0.3004",
+    "p3004p4506" : "0.3004 < max(btag discr) $\leq$ 0.4506",
+    "p00p1389" : "0.0 < max(btag discr) $\leq$ 0.1389",
+    "p1389p2779" : "0.1389 < max(btag discr) $\leq$ 0.2779",
+    "p2779p4168" : "0.2779 < max(btag discr) $\leq$ 0.4168",
+}
+
+btag_groups = {
+    "2016APV" : {
+        "btagPass" : ["btagPass"],
+        "p00p20" : ["p00p20"],
+        "p20p40" : ["p20p40"],
+        "p40p60" : ["p40p60"],
+    },
+    "2016" : {
+        "btagPass" : ["btagPass"],
+        "p00p1949" : ["p00p1949"],
+        "p1949p3898" : ["p1949p3898"],
+        "p3898p5847" : ["p3898p5847"],
+    },
+    "2017" : {
+        "btagPass" : ["btagPass"],
+        "p00p1502" : ["p00p1502"],
+        "p1502p3004" : ["p1502p3004"],
+        "p3004p4506" : ["p3004p4506"],
+    },
+    "2018" : {
+        "btagPass" : ["btagPass"],
+        "p00p1389" : ["p00p1389"],
+        "p1389p2779" : ["p1389p2779"],
+        "p2779p4168" : ["p2779p4168"],
+    },
+}
