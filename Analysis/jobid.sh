8736f7b17da37581a00fbe5069fc4342aba72870
jdulemba <josdrw9@gmail.com>
2022-04-26 17:16:08 +0200

    Added ability for ULnanoAODv9 datasets

--- Analysis/jobid.sh
+++ Analysis/jobid.sh
@@ -19,12 +19,16 @@
 #export jobid=Summer20UL_new_lepSFs
 export jobid=Summer20UL_POG_lepSFs
 
+#export jobid=ULnanoAODv9
+
 if [[ $jobid == *"NanoAODv6"* ]]; then
     export base_jobid=NanoAODv6
 elif [[ $jobid == *"Summer20UL"* ]]; then
     export base_jobid=Summer20UL
-elif [[ $jobid == *"ULnanoAOD"* ]]; then
-    export base_jobid=ULnanoAOD
+elif [[ $jobid == *"ULnanoAODv9"* ]]; then
+    export base_jobid=ULnanoAODv9
+#elif [[ $jobid == *"ULnanoAOD"* ]]; then
+#    export base_jobid=ULnanoAOD
 else
     echo "base_jobid NOT SET"
 fi
