2c11aab170b4655dea0c019c30e3333d352dc0dc
jdulemba <josdrw9@gmail.com>
2022-04-26 16:42:28 +0200

    Merge output files in my cernbox now.

--- Analysis/Run_Jobs/merge_jobs.py
+++ Analysis/Run_Jobs/merge_jobs.py
@@ -10,27 +10,28 @@ args = parser.parse_args()
 proj_dir = os.environ["PROJECT_DIR"]
 user = os.environ["USER"]
 
-## once jobs are finished, merge all output coffea files for each sample into one
-jobdir = args.jobdir if (args.jobdir).startswith(os.path.join("afs", "cern.ch", "work", "j", user)) else os.path.join(proj_dir, args.jobdir)
-jobdir = jobdir[:-1] if jobdir.endswith("/") else jobdir
-if not os.path.isdir(jobdir):
-    raise IOError("Directory: %s does not exist" % jobdir)
+condor_jobdir = args.jobdir if (args.jobdir).startswith(os.path.join("afs", "cern.ch", "work", "j", user)) else os.path.join(proj_dir, args.jobdir)
+condor_jobdir = condor_jobdir[:-1] if condor_jobdir.endswith("/") else condor_jobdir
+if not os.path.isdir(condor_jobdir):
+    raise IOError(f"Directory: {condor_jobdir} does not exist")
 
-
-
-if os.path.isfile(os.path.join(jobdir, f"{args.jobdir}_TOT.coffea")):
+basedir = os.path.basename(condor_jobdir)
+eos_dir = os.path.join("/eos", "user", user[0], user, "NanoAOD_Analyses")
+eos_jobdir = os.path.join(eos_dir, basedir)
+if os.path.isfile(os.path.join(eos_jobdir, f"{args.jobdir}_TOT.coffea")):
     print(f"Combined output file {jobdir}/{args.jobdir}_TOT.coffea already exists")
     import sys; sys.exit()
 
 orig_dir = os.getcwd()
-all_possible_samples = sorted([dirname for dirname in os.listdir(jobdir) if os.path.isdir(os.path.join(jobdir, dirname))])
+all_possible_samples = sorted([dirname for dirname in os.listdir(condor_jobdir) if os.path.isdir(os.path.join(condor_jobdir, dirname))])
 
 tot_files = {}
-def merge_files(jobdir, samples):
+def merge_files(samples):
     #set_trace()
     for sample in samples:
-        merged_fname = os.path.join(jobdir, sample, f"{sample}_TOT.coffea")
-        output_files = [f"{jobdir}/{sample}/{fname}" for fname in os.listdir(f"{jobdir}/{sample}") if fname.endswith(".coffea")]
+        print(f"Merging {sample}")
+        merged_fname = os.path.join(eos_jobdir, sample, f"{sample}_TOT.coffea")
+        output_files = [f"{eos_jobdir}/{sample}/{fname}" for fname in os.listdir(f"{eos_jobdir}/{sample}") if fname.endswith(".coffea")]
             # don't re-merge files if this has already been done
         if merged_fname in output_files:
             tot_files[sample] = merged_fname
@@ -48,18 +49,17 @@ def merge_files(jobdir, samples):
             tot_files[sample] = merged_fname
 
     # open to_merge.txt
-if not os.path.isfile(os.path.join(jobdir, "to_merge.txt")):
-    raise ValueError(f"No file found to merge in {jobdir}")
+if not os.path.isfile(os.path.join(condor_jobdir, "to_merge.txt")):
+    raise ValueError(f"No file found to merge in {condor_jobdir}")
 
-txt_file = open(os.path.join(jobdir, "to_merge.txt"), "r")
-samples = [sample.strip("\n") for sample in txt_file]
 #set_trace()
-for sample in samples:
-    merge_files(jobdir, [sample])
+txt_file = open(os.path.join(condor_jobdir, "to_merge.txt"), "r")
+samples_to_merge = [sample.strip("\n") for sample in txt_file]
+merge_files(samples_to_merge)
 
 ## merge all TOT files from each sample into one
 if sorted(set(tot_files.keys())) == sorted(set(all_possible_samples)):
+    #set_trace()
     tot_acc = plot_tools.add_coffea_files(list(tot_files.values()))
     tot_outname = "%s_TOT.coffea" % args.jobdir.strip("/")
-    plot_tools.save_accumulator(tot_acc, f"{jobdir}/{tot_outname}")
-
+    plot_tools.save_accumulator(tot_acc, f"{eos_jobdir}/{tot_outname}")
