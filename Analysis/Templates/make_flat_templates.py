69ac46b808c0fa2dc208dd47a856bb3548523a0f
jdulemba <josdrw9@gmail.com>
2022-04-26 16:59:41 +0200

    Added ability to make templates for ME reweighted signal. Changed how to specify which templates to create, from choices list to ':' separated input

--- Analysis/Templates/make_flat_templates.py
+++ Analysis/Templates/make_flat_templates.py
@@ -15,8 +15,7 @@ base_jobid = os.environ["base_jobid"]
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("year", choices=["2016APV", "2016", "2017", "2018"], help="What year is the ntuple from.")
-parser.add_argument("--only_bkg", action="store_true", help="Make background templates only.")
-parser.add_argument("--only_sig", action="store_true", help="Make signal templates only.")
+parser.add_argument("templates_to_run", type=str, help="Choose which type of templates to run, multiple options can be input as ':' separated strings.")
 parser.add_argument("--kfactors", action="store_true", help="Apply signal k-factors to signal")
 parser.add_argument("--scale_mtop3gev", action="store_true", help="Scale 3GeV mtop variations by 1/6")
 args = parser.parse_args()
@@ -76,37 +75,71 @@ def flatten_sig_templates(fname):
     print(f"{coffea_out} written")
 
 
+def flatten_MEsig_templates(fname):
+    """
+    Function that writes linearized mtt vs costheta distributions to root file.
+    """
+    histo_dict = processor.dict_accumulator({njets : {"Muon" : {}, "Electron" :{}} for njets in njets_to_run})
+    hdict = load(fname)
+    for jmult in hdict.keys():
+        for lep in hdict[jmult].keys():
+            for tname, orig_template in hdict[jmult][lep].items():
+                if "Res" in tname:
+                    signal = "_".join([tname.split("_Res")[0], "Res"])
+                else:
+                    signal = "_".join([tname.split("_neg")[0], "neg"]) if "neg" in tname else "_".join([tname.split("_pos")[0], "pos"])
+                sys = sorted(filter(None, tname.split(f"{signal}_")))[0]
+                print(lep, jmult, sys, signal)                
+
+                    # perform flattening
+                flattened_histo = hdict[jmult][lep][f"{signal}_nosys"].copy() if sys == "nosys" else Plotter.flatten(nosys=hdict[jmult][lep][f"{signal}_nosys"].copy(), systematic=orig_template.copy())
+                
+                    ## save template histos to coffea dict
+                histo_dict[jmult][lep][tname] = flattened_histo.copy()
+
+    #set_trace()
+    coffea_out = os.path.join(input_dir, f"flattened_templates_lj_MEsig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else  f"flattened_templates_lj_MEsig_{args.year}_{jobid}.coffea")
+    save(histo_dict, coffea_out)
+    print(f"{coffea_out} written")
+
+
 if __name__ == "__main__":
+    allowed_template_options = ["bkg", "sig", "MEreweight_sig"]
+    templates_to_run = (args.templates_to_run).split(":")
+    templates_to_run = [template for template in templates_to_run if template in allowed_template_options]
+
     proj_dir = os.environ["PROJECT_DIR"]
     jobid = os.environ["jobid"]
 
     njets_to_run = ["3Jets", "4PJets"]
 
     analyzer = "htt_btag_sb_regions"
-    base_bkg_template_name = f"raw_templates_lj_bkg_mtopscaled_{args.year}_{jobid}.coffea" if args.scale_mtop3gev else f"raw_templates_lj_bkg_{args.year}_{jobid}.coffea"
-    base_sig_template_name = f"raw_templates_lj_sig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_sig_{args.year}_{jobid}.coffea"
 
     input_dir = os.path.join(proj_dir, "results", f"{args.year}_{jobid}", f"Templates_{analyzer}")
     if not os.path.isdir(input_dir): raise ValueError("No background file found.")
 
-    if not args.only_sig:
-        try:
+    if "bkg" in templates_to_run:
                 # define variables to get histogram for background
-            bkg_fname = os.path.join(input_dir, base_bkg_template_name)
-            if not os.path.isfile(bkg_fname): raise ValueError("No background file found.")
-            print("Creating flattened background templates")
-            flatten_bkg_templates(bkg_fname)
-        except:
-            print("Could not write background templates to file")
+        base_bkg_template_name = f"raw_templates_lj_bkg_mtopscaled_{args.year}_{jobid}.coffea" if args.scale_mtop3gev else f"raw_templates_lj_bkg_{args.year}_{jobid}.coffea"
+        bkg_fname = os.path.join(input_dir, base_bkg_template_name)
+        if not os.path.isfile(bkg_fname): raise ValueError("No background file found.")
+        print("Creating flattened background templates")
+        flatten_bkg_templates(bkg_fname)
 
-    if not args.only_bkg:
-        try:
-            sig_fname = os.path.join(input_dir, base_sig_template_name)
-            if not os.path.isfile(sig_fname): raise ValueError("No signal file found.")
-            print("Creating flattened signal templates")
-            flatten_sig_templates(sig_fname)
-        except:
-            print("Could not write signal templates to file")
+    if "sig" in templates_to_run:
+        base_sig_template_name = f"raw_templates_lj_sig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_sig_{args.year}_{jobid}.coffea"
+        sig_fname = os.path.join(input_dir, base_sig_template_name)
+        if not os.path.isfile(sig_fname): raise ValueError("No signal file found.")
+        print("Creating flattened signal templates")
+        flatten_sig_templates(sig_fname)
+
+    if "MEreweight_sig" in templates_to_run:
+        base_sig_template_name = f"raw_templates_lj_MEsig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_MEsig_{args.year}_{jobid}.coffea"
+        sig_fname = os.path.join(input_dir, base_sig_template_name)
+        if not os.path.isfile(sig_fname): raise ValueError("No signal file found.")
+        print("Creating ME reweighting signal templates")
+        flatten_MEsig_templates(sig_fname)
+
 
     toc = time.time()
     print("Total time: %.1f" % (toc - tic))
--- Analysis/Templates/make_smooth_templates.py
+++ Analysis/Templates/make_smooth_templates.py
@@ -15,8 +15,7 @@ base_jobid = os.environ["base_jobid"]
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("year", choices=["2016APV", "2016", "2017", "2018"], help="What year is the ntuple from.")
-parser.add_argument("--only_bkg", action="store_true", help="Make background templates only.")
-parser.add_argument("--only_sig", action="store_true", help="Make signal templates only.")
+parser.add_argument("templates_to_run", type=str, help="Choose which type of templates to run, multiple options can be input as ':' separated strings.")
 parser.add_argument("--kfactors", action="store_true", help="Apply signal k-factors to signal")
 parser.add_argument("--scale_mtop3gev", action="store_true", help="Scale 3GeV mtop variations by 1/6")
 args = parser.parse_args()
@@ -74,6 +73,33 @@ def smooth_sig_templates(fname):
     print(f"{coffea_out} written")
 
 
+def smooth_MEsig_templates(fname):
+    """
+    Function that writes linearized mtt vs costheta distributions to root file.
+    """
+    histo_dict = processor.dict_accumulator({njets : {"Muon" : {}, "Electron" :{}} for njets in njets_to_run})
+    hdict = load(fname)
+    for jmult in hdict.keys():
+        for lep in hdict[jmult].keys():
+            for tname, orig_template in hdict[jmult][lep].items():
+                if "Res" in tname:
+                    signal = "_".join([tname.split("_Res")[0], "Res"])
+                else:
+                    signal = "_".join([tname.split("_neg")[0], "neg"]) if "neg" in tname else "_".join([tname.split("_pos")[0], "pos"])
+                sys = sorted(filter(None, tname.split(f"{signal}_")))[0]
+                print(lep, jmult, sys, signal)
+
+                    # perform smoothing
+                smoothed_histo = hdict[jmult][lep][f"{signal}_nosys"].copy() if sys == "nosys" \
+                    else Plotter.smoothing_mttbins(nosys=hdict[jmult][lep][f"{signal}_nosys"], systematic=orig_template, mtt_centers=mtt_centers, nbinsx=len(linearize_binning[0])-1, nbinsy=len(linearize_binning[1])-1)
+                    ## save template histos to coffea dict
+                histo_dict[jmult][lep][tname] = smoothed_histo.copy()
+
+    coffea_out = os.path.join(input_dir, f"smoothed_templates_lj_MEsig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"smoothed_templates_lj_MEsig_{args.year}_{jobid}.coffea")
+    save(histo_dict, coffea_out)
+    print(f"{coffea_out} written")
+
+
 if __name__ == "__main__":
 
     instructions = """
@@ -84,14 +110,17 @@ if __name__ == "__main__":
     Add python paths by 'source venv.sh' and then run this script
     """
     print(instructions)
+
+    allowed_template_options = ["bkg", "sig", "MEreweight_sig"]
+    templates_to_run = (args.templates_to_run).split(":")
+    templates_to_run = [template for template in templates_to_run if template in allowed_template_options]
+
     proj_dir = os.environ["PROJECT_DIR"]
     jobid = os.environ["jobid"]
 
     njets_to_run = ["3Jets", "4PJets"]
 
     analyzer = "htt_btag_sb_regions"
-    base_bkg_template_name = f"raw_templates_lj_bkg_mtopscaled_{args.year}_{jobid}.coffea" if args.scale_mtop3gev else f"raw_templates_lj_bkg_{args.year}_{jobid}.coffea"
-    base_sig_template_name = f"raw_templates_lj_sig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_sig_{args.year}_{jobid}.coffea"
 
         # get matching pattern based on args.njets
     njets_regex = "*" if len(njets_to_run) > 1 else njets_to_run[0]
@@ -104,17 +133,26 @@ if __name__ == "__main__":
     )
     mtt_centers =  np.array([(linearize_binning[0][idx]+linearize_binning[0][idx+1])/2 for idx in range(len(linearize_binning[0])-1)])
 
-    if not args.only_sig:
+    if "bkg" in templates_to_run:
+        base_bkg_template_name = f"raw_templates_lj_bkg_mtopscaled_{args.year}_{jobid}.coffea" if args.scale_mtop3gev else f"raw_templates_lj_bkg_{args.year}_{jobid}.coffea"
         bkg_fname = os.path.join(input_dir, base_bkg_template_name)
         if not os.path.isfile(bkg_fname): raise ValueError("No background file found.")
         print("Creating smoothed background templates")
         smooth_bkg_templates(bkg_fname)
 
-    if not args.only_bkg:
+    if "sig" in templates_to_run:
+        base_sig_template_name = f"raw_templates_lj_sig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_sig_{args.year}_{jobid}.coffea"
         sig_fname = os.path.join(input_dir, base_sig_template_name)
         if not os.path.isfile(sig_fname): raise ValueError("No signal file found.")
         print("Creating smoothed signal templates")
         smooth_sig_templates(sig_fname)
+
+    if "MEreweight_sig" in templates_to_run:
+        base_sig_template_name = f"raw_templates_lj_MEsig_kfactors_{args.year}_{jobid}.coffea" if args.kfactors else f"raw_templates_lj_MEsig_{args.year}_{jobid}.coffea"
+        sig_fname = os.path.join(input_dir, base_sig_template_name)
+        if not os.path.isfile(sig_fname): raise ValueError("No signal file found.")
+        print("Creating ME reweighting signal templates")
+        smooth_MEsig_templates(sig_fname)
 
     toc = time.time()
     print("Total time: %.1f" % (toc - tic))
