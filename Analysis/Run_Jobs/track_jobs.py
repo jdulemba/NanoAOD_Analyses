1f18295805be83d46b375bf12bd578fc6c9fe4a9
jdulemba <josdrw9@gmail.com>
2022-04-26 16:50:55 +0200

    Checks for files in cernbox. Saves files to merge before resubmitting jobs to condor. Has ability to not resubmit jobs.

--- Analysis/Run_Jobs/track_jobs.py
+++ Analysis/Run_Jobs/track_jobs.py
@@ -5,32 +5,37 @@ import Utilities.plot_tools as plot_tools
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("jobdir", help="specify name of directory where jobs were submitted")
+parser.add_argument("--resub", action="store_false", help="Resubmit failed jobs, default is True.")
 args = parser.parse_args()
 
 proj_dir = os.environ["PROJECT_DIR"]
 user = os.environ["USER"]
 
+#print(args.resub)
+#set_trace()
 ## once jobs are finished, merge all output coffea files for each sample into one
-jobdir = args.jobdir if (args.jobdir).startswith(os.path.join("afs", "cern.ch", "work", "j", user)) else os.path.join(proj_dir, args.jobdir)
-jobdir = jobdir[:-1] if jobdir.endswith("/") else jobdir
-if not os.path.isdir(jobdir):
-    raise IOError(f"Directory: {jobdir} does not exist")
+condor_jobdir = args.jobdir if (args.jobdir).startswith(os.path.join("afs", "cern.ch", "work", "j", user)) else os.path.join(proj_dir, args.jobdir)
+condor_jobdir = condor_jobdir[:-1] if condor_jobdir.endswith("/") else condor_jobdir
+if not os.path.isdir(condor_jobdir):
+    raise IOError(f"Directory: {condor_jobdir} does not exist")
 
-if os.path.isfile(os.path.join(jobdir, f"{args.jobdir}_TOT.coffea")):
-    print(f"Combined output file {jobdir}/{args.jobdir}_TOT.coffea already exists")
+basedir = os.path.basename(condor_jobdir)
+eos_dir = os.path.join("/eos", "user", user[0], user, "NanoAOD_Analyses")
+eos_jobdir = os.path.join(eos_dir, basedir)
+if os.path.isfile(os.path.join(eos_jobdir, f"{args.jobdir}_TOT.coffea")):
+    print(f"Combined output file {eos_jobdir}/{args.jobdir}_TOT.coffea already exists")
     import sys; sys.exit()
 
-basedir = os.path.basename(jobdir)
 
 orig_dir = os.getcwd()
-samples = [dirname for dirname in os.listdir(jobdir) if os.path.isdir(os.path.join(jobdir, dirname))]
+samples = [dirname for dirname in os.listdir(condor_jobdir) if os.path.isdir(os.path.join(condor_jobdir, dirname))]
 
-def check_correctness(jobdir, dump_rescue = False):
+def check_correctness(sample, dump_rescue=False):
+    #set_trace()
     isCorrect = True
-    sample = jobdir.split("/")[-1]
-    if os.path.isfile(f"{jobdir}/{sample}_TOT.coffea"):  ## TOT output file already created
+    if os.path.isfile(f"{eos_jobdir}/{sample}/{sample}_TOT.coffea"):  ## TOT output file already created
         return isCorrect, 0, True
-    jdl = open(f"{jobdir}/condor.jdl").read()
+    jdl = open(f"{condor_jobdir}/{sample}/condor.jdl").read()
     blocks = jdl.split("\n\n")
     header = blocks[0]
     block_map = {}
@@ -43,8 +48,7 @@ def check_correctness(jobdir, dump_rescue = False):
     npass = 0
     fails = []
     for fname, arguments in block_map.items():
-        if not os.path.isfile(os.path.join(jobdir, fname)):
-        #if not os.path.isfile(fname):
+        if not os.path.isfile(os.path.join(eos_jobdir, sample, fname)):
             fails.append(fname)
         else:
             npass += 1
@@ -58,7 +62,7 @@ Failed jobs: %d
         isCorrect = False
         if dump_rescue:
             print("dumping rescue job")
-            with open(f"{jobdir}/condor.rescue.jdl", "w") as rescue:
+            with open(f"{condor_jobdir}/{sample}/condor.rescue.jdl", "w") as rescue:
                 rescue.write(header)
                 rescue.write("\n\n")
                 rescue.write(
@@ -77,16 +81,21 @@ while not escape:
     njobs = 0
     samples_status = {}
 
-    #set_trace()
+        # find files that are already finished
+    if os.path.isfile(os.path.join(condor_jobdir, "to_merge.txt")):
+        to_merge_txt = open(os.path.join(condor_jobdir, "to_merge.txt")).read()
+        finished_samples = to_merge_txt.split("\n")
+
         ## this loop is just to check how many jobs are still running for each sample
     for sample in samples:
         if sample in finished_samples: continue
             ## checks how many lines correspond to "outfname=sample_out"
         sample_njobs = len([line for line in stdout.split(b"\n") if ((f"outfname={basedir}_{sample}_out").encode() in line)])
         if sample_njobs == 0:
-            isCorrect, nfails, alreadyComplete = check_correctness(os.path.join(jobdir, sample), dump_rescue=True)
+            isCorrect, nfails, alreadyComplete = check_correctness(sample=sample, dump_rescue=args.resub)
             if isCorrect == True:
                 status = "MERGED" if alreadyComplete else "COMPLETE" # jobs are already merged or (completed but still need to be merged)
+                finished_samples.append(sample)
             else:
                 status = "RESUB" # jobs need to be resubmitted
             samples_status[sample] = (status, nfails)
@@ -98,9 +107,10 @@ while not escape:
                 ## check how many jobs haven"t been removed
             njobs_running = len([status for status in job_statuses if not (status == "X")])
             if njobs_running == 0:
-                isCorrect, nfails, alreadyComplete = check_correctness(os.path.join(jobdir, sample), dump_rescue=True)
+                isCorrect, nfails, alreadyComplete = check_correctness(sample=sample, dump_rescue=args.resub)
                 if isCorrect == True:
                     status = "MERGED" if alreadyComplete else "COMPLETE" # jobs are already merged or (completed but still need to be merged)
+                    finished_samples.append(sample)
                 else:
                     status = "RESUB" # jobs need to be resubmitted
                 samples_status[sample] = (status, nfails)
@@ -108,15 +118,21 @@ while not escape:
                 samples_status[sample] = ("RUNNING", njobs_running) # jobs running
                 njobs += njobs_running
 
-    #set_trace()
+        # write finished samples to txt file
+    if finished_samples:
+        to_merge_txt = open(os.path.join(condor_jobdir, "to_merge.txt"),"w")
+        to_merge_txt.write("\n".join(finished_samples))
+        to_merge_txt.close()
+        print(f"{os.path.join(condor_jobdir, 'to_merge.txt')} written\n")
+        #set_trace()
+
         ## this loop is responsible for merging or resubmitting jobs based on status
     for sample, (status, sample_njobs) in samples_status.items():
             ## if status is "RESUB", resubmit jobs to condor
         if status == "RESUB":
             #set_trace()
             print(f"\t{sample} needs to be resubmitted")
-            os.system(f"cd {jobdir}/{sample} && condor_submit condor.rescue.jdl && cd {orig_dir}")
-            #os.system('cd %s/%s && condor_submit condor.rescue.jdl && cd %s' % (jobdir, sample, orig_dir))
+            os.system(f"cd {condor_jobdir}/{sample} && condor_submit condor.rescue.jdl && cd {orig_dir}")
             samples_status[sample] = ("RUNNING", sample_njobs)
             njobs += sample_njobs
 
@@ -124,33 +140,17 @@ while not escape:
         elif status == "RUNNING":
             print("\t%s has %i jobs running" % (sample, sample_njobs) )
 
-            ## if status is "MERGED" or "COMPLETE", run merge_jobs and add files to finished_samples list so they"re not check anymore
-        else:
-            #set_trace()
-            #merge_files(jobdir, [sample])
-            finished_samples.append(sample)
-
-        # write finished samples to txt file
-    if finished_samples:
-        to_merge_txt = open(os.path.join(jobdir, "to_merge.txt"),"w")
-        to_merge_txt.write("\n".join(finished_samples))
-        to_merge_txt.close()
-        #set_trace()
-
         ## recheck all jobs if there are none running
     if njobs == 0:
         failed_samples = []
         finished_samples = [] # reinitialize finished_samples to check if they're actually finished
         for sample in samples:
-            isCorrect, nfails, alreadyComplete = check_correctness(f"{jobdir}/{sample}", dump_rescue=True)
+            isCorrect, nfails, alreadyComplete = check_correctness(sample=sample, dump_rescue=args.resub)
             if isCorrect == False: 
                 failed_samples.append(sample)
-                os.system(f"cd {jobdir}/{sample} && condor_submit condor.rescue.jdl && cd {orig_dir}")
+                os.system(f"cd {condor_jobdir}/{sample} && condor_submit condor.rescue.jdl && cd {orig_dir}")
             else:
                 finished_samples.append(sample)
-        #if finished_samples:
-        #    set_trace()
-        #    merge_files(jobdir, finished_samples)
         if len(failed_samples) == 0:
             print("All jobs completed")
             escape = True
