012add5784e4432b01a2211c630b5f6389737830
jdulemba <josdrw9@gmail.com>
2022-05-23 17:34:33 +0200

    Deleted function that makes flattened version of cosine theta*

--- Analysis/Utilities/make_variables.py
+++ Analysis/Utilities/make_variables.py
@@ -18,7 +18,7 @@ def ctstar(top1, top2, flatten=False):
         return top1_ctstar, top2_ctstar
 
 
-def cpTP(top1, top2):
+def cpTP(top1, top2, flatten=False):
     """
     cpTP is obtained by boosting into the zero momentum frame of the top pair system, and then taking the pz/|p| of the top quark in this frame
     """
@@ -32,7 +32,10 @@ def cpTP(top1, top2):
     # find cpTP
     top1_cpTP, top2_cpTP = rf_top1.pz/abs(rf_top1.p), rf_top2.pz/abs(rf_top2.p)
 
-    return top1_cpTP, top2_cpTP
+    if flatten:
+        return ak.flatten(top1_cpTP, axis=None), ak.flatten(top2_cpTP, axis=None)
+    else:
+        return top1_cpTP, top2_cpTP
 
 
 def MT(leptons, met, debug=False):
@@ -43,24 +46,6 @@ def MT(leptons, met, debug=False):
     met_py = (ak.ones_like(leptons.pt))*(met.py)
 
     return np.sqrt( np.square(leptons.pt + met_pt) - np.square(leptons.px + met_px) - np.square(leptons.py + met_py) )
-
-def ctstar_flat(top_p4, tbar_p4):
-    # convert 4vecs to cartesian
-    tops = top_p4._to_cartesian()
-    tbars = tbar_p4._to_cartesian()
-
-    ttbar = (tops+tbars)
-
-    # find top quarks in rest frame
-    tt_boost = ttbar.boostp3
-    rf_tops = tops.boost(tt_boost*-1)
-    rf_tbars = tbars.boost(tt_boost*-1)
-
-    # find ctstar
-    top_ctstar = ttbar.p3.unit.dot(rf_tops.p3.unit)
-    tbar_ctstar = ttbar.p3.unit.dot(rf_tbars.p3.unit)
-
-    return top_ctstar, tbar_ctstar
 
 
 def rapidity(top, flatten=False):
