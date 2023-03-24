326d35ac9777f8b3861a30e5cb6221d15064cf71
jdulemba <josdrw9@gmail.com>
2023-03-24 16:56:10 +0100

    Added 'plots_dir' and 'eos_dir' variables, add 'smoothing_compiled' to LD_LIBRARY_PATH

--- Analysis/environment.sh
+++ Analysis/environment.sh
@@ -10,14 +10,24 @@ then
 fi 
 
 #This part should be changed by the user(s)
-export jobid=NOTSET
-if [ -e $PROJECT_DIR/jobid.sh ]
+
+if [ ! -z $1 ]
 then
-        source $PROJECT_DIR/jobid.sh
-        echo "set jobid: $jobid"
-        echo "set nanoAOD version: $base_jobid"
+    export jobid=$1
+    echo "set jobid: $jobid"
+    source $PROJECT_DIR/base_jobid.sh
+    echo "set nanoAOD version: $base_jobid"
 else
-        echo "I did not find jobid.sh, are you sure you do not want to set the jobid and leave it to $jobid?"
+    export jobid=NOTSET
+    if [ -e $PROJECT_DIR/jobid.sh ]
+    then
+            source $PROJECT_DIR/jobid.sh
+            echo "set jobid: $jobid"
+            source $PROJECT_DIR/base_jobid.sh
+            echo "set nanoAOD version: $base_jobid"
+    else
+            echo "I did not find jobid.sh, are you sure you do not want to set the jobid and leave it to $jobid?"
+    fi
 fi
 
 #HERE ARE LIONS!
@@ -25,5 +35,9 @@ export LIBRARY_PATH=$LD_LIBRARY_PATH
 
 export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
 export LD_LIBRARY_PATH=$PROJECT_DIR/compiled:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PROJECT_DIR/smoothing_compiled:$LD_LIBRARY_PATH
 
 python --version
+
+export plots_dir="/eos/user/${USER:0:1}/$USER/NanoAOD_Analyses/plots"
+export eos_dir="/eos/user/${USER:0:1}/$USER/NanoAOD_Analyses/"
