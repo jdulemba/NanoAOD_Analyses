bafd9b8c3f9c39c28ab29d9fd7be4d16c391d1de
jdulemba <josdrw9@gmail.com>
2023-03-24 14:45:24 +0100

    Changed plotting pars and added differential distributions

--- Analysis/NNLO_files/plot_NNLOqcd_dists.py
+++ Analysis/NNLO_files/plot_NNLOqcd_dists.py
@@ -8,7 +8,7 @@ plt.style.use(hep.cms.style.ROOT)
 plt.switch_backend("agg")
 from matplotlib import rcParams
 rcParams["font.size"] = 20
-rcParams["savefig.format"] = "png"
+rcParams["savefig.format"] = "pdf"
 rcParams["savefig.bbox"] = "tight"
 
 import Utilities.Plotter as Plotter
@@ -31,9 +31,10 @@ args = parser.parse_args()
 proj_dir = os.environ["PROJECT_DIR"]
 base_jobid = os.environ["base_jobid"]
 analyzer = "NNLOqcd_dists"
+plots_dir = os.environ["plots_dir"]
 
 f_ext = "TOT.coffea"
-outdir = os.path.join(proj_dir, "plots", base_jobid, analyzer)
+outdir = os.path.join(plots_dir, base_jobid, analyzer)
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
    
@@ -49,19 +50,26 @@ nnlo_dict = Plotter.root_converters_dict_to_hist(nnlo_file, vars=[nnlo_var],
     dense_axes_list=[{"name" : "ctstar", "idx" : 0}, {"name": "mtt", "idx" : 1}],
 )
 
+max_mtt_val = 2000.
 mtt_binning = nnlo_dict[nnlo_var].axis("mtt").edges()
+max_mtt_val_bin = np.argwhere(mtt_binning == max_mtt_val)[0][0]
+mtt_binwidths = np.array([mtt_binning[i+1] - mtt_binning[i] for i in range(max_mtt_val_bin)])
+#mtt_binwidths = np.array([mtt_binning[i+1] - mtt_binning[i] for i in range(len(mtt_binning) - 1)])
 ctstar_binning = nnlo_dict[nnlo_var].axis("ctstar").edges()
+ctstar_binwidths = np.array([ctstar_binning[i+1] - ctstar_binning[i] for i in range(len(ctstar_binning) - 1)])
+tiled_mtt_binwidths = np.tile(mtt_binwidths, (ctstar_binwidths.size, 1))
+tiled_ctstar_binwidths = np.tile(ctstar_binwidths, (mtt_binwidths.size, 1)).T
 vlines = [(len(mtt_binning)-1)*ybin for ybin in range(1, len(ctstar_binning)-1)]
 #set_trace()
 
 ## linearize nnlo_hist
-nnlo_lin_dict = {name: Plotter.linearize_hist(nnlo_dict[name].integrate("dataset")) for name in nnlo_dict.keys()}
+#nnlo_lin_dict = {name: Plotter.linearize_hist(nnlo_dict[name].integrate("dataset")) for name in nnlo_dict.keys()}
 
 png_ext = "StatUncs"
 nnlo_leg = "(Stat.)"
 
 axis_labels_dict = {
-    "mtt" : "$m_{t\\bar{t}}$",
+    "mtt" : "$m_{t\\bar{t}}$ [GeV]",
     "ctstar" : "cos($\\theta^{*}_{t}$)"
 }
 
@@ -69,22 +77,25 @@ axis_labels_dict = {
 ############## Plot hists ################
 
 ### raw histo ###
-nnlo_histo = nnlo_dict[nnlo_var]
+nnlo_histo = nnlo_dict[nnlo_var].integrate("dataset")
 
         # histo plotting params
 xtitle, ytitle = axis_labels_dict[nnlo_histo.dense_axes()[0].name], axis_labels_dict[nnlo_histo.dense_axes()[1].name]
 ztitle = "$\dfrac{d^{2} \\sigma}{d m_{t\\bar{t}} d cos(\\theta^{*}_{t})}$"
 opts = {"cmap_label" : ztitle}
 norm_opts = {"cmap_label" : "$\dfrac{1}{\\sigma}$%s [$GeV^{-1}$]" % ztitle}
-x_lims = (np.min(nnlo_histo.dense_axes()[0].edges()), np.max(nnlo_histo.dense_axes()[0].edges()))
-y_lims = (np.min(nnlo_histo.dense_axes()[1].edges()), np.max(nnlo_histo.dense_axes()[1].edges()))
+#set_trace()
+x_lims = (np.min(nnlo_histo.dense_axes()[0].edges()), min(np.max(nnlo_histo.dense_axes()[0].edges()), max_mtt_val))
+y_lims = (np.min(nnlo_histo.dense_axes()[1].edges()), min(np.max(nnlo_histo.dense_axes()[1].edges()), max_mtt_val))
 
     # plot 2d version of original hist
 fig, ax = plt.subplots()
 fig.subplots_adjust(hspace=.07)
 
-nnlo_vals = nnlo_histo.integrate("dataset").values()[()]
-Plotter.plot_2d_norm(hdict=nnlo_histo, xaxis_name=nnlo_histo.dense_axes()[0].name, yaxis_name=nnlo_histo.dense_axes()[1].name,
+#set_trace()
+#nnlo_vals = nnlo_histo.integrate("dataset").values()[()]
+nnlo_vals = nnlo_histo[:, y_lims[0]:y_lims[-1]].values()[()]
+Plotter.plot_2d_norm(hdict=nnlo_histo[:, y_lims[0]:y_lims[-1]], xaxis_name=nnlo_histo.dense_axes()[0].name, yaxis_name=nnlo_histo.dense_axes()[1].name,
     values=np.ma.masked_where(nnlo_vals <= 0.0, nnlo_vals), # mask nonzero probabilities for plotting
     xlimits=x_lims, ylimits=y_lims, xlabel=xtitle, ylabel=ytitle,
     ax=ax, **opts)
@@ -92,7 +103,6 @@ Plotter.plot_2d_norm(hdict=nnlo_histo, xaxis_name=nnlo_histo.dense_axes()[0].nam
 #set_trace()
 ax.text(
     0.98, 0.90, "$t\\bart$\nparton level",
-    #0.98, 0.90, "$t\\bart \\rightarrow e/\mu + jets$\nparton level",
     fontsize=rcParams["font.size"], horizontalalignment="right", verticalalignment="bottom", transform=ax.transAxes, color="w",
 )
 hep.label.exp_label(ax=ax, exp="MATRIX", rlabel="NNLO QCD")
@@ -106,14 +116,13 @@ print(f"{figname} written")
 fig, ax = plt.subplots()
 fig.subplots_adjust(hspace=.07)
 
-Plotter.plot_2d_norm(hdict=nnlo_histo, xaxis_name=nnlo_histo.dense_axes()[1].name, yaxis_name=nnlo_histo.dense_axes()[0].name,
+Plotter.plot_2d_norm(hdict=nnlo_histo[:, y_lims[0]:y_lims[-1]], xaxis_name=nnlo_histo.dense_axes()[1].name, yaxis_name=nnlo_histo.dense_axes()[0].name,
     values=np.ma.masked_where(nnlo_vals.T <= 0.0, nnlo_vals.T), # mask nonzero probabilities for plotting
     xlimits=y_lims, ylimits=x_lims, xlabel=ytitle, ylabel=xtitle,
     ax=ax, **opts)
 
 ax.text(
     0.98, 0.90, "$t\\bart$\nparton level",
-    #0.98, 0.90, "$t\\bart \\rightarrow e/\mu + jets$\nparton level",
     fontsize=rcParams["font.size"], horizontalalignment="right", verticalalignment="bottom", transform=ax.transAxes, color="w",
 )
 hep.label.exp_label(ax=ax, exp="MATRIX", rlabel="NNLO QCD")
@@ -127,20 +136,23 @@ print(f"{figname} written")
 fig_norm, ax_norm = plt.subplots()
 fig_norm.subplots_adjust(hspace=.07)
 
+#set_trace()
 nnlo_norm_vals = nnlo_vals/np.sum(nnlo_vals)
-Plotter.plot_2d_norm(hdict=nnlo_histo, xaxis_name=nnlo_histo.dense_axes()[0].name, yaxis_name=nnlo_histo.dense_axes()[1].name,
+    # make values differential
+nnlo_norm_vals = nnlo_norm_vals/(tiled_ctstar_binwidths*tiled_mtt_binwidths)
+Plotter.plot_2d_norm(hdict=nnlo_histo[:, y_lims[0]:y_lims[-1]], xaxis_name=nnlo_histo.dense_axes()[0].name, yaxis_name=nnlo_histo.dense_axes()[1].name,
     values=np.ma.masked_where(nnlo_norm_vals <= 0.0, nnlo_norm_vals), # mask nonzero probabilities for plotting
     xlimits=x_lims, ylimits=y_lims, xlabel=xtitle, ylabel=ytitle,
     ax=ax_norm, **norm_opts)
 
 ax_norm.text(
     0.98, 0.90, "$t\\bart$\nparton level",
-    #0.98, 0.90, "$t\\bart \\rightarrow e/\mu + jets$\nparton level",
     fontsize=rcParams["font.size"], horizontalalignment="right", verticalalignment="bottom", transform=ax_norm.transAxes, color="w",
 )
 hep.label.exp_label(ax=ax_norm, exp="MATRIX", rlabel="NNLO QCD")
 
-figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Norm" % (base_jobid, nnlo_histo.dense_axes()[0].name, nnlo_histo.dense_axes()[1].name, png_ext))
+figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Differential" % (base_jobid, nnlo_histo.dense_axes()[0].name, nnlo_histo.dense_axes()[1].name, png_ext))
+#figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Norm" % (base_jobid, nnlo_histo.dense_axes()[0].name, nnlo_histo.dense_axes()[1].name, png_ext))
 fig_norm.savefig(figname_norm)
 print(f"{figname_norm} written")
     
@@ -149,19 +161,19 @@ print(f"{figname_norm} written")
 fig_norm, ax_norm = plt.subplots()
 fig_norm.subplots_adjust(hspace=.07)
 
-Plotter.plot_2d_norm(hdict=nnlo_histo, xaxis_name=nnlo_histo.dense_axes()[1].name, yaxis_name=nnlo_histo.dense_axes()[0].name,
+Plotter.plot_2d_norm(hdict=nnlo_histo[:, y_lims[0]:y_lims[-1]], xaxis_name=nnlo_histo.dense_axes()[1].name, yaxis_name=nnlo_histo.dense_axes()[0].name,
     values=np.ma.masked_where(nnlo_norm_vals.T <= 0.0, nnlo_norm_vals.T), # mask nonzero probabilities for plotting
     xlimits=y_lims, ylimits=x_lims, xlabel=ytitle, ylabel=xtitle,
     ax=ax_norm, **norm_opts)
 
 ax_norm.text(
     0.98, 0.90, "$t\\bart$\nparton level",
-    #0.98, 0.90, "$t\\bart \\rightarrow e/\mu + jets$\nparton level",
     fontsize=rcParams["font.size"], horizontalalignment="right", verticalalignment="bottom", transform=ax_norm.transAxes, color="w",
 )
 hep.label.exp_label(ax=ax_norm, exp="MATRIX", rlabel="NNLO QCD")
 
-figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Norm" % (base_jobid, nnlo_histo.dense_axes()[1].name, nnlo_histo.dense_axes()[0].name, png_ext))
+figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Differential" % (base_jobid, nnlo_histo.dense_axes()[1].name, nnlo_histo.dense_axes()[0].name, png_ext))
+#figname_norm = os.path.join(outdir, "%s_ttJets_xsec_%s_vs_%s_%s_Norm" % (base_jobid, nnlo_histo.dense_axes()[1].name, nnlo_histo.dense_axes()[0].name, png_ext))
 fig_norm.savefig(figname_norm)
 print(f"{figname_norm} written")
     
